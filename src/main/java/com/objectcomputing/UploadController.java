0339f2371d4ee46f62bf4bdd3155f9e6fe151f7d
chrismckiernan <mckiernanc@objectcomputing.com>
2019-05-09 15:55:18 -0500

    Able to push now to a specific folder

--- src/main/java/com/objectcomputing/UploadController.java
+++ src/main/java/com/objectcomputing/UploadController.java
@@ -1,11 +1,17 @@
 package com.objectcomputing;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import com.google.api.client.http.InputStreamContent;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
@@ -15,13 +21,18 @@ import io.micronaut.views.View;
 
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
+import java.util.List;
 
 @Validated
 @Controller("upload")
 public class UploadController {
+    private static final String DIRECTORY_FILE_PATH = "/secrets/directory.json";
     private static final String RSP_SERVER_ERROR_KEY = "serverError";
     private static final String RSP_ERROR_KEY = "error";
     private static final String RSP_COMPLETE_MESSAGE_KEY = "completeMessage";
+
+    @Property(name = "oci-google-drive.application.dir-key")
+    private String directoryKey;
 
     @NotNull
     private GoogleDriveAccessor googleDriveAccessor;
@@ -52,8 +63,14 @@ public class UploadController {
      */
     @View("upload")
     @Post(consumes = MediaType.MULTIPART_FORM_DATA)
-    public HttpResponse upload(CompletedFileUpload file) throws IOException {
-        Drive drive = googleDriveAccessor.accessGoogleDrive();
+    public HttpResponse upload(@Body CompletedFileUpload file) {
+        Drive drive = null;
+        try {
+            drive = googleDriveAccessor.accessGoogleDrive();
+        } catch (IOException e) {
+            // Catch down below
+        }
+
         if(drive == null) {
             return HttpResponse.serverError(CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY,
                     "Unable to access Google Drive"));
@@ -63,10 +80,29 @@ public class UploadController {
             return HttpResponse.badRequest(CollectionUtils.mapOf(RSP_ERROR_KEY, "Required file"));
         }
 
+        if(directoryKey == null) {
+            return HttpResponse.serverError(CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY,
+                    "Directory key error, please contact admin"));
+        }
+
+        JsonNode dirNode = null;
+        try {
+            dirNode = new ObjectMapper().readTree(this.getClass().getResourceAsStream(DIRECTORY_FILE_PATH));
+        } catch (IOException e) {
+            return HttpResponse.serverError(CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY,
+                    "Configuration error, please contact admin"));
+        }
+
+        String parentId = dirNode.get(directoryKey) != null ? dirNode.get(directoryKey).asText() : null;
+        if(parentId == null) {
+            return HttpResponse.serverError(CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY,
+                    "Configuration error, please contact admin"));
+        }
 
         File fileMetadata = new File();
         fileMetadata.setName(file.getFilename());
         fileMetadata.setMimeType(file.getContentType().orElse(MediaType.APPLICATION_OCTET_STREAM_TYPE).toString());
+        fileMetadata.setParents(List.of(parentId));
 
         InputStreamContent content;
         try {
@@ -77,7 +113,7 @@ public class UploadController {
         }
 
         try {
-            drive.files().create(fileMetadata, content).execute();
+            drive.files().create(fileMetadata, content).setFields("parents").execute();
         } catch (IOException e) {
             return HttpResponse.serverError(CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY,
                     "Unable to upload file to Google Drive"));
