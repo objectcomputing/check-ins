4196492fba2474b38178c6bee6e2d529dc48fbbd
Michael Kimberlin <mkimberlin@gmail.com>
2020-04-21 15:37:31 -0500

    readme updates and simplified config

--- src/main/java/com/objectcomputing/UploadController.java
+++ src/main/java/com/objectcomputing/UploadController.java
@@ -14,7 +14,6 @@ import com.google.api.services.drive.model.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.micronaut.context.annotation.Property;
 import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
@@ -32,12 +31,10 @@ public class UploadController {
 
     private static final Logger LOG = LoggerFactory.getLogger(UploadController.class);
 
+    private static final String DIRECTORY_KEY = "upload-directory-id";
     private static final String DIRECTORY_FILE_PATH = "/secrets/directory.json";
     private static final String RSP_SERVER_ERROR_KEY = "serverError";
     private static final String RSP_COMPLETE_MESSAGE_KEY = "completeMessage";
-
-    @Property(name = "oci-google-drive.application.dir-key")
-    private String directoryKey;
 
     @NotNull
     private final GoogleDriveAccessor googleDriveAccessor;
@@ -92,11 +89,6 @@ public class UploadController {
             return HttpResponse.badRequest(CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY, "Please select a file before uploading."));
         }
 
-        if (directoryKey == null) {
-            return HttpResponse.serverError(
-                    CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY, "Directory key error, please contact admin"));
-        }
-
         JsonNode dirNode = null;
         try {
             dirNode = new ObjectMapper().readTree(this.getClass().getResourceAsStream(DIRECTORY_FILE_PATH));
@@ -105,7 +97,7 @@ public class UploadController {
                     CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY, "Configuration error, please contact admin"));
         }
 
-        final String parentId = dirNode.get(directoryKey) != null ? dirNode.get(directoryKey).asText() : null;
+        final String parentId = dirNode.get(DIRECTORY_KEY) != null ? dirNode.get(DIRECTORY_KEY).asText() : null;
         if (parentId == null) {
             return HttpResponse.serverError(
                     CollectionUtils.mapOf(RSP_SERVER_ERROR_KEY, "Configuration error, please contact admin"));
