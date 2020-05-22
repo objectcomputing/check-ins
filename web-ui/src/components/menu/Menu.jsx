c01dc5ddf3f6d852e1827bc908507c8ad4cc9bb6
Jesse Hanner <hannerj@objectcomputing.com>
2020-05-22 13:53:01 -0500

    working on converting the menu to a material-ui drawer

--- web-ui/src/components/menu/Menu.jsx
+++ web-ui/src/components/menu/Menu.jsx
@@ -1,24 +1,140 @@
-import React from 'react';
-import {
-    Link
-} from "react-router-dom";
-import './Menu.css';
+import { Link } from "react-router-dom";
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
-const Menu = () => {
+const drawerWidth = 150;
 
-    return (
-        <div>
-            <input type="checkbox" id="menu-toggle" />
-            <label id="trigger" for="menu-toggle"></label>
-            <label id="burger" for="menu-toggle"></label>
-            <ul id="menu">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/team">My Team</Link></li>
-                <li><Link to="/resources">Resources</Link></li>
-                <li><Link to="/upload">Uploads</Link></li>
-            </ul>
-        </div >
-    )
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function Menu(props) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        <Button>
+          <Link to="/">Home</Link>
+        </Button>
+        <br />
+        <Button>
+          <Link to="/team">Team</Link>
+        </Button>
+        <br />
+        <Button>
+          <Link to="/resources">Resources</Link>
+        </Button>
+        <br />
+        <Button>
+          <Link to="/upload">Uploads</Link>
+        </Button>
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root} style={{ paddingRight: `${drawerWidth}px` }}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+      </main>
+    </div>
+  );
+}
 
 export default Menu;
