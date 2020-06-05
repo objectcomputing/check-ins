6b36150f06e50ce0be72c9d07cf945a2e596c187
Jesse Hanner <hannerj@objectcomputing.com>
2020-06-05 12:22:51 -0500

    changed img src

--- web-ui/src/components/profile/Profile.js
+++ web-ui/src/components/profile/Profile.js
@@ -1,7 +1,45 @@
 import React from "react";
 
-const Profile = () => {
-  return <div>Profile page coming soon</div>;
+const Profile = (profile = {}) => {
+  const { image_url, name, email, role, pdl } = profile;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          marginLeft: "50px",
+          marginRight: "50px",
+        }}
+      >
+        <img
+          src={image_url}
+          style={{ maxWidth: "256px", maxHeight: "256px" }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <div style={{ textAlign: "left" }}>
+          <h2>Name</h2>
+          <div>
+            <p>Role</p>
+            <p>Email</p>
+            <p>Current PDL</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
