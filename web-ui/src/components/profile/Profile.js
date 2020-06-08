d0b40e70eeb8e2c6b41ce9dccf0a896186ca2585
Jesse Hanner <hannerj@objectcomputing.com>
2020-06-08 14:12:05 -0500

    moved css outside of Profile.js

--- web-ui/src/components/profile/Profile.js
+++ web-ui/src/components/profile/Profile.js
@@ -1,48 +1,20 @@
 import React from "react";
+import "./Profile.css";
 
 const Profile = (profile = {}) => {
   const { image_url, name, email, role, pdl } = profile;
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          marginLeft: "50px",
-          marginRight: "50px",
-        }}
-      >
+    <div className="flex-row">
+      <div className="image-div">
         <img
           alt="Profile"
           src={image_url ? image_url : "https://i.imgur.com/TkSNOpF.jpg"}
-          style={{ borderRadius: "50%", maxWidth: "256px", maxHeight: "256px" }}
         />
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
+      <div className="flex-row">
         <div style={{ marginTop: "30%", textAlign: "left" }}>
-          <div
-            style={{
-              backgroundColor: "green",
-              borderRadius: "15px",
-              color: "white",
-              padding: "5px",
-              marginBottom: "10px",
-              textAlign: "center",
-            }}
-          >
-            Software Engineer
-          </div>
+          <div className="tag">Software Engineer</div>
           {/* could put role up here as tag instead of below?? */}
           <h2 style={{ margin: 0 }}>Name</h2>
           <div>
