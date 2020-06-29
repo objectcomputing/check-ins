353039f98db52e5f5580cd63e9d5400ff666e05e
Jesse Hanner <hannerj@objectcomputing.com>
2020-06-29 10:56:20 -0500

    added context to keep track of current user

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import "./App.css";
+
 import MyTeamPage from "./pages/MyTeamPage";
 import ResourcesPage from "./pages/ResourcesPage";
 import UploadNotesPage from "./pages/UploadNotesPage";
@@ -9,46 +9,59 @@ import HomePage from "./pages/HomePage";
 import Menu from "./components/menu/Menu";
 import Header from "./components/header/Header";
 import Profile from "./components/profile/Profile";
+import ProfileContext from "./context/ProfileContext";
+
+import "./App.css";
+
+const defaultProfile = {
+  bio: "It was all a dream, I used to read Word Up magazine",
+  email: "Biggie@oci.com",
+  name: "Christopher Wallace",
+  PDL: "Tupac Shakur",
+  role: "Lyrical Poet",
+};
 
 const customHistory = createBrowserHistory();
 
 function App() {
   return (
     <Router history={customHistory}>
-      <div>
-        <Menu />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-          className="App"
-        >
-          <Switch>
-            <Route path="/team">
-              <Header title="My Team" />
-              <MyTeamPage />
-            </Route>
-            <Route path="/resources">
-              <Header title="Resources" />
-              <ResourcesPage />
-            </Route>
-            <Route path="/upload">
-              <Header title="Upload Notes" />
-              <UploadNotesPage />
-            </Route>
-            <Route path="/profile">
-              <Header title="Profile" />
-              <Profile />
-            </Route>
-            <Route path="/">
-              <Header title="Professional Development @ OCI" />
-              <HomePage />
-            </Route>
-          </Switch>
+      <ProfileContext.Provider value={defaultProfile}>
+        <div>
+          <Menu />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+            className="App"
+          >
+            <Switch>
+              <Route path="/team">
+                <Header title="My Team" />
+                <MyTeamPage />
+              </Route>
+              <Route path="/resources">
+                <Header title="Resources" />
+                <ResourcesPage />
+              </Route>
+              <Route path="/upload">
+                <Header title="Upload Notes" />
+                <UploadNotesPage />
+              </Route>
+              <Route path="/profile">
+                <Header title="Profile" />
+                <Profile />
+              </Route>
+              <Route path="/">
+                <Header title="Professional Development @ OCI" />
+                <HomePage />
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </ProfileContext.Provider>
     </Router>
   );
 }
--- web-ui/src/components/profile/Profile.jsx
+++ web-ui/src/components/profile/Profile.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import ProfileContext from "../../context/ProfileContext";
 import "./Profile.css";
 
-const Profile = (props) => {
-  const { image_url, name } = props;
+const Profile = () => {
+  const { bio, email, image_url, name, PDL, role } = useContext(ProfileContext);
 
   return (
-    <div className="flex-row">
+    <div className="flex-row" style={{ marginTop: "20px" }}>
       <div className="profile-image">
         <img
           alt="Profile"
@@ -13,12 +14,13 @@ const Profile = (props) => {
         />
       </div>
       <div className="flex-row">
-        <div style={{ marginTop: "50%", textAlign: "left" }}>
+        <div style={{ textAlign: "left" }}>
           <h2 style={{ margin: 0 }}>{name}</h2>
           <div>
-            <p>Role</p>
-            <p>Email</p>
-            <p>Current PDL</p>
+            <p>Role: {role}</p>
+            <p>Email: {email}</p>
+            <p>Current PDL: {PDL}</p>
+            <p>{bio}</p>
           </div>
         </div>
       </div>
