cad839ab87b276b94672cccf507c9cfc8d1af8fa
Jesse Hanner <hannerj@objectcomputing.com>
2020-07-01 09:52:24 -0500

    working on making the profile editable

--- web-ui/src/components/profile/Profile.jsx
+++ web-ui/src/components/profile/Profile.jsx
@@ -1,10 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ProfileContext from "../../context/ProfileContext";
+import EditIcon from "@material-ui/icons/Edit";
 import "./Profile.css";
 
 const Profile = () => {
   const context = useContext(ProfileContext);
   const { bio, email, image_url, name, PDL, role } = context.defaultProfile;
+  const [roles, setRole] = useState(role);
+  const [disabled, setDisabled] = useState(true);
+
+  // function useInput(item) {
+  //   const [value, setValue] = useState(item);
+  //   const input = (
+  //     <input value={value} onChange={(e) => setValue(e.target.value)} />
+  //   );
+  //   return [value, input];
+  // }
 
   return (
     <div className="flex-row" style={{ marginTop: "20px" }}>
@@ -16,12 +27,58 @@ const Profile = () => {
       </div>
       <div className="flex-row">
         <div style={{ textAlign: "left" }}>
-          <h2 style={{ margin: 0 }}>{name}</h2>
+          <h2 style={{ margin: 0 }}>
+            {name}
+            <EditIcon
+              onClick={() => {
+                setDisabled(!disabled);
+              }}
+              style={{
+                color: !disabled ? "green" : "black",
+                marginLeft: "20px",
+              }}
+            />
+          </h2>
           <div>
-            <p>Role: {role}</p>
-            <p>Email: {email}</p>
-            <p>Current PDL: {PDL}</p>
-            <p>{bio}</p>
+            <label htmlFor="role">Role:</label>
+            <input
+              disabled={disabled}
+              onChange={(e) => setRole(e.target.value)}
+              id="role"
+              style={{
+                border: disabled ? "none" : "1px solid black",
+              }}
+              value={roles}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              disabled={disabled}
+              id="email"
+              style={{ border: disabled ? "none" : "1px solid black" }}
+              value={email}
+            ></input>
+          </div>
+          <div style={{ display: "flex", textAlign: "left" }}>
+            <label htmlFor="pdl">PDL:</label>
+            <input
+              disabled
+              id="pdl"
+              style={{ border: "none" }}
+              value={PDL}
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="bio">Bio:</label>
+            <textarea
+              disabled={disabled}
+              id="bio"
+              style={{
+                border: disabled ? "none" : "1px solid black",
+              }}
+              value={bio}
+            ></textarea>
           </div>
         </div>
       </div>
