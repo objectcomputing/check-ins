3c487135a0555db38b999fe831b1f7b4e6f725f2
pugmark <pramukhdbagur@gmail.com>
2020-07-09 18:09:16 -0500

    Logs JDBC URL to test environment setup

--- server/src/main/java/com/objectcomputing/checkins/security/HomeController.java
+++ server/src/main/java/com/objectcomputing/checkins/security/HomeController.java
@@ -8,17 +8,28 @@ import javax.annotation.Nullable;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.views.View;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 @Secured(SecurityRule.IS_ANONYMOUS)
 public class HomeController {
 
+	@Property(name = "datasources.default.url")
+	String jdbcUrl;
+
+	private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
+
 	@Get("/unauthorized")
 	@View("unauthorized")
 	public Map<String, Object> unauthorized(@Nullable Principal principal) {
+
+		LOG.info("Value of jdbcUrl: " + jdbcUrl);
 		return new HashMap<>();
 	}
 
