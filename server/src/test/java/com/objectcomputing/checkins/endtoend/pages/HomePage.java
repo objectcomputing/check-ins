fdbe253e82d0c38329ed776373e3b8ae76d59d52
chrismckiernan <mckiernanc@objectcomputing.com>
2020-07-30 08:36:15 -0500

    End to end test fixes

--- server/src/test/java/com/objectcomputing/checkins/endtoend/pages/HomePage.java
+++ server/src/test/java/com/objectcomputing/checkins/endtoend/pages/HomePage.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.objectcomputing.checkins.endtoend.pages;
 
@@ -11,62 +11,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author tajr
- * 
+ *
  * This class will store all the locators and methods of home page.
  *
  */
 public class HomePage {
-	
-	WebDriver driver;
-	
-	public HomePage(WebDriver ldriver)
-	{
-		this.driver = ldriver;
-	}
-	
-	@FindBy(xpath="/html[1]/body[1]/div[1]/form[1]/fieldset[1]/label[1]") 
-	WebElement chooseafile_btn;
-	
-	@FindBy(id="file")
-	WebElement filepath;
-	
-	@FindBy(name="submit")
-	WebElement upload_btn;
 
-	@FindBy(className="success")
-	WebElement success_msg;
-	
-	@FindBy(className="error")
-	WebElement error_msg;
-	
-	
-	public void uploadFile() throws InterruptedException
-	{
-		// TODO needs to be fixed
-		filepath.sendKeys("/Users/tajr/Desktop/File.rtf");
-		Thread.sleep(5000);
-		upload_btn.click();
-		Thread.sleep(5000);
-	}
-	
-	public void uploadFileSuccessMsg() throws InterruptedException
-	{
-		String successMsg = success_msg.getText();
-		System.out.println("Upload File Msg: " +successMsg);
-		Thread.sleep(1000);	
-		assertEquals(success_msg.getText(),"THE FILE FILE.RTF WAS UPLOADED");
-		Thread.sleep(5000);
-	}
-		
-	public void uploadFileErrorMsg() throws InterruptedException
-	{
-		upload_btn.click();
-		Thread.sleep(5000);
-		String errorMsg = error_msg.getText();
-		System.out.println("Upload File Msg: " +errorMsg);
-		Thread.sleep(1000);	
-		assertEquals(error_msg.getText(),"Please select a file before uploading.");
-		Thread.sleep(5000);
-	}
+    WebDriver driver;
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/form[1]/fieldset[1]/label[1]")
+    WebElement chooseafile_btn;
+    @FindBy(id = "file")
+    WebElement filepath;
+    @FindBy(name = "submit")
+    WebElement upload_btn;
+    @FindBy(className = "success")
+    WebElement success_msg;
+    @FindBy(className = "error")
+    WebElement error_msg;
+
+    public HomePage(WebDriver ldriver) {
+        this.driver = ldriver;
+    }
+
+    public void uploadFile() throws InterruptedException {
+        // TODO needs to be fixed
+        filepath.sendKeys("/Users/tajr/Desktop/File.rtf");
+        Thread.sleep(5000);
+        upload_btn.click();
+        Thread.sleep(5000);
+    }
+
+    public void uploadFileSuccessMsg() throws InterruptedException {
+        String successMsg = success_msg.getText();
+        System.out.println("Upload File Msg: " + successMsg);
+        Thread.sleep(1000);
+        assertEquals(success_msg.getText(), "THE FILE FILE.RTF WAS UPLOADED");
+        Thread.sleep(5000);
+    }
+
+    public void uploadFileErrorMsg() throws InterruptedException {
+        upload_btn.click();
+        Thread.sleep(5000);
+        String errorMsg = error_msg.getText();
+        System.out.println("Upload File Msg: " + errorMsg);
+        Thread.sleep(1000);
+        assertEquals(error_msg.getText(), "Please select a file before uploading.");
+        Thread.sleep(5000);
+    }
 
 }
