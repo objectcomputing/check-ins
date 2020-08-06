a564c189197bc74781a11ee466b3ae83ba2e99e0
Michael Kimberlin <mkimberlin@gmail.com>
2020-08-05 21:10:45 -0500

    Fixes #179 - remove logging of database information

--- server/src/main/java/com/objectcomputing/checkins/gcp/postgres/GoogleCloudDatabaseSetup.java
+++ server/src/main/java/com/objectcomputing/checkins/gcp/postgres/GoogleCloudDatabaseSetup.java
@@ -1,10 +1,5 @@
 package com.objectcomputing.checkins.gcp.postgres;
 
-import javax.inject.Singleton;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.micronaut.configuration.jdbc.hikari.DatasourceConfiguration;
 import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Requires;
@@ -12,14 +7,14 @@ import io.micronaut.context.env.Environment;
 import io.micronaut.context.event.BeanCreatedEvent;
 import io.micronaut.context.event.BeanCreatedEventListener;
 
+import javax.inject.Singleton;
+
 @Singleton
 @Requires(env = Environment.GOOGLE_COMPUTE)
 @Requires(property = GoogleCloudDatabaseSetup.CLOUD_DB_CONNECTION_NAME)
 @Requires(property = GoogleCloudDatabaseSetup.DATASOURCES_DEFAULT_USERNAME)
 @Requires(property = GoogleCloudDatabaseSetup.DATASOURCES_DEFAULT_PASSWORD)
 public class GoogleCloudDatabaseSetup implements BeanCreatedEventListener<DatasourceConfiguration> {
-
-    private static final Logger LOG = LoggerFactory.getLogger(GoogleCloudDatabaseSetup.class);
 
     public static final String CLOUD_DB_CONNECTION_NAME = "cloud.db.connection.name";
     public static final String DATASOURCES_DEFAULT_USERNAME = "datasources.default.username";
