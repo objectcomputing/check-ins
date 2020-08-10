707d5e5ad0eaeae68190831f425ebb3b70ca41dd
Suman Maroju <sumanmaroju2020@u.northwestern.edu>
2020-08-10 00:42:43 -0500

    removing unnecessary @Schema

--- server/src/main/java/com/objectcomputing/checkins/services/agenda_item/AgendaItemCreateDTO.java
+++ server/src/main/java/com/objectcomputing/checkins/services/agenda_item/AgendaItemCreateDTO.java
@@ -1,7 +1,6 @@
 package com.objectcomputing.checkins.services.agenda_item;
 
 import io.micronaut.core.annotation.Introspected;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
@@ -9,15 +8,11 @@ import java.util.UUID;
 @Introspected
 public class AgendaItemCreateDTO {
     @NotNull
-    @Schema(description = "id of the checkin this entry is associated with", required = true)
     private UUID checkinid;
 
     @NotNull
-    @Schema(description = "id of the member this entry is associated with", required = true)
     private UUID createdbyid;
 
-    @Schema(description = "description of the agenda item",
-            nullable = true)
     private String description;
 
     public UUID getCheckinid() {
