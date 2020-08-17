af44063a86cc3fc4b83f4aabeacf88ba0125dbdd
chrismckiernan <mckiernanc@objectcomputing.com>
2020-08-17 09:33:52 -0500

    Revert team changes

--- server/src/main/java/com/objectcomputing/checkins/services/team/TeamRepository.java
+++ server/src/main/java/com/objectcomputing/checkins/services/team/TeamRepository.java
@@ -1,14 +1,26 @@
 package com.objectcomputing.checkins.services.team;
 
-import java.util.List;
-import java.util.UUID;
-
+import edu.umd.cs.findbugs.annotations.NonNull;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface TeamRepository extends CrudRepository<Team, UUID> {
-    List<Team> findByName(String name);
-    List<Team> findAll();
+
+    Optional<Team> findByName(String name);
+
+    List<Team> findByNameIlike(String name);
+
+    @Override
+    <S extends Team> List<S> saveAll(@Valid @NotNull Iterable<S> entities);
+
+    @Override
+    <S extends Team> S save(@Valid @NotNull @NonNull S entity);
 }
