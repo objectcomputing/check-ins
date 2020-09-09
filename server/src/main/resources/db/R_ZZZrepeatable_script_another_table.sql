3edccf6977ff0705899bbd60eccf284fcfe06c58
meyerinj <meyerinj@objectcomputing.com>
2020-09-09 18:10:51 -0500

    fixed script I messed with

--- server/src/main/resources/db/R_ZZZrepeatable_script_another_table.sql
+++ server/src/main/resources/db/R_ZZZrepeatable_script_another_table.sql
@@ -2,6 +2,5 @@ drop table if exists repeatable_table_electric_boogaloo;
 CREATE TABLE repeatable_table_electric_boogaloo(
    id varchar PRIMARY KEY,
    repeatableId varchar REFERENCES repeatable_table(id),
-   dependOnVersionedField varchar REFERENCES versioned_table_dependent_on_repeatable_table(id),
    fakeField2 varchar
 );
--- server/src/main/resources/db/V20__create_agenda_table.sql
+++ server/src/main/resources/db/V20__create_agenda_table.sql
@@ -2,6 +2,6 @@ drop table if exists agenda_items;
 CREATE TABLE agenda_items(
    id varchar PRIMARY KEY,
    checkinId varchar REFERENCES checkins(id),
-   createdById varchar REFERENCES member_profile(id),
+   createdById varchar REFERENCES member_profile(uuid),
    description varchar
 );
