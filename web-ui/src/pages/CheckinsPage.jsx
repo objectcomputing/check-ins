96cc257cd72e1910070bdf97d041617105daa36d
Holly Williams <williamsh@objectcomputing.com>
2020-09-10 15:49:42 -0500

    added snapshot test

--- web-ui/src/pages/CheckinsPage.jsx
+++ web-ui/src/pages/CheckinsPage.jsx
@@ -6,7 +6,6 @@ import Modal from "../components/modal/Modal";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import GuidesPanel from "../components/guides/GuidesPanel";
 import Note from "../components/notes/Note";
 import {AppContext} from "../context/AppContext";
@@ -31,46 +30,42 @@ const CheckinsPage = () => {
                     spacing={3}
                 >
                     <Grid item sm={9} justify="center">
-                        {/*<Paper>*/}
-                            <Container maxWidth="md">
-                                <div className="contents">
-                                    <CheckinsHistory setIndex={setIndex}/>
-                                </div>
-                                <CheckinDocs/>
-                                <div className="modal-container">
-                                    <Modal close={showModal} show={show}>
-                                        The checkin will no longer be able to be edited. Are you sure that you
-                                        are ready to close this check-in?
-                                    </Modal>
-                                    <Button
-                                        style={{
-                                            backgroundColor: "#3f51b5",
-                                            color: "white",
-                                            display: show ? "none" : "",
-                                        }}
-                                        onClick={() => showModal()}
-                                    >
-                                        Submit
-                                    </Button>
-                                </div>
-                            </Container>
-                        {/*</Paper>*/}
+                        <Container maxWidth="md">
+                            <div className="contents">
+                                <CheckinsHistory setIndex={setIndex}/>
+                            </div>
+                            <CheckinDocs/>
+                            <div className="modal-container">
+                                <Modal close={showModal} show={show}>
+                                    The checkin will no longer be able to be edited. Are you sure that you
+                                    are ready to close this check-in?
+                                </Modal>
+                                <Button
+                                    style={{
+                                        backgroundColor: "#3f51b5",
+                                        color: "white",
+                                        display: show ? "none" : "",
+                                    }}
+                                    onClick={() => showModal()}
+                                >
+                                    Submit
+                                </Button>
+                            </div>
+                        </Container>
                     </Grid>
                     <Grid item sm={3} justify="flex-end">
-                        {/*<Paper>*/}
-                            <Container maxWidth="md">
-                                <div className="right-sidebar">
-                                    <Personnel/>
-                                    <GuidesPanel/>
-                                </div>
-                                {checkin && checkin.id && (
-                                    <Note
-                                        checkin={checkin}
-                                        memberName={userProfile.name}
-                                    />
-                                )}
-                            </Container>
-                        {/*</Paper>*/}
+                        <Container maxWidth="md">
+                            <div className="right-sidebar">
+                                <Personnel/>
+                                <GuidesPanel/>
+                            </div>
+                            {checkin && checkin.id && (
+                                <Note
+                                    checkin={checkin}
+                                    memberName={userProfile.name}
+                                />
+                            )}
+                        </Container>
                     </Grid>
                 </Grid>
             </Container>
