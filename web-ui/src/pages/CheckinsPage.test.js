3e90bdd1ab96fc508228d3b6145f344b3d70e056
meyerinj <meyerinj@objectcomputing.com>
2020-09-21 15:54:43 -0500

    undid some unrelated changes and deleted the project properties that is not used in the gradle sonarqube plugin

--- web-ui/src/pages/CheckinsPage.test.js
+++ web-ui/src/pages/CheckinsPage.test.js
@@ -1,3 +1,11 @@
 import React from "react";
 import CheckinsPage from "./CheckinsPage";
 import { AppContextProvider } from "../context/AppContext";
+
+it("renders correctly", () => {
+    <AppContextProvider>
+        snapshot(
+        <CheckinsPage />
+        );
+    </AppContextProvider>;
+});
