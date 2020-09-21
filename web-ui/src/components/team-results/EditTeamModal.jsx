9641edd1ef7e6f6843c131c0d94dd9754ee1ef27
Michael Kimberlin <mkimberlin@gmail.com>
2020-09-21 11:34:42 -0500

    Initial Team page and supporting components

---
+++ web-ui/src/components/team-results/EditTeamModal.jsx
@@ -0,0 +1,99 @@
+import React, { useContext, useState } from 'react';
+import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import './EditTeamModal.css';
+import { Button } from '@material-ui/core';
+import { AppContext } from '../../context/AppContext';
+
+const EditTeamModal = ({ team = {}, open, onSave, onClose }) => {
+    const { state } = useContext(AppContext);
+    const { memberProfiles } = state;
+    const [editedTeam, setTeam] = useState(team);
+
+    const teamMemberOptions = memberProfiles;
+
+    const onLeadsChange = (event, newValue) => {
+        setTeam({
+            ...editedTeam,
+            teamLeads: newValue
+        });
+    };
+
+    const onTeamMembersChange = (event, newValue) => {
+        setTeam({
+            ...editedTeam,
+            teamMembers: newValue
+        });
+    };
+
+    return (
+        <Modal
+            open={open}
+            onClose={onClose}
+            aria-labelledby="edit-team-modal-title"
+        >
+            <div className="EditTeamModal">
+                <h2>Add a team</h2>
+                <TextField
+                    id="team-name-input"
+                    label="Team Name"
+                    required
+                    className="halfWidth"
+                    placeholder="Awesome Team"
+                    value={editedTeam.name ? editedTeam.name : ""}
+                    onChange={(e) => setTeam({...editedTeam, name: e.target.value})}
+                />
+                <TextField
+                    label="Description"
+                    className="fullWidth"
+                    placeholder="What do they do?"
+                    value={editedTeam.description ? editedTeam.description : ""}
+                    onChange={(e) => setTeam({...editedTeam, description: e.target.value})}
+                />
+                <Autocomplete
+                    multiple
+                    options={teamMemberOptions}
+                    value={editedTeam.teamLeads ? editedTeam.teamLeads : []}
+                    onChange={onLeadsChange}
+                    getOptionLabel={(option) => option.name}
+                    getOptionSelected={(option, value) => value ? value.id === option.id : false}
+                    renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        className="fullWidth"
+                        label="Team Leads"
+                        placeholder="Add a team lead..."
+                    />
+                    )}
+                />
+                <Autocomplete
+                    multiple
+                    options={teamMemberOptions}
+                    value={editedTeam.teamMembers ? editedTeam.teamMembers : []}
+                    onChange={onTeamMembersChange}
+                    getOptionLabel={(option) => option.name}
+                    getOptionSelected={(option, value) => value ? value.id === option.id : false}
+                    renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        className="fullWidth"
+                        label="Team Members"
+                        placeholder="Add a team member..."
+                    />
+                    )}
+                />
+                <div className="EditTeamModal-actions fullWidth">
+                    <Button onClick={onClose} color="secondary">
+                        Cancel
+                    </Button>
+                    <Button onClick={() => onSave(editedTeam)} color="primary">
+                        Save Team
+                    </Button>
+                </div>
+            </div>
+        </Modal>
+    );
+};
+
+export default EditTeamModal;
---
+++ web-ui/src/components/team-results/TeamSummaryCard.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Card, CardActions, CardContent, CardHeader } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+import { getMembersByTeam } from '../../api/team';
+import { AppContext } from '../../context/AppContext';
+
+const propTypes = {
+    team: PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string,
+        description: PropTypes.string
+    })
+}
+
+const displayName = "TeamSummaryCard";
+
+const TeamSummaryCard = ({ team }) => {
+    const {state} = useContext(AppContext);
+    const teamMembers = AppContext.selectMemberProfilesByTeamId(state)(team.id);
+
+    console.log(JSON.stringify(teamMembers));
+
+    let leads = teamMembers == null ? null : teamMembers.filter((teamMember) => teamMember.lead);
+    let nonLeads = teamMembers == null ? null : teamMembers.filter((teamMember) => !teamMember.lead);
+
+    return (
+        <Card>
+            <CardHeader title={team.name} subheader={team.description} />
+            <CardContent>
+                {
+                    teamMembers == null ?
+                        <React.Fragment>
+                            <Skeleton />
+                            <Skeleton />
+                        </React.Fragment> :
+                        <React.Fragment>
+                            <strong>Team Leads: </strong>
+                            {
+                                leads.map((lead, index) => {
+                                    return index !== leads.length - 1 ? `${lead.name}, ` : lead.name
+                                })
+                            }
+                            <br />
+                            <strong>Team Members: </strong>
+                            {
+                                nonLeads.map((member, index) => {
+                                    return index !== nonLeads.length - 1 ? `${member.name}, ` : member.name
+                                })
+                            }
+                        </React.Fragment>
+                }
+            </CardContent>
+            <CardActions>
+                <Button>Edit Team</Button>
+                <Button>Delete Team</Button>
+            </CardActions>
+        </Card>
+    );
+};
+
+TeamSummaryCard.displayName = displayName;
+TeamSummaryCard.propTypes = propTypes;
+
+export default TeamSummaryCard;
