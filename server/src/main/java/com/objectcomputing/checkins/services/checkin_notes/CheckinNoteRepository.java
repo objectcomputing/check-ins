3137415fc1d98a4cd3bf26c2f8e56a6e6266953f
Mohit Bhatia <bhatiam@objectcomputing.com>
2020-09-24 22:55:28 -0500

    changed notes to use query

--- server/src/main/java/com/objectcomputing/checkins/services/checkin_notes/CheckinNoteRepository.java
+++ server/src/main/java/com/objectcomputing/checkins/services/checkin_notes/CheckinNoteRepository.java
@@ -1,21 +1,27 @@
 package com.objectcomputing.checkins.services.checkin_notes;
 
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface CheckinNoteRepository extends CrudRepository<CheckinNote, UUID> {
-    List<CheckinNote> findByCheckinid(UUID checkinid);
 
-    List<CheckinNote> findByCreatedbyid(UUID createbyid);
+    @Query(" SELECT * " +
+            "FROM checkin_notes cn " +
+            "WHERE (:checkinid  IS NULL OR cn.checkinId= :checkinid) " +
+            "AND (:createdById  IS NULL OR cn.createdByid= :createdById) ")
+    Set<CheckinNote> search(@Nullable String checkinid, @Nullable String createdById);
 
     @Override
     <S extends CheckinNote> List<S> saveAll(@Valid @NotNull Iterable<S> entities);
--- server/src/main/java/com/objectcomputing/checkins/services/checkin_notes/CheckinNoteServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/checkin_notes/CheckinNoteServicesImpl.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+import static com.objectcomputing.checkins.util.Util.nullSafeUUIDToString;
+
 @Singleton
 public class CheckinNoteServicesImpl implements CheckinNoteServices {
 
@@ -41,22 +43,22 @@ public class CheckinNoteServicesImpl implements CheckinNoteServices {
     @Override
     public CheckinNote save(CheckinNote checkinNote) {
         CheckinNote checkinNoteRet = null;
-        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
-        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
-        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
+        String workEmail = securityService != null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail != null ? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService != null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
 
         if (checkinNote != null) {
             final UUID checkinId = checkinNote.getCheckinid();
             final UUID createById = checkinNote.getCreatedbyid();
             CheckIn checkinRecord = checkinRepo.findById(checkinId).orElse(null);
-            Boolean isCompleted  = checkinRecord!=null?checkinRecord.isCompleted():null;
-            final UUID pdlId = checkinRecord!=null?checkinRecord.getPdlId():null;
-            validate(checkinId==null||createById==null, "Invalid checkin note %s", checkinNote);
-            validate(checkinNote.getId() != null,"Found unexpected id %s for check in note",checkinNote.getId());
-            validate(checkinRepo.findById(checkinId).isEmpty(),"CheckIn %s doesn't exist",checkinId);
-            validate(memberRepo.findById(createById).isEmpty(),"Member %s doesn't exist",createById);
-            if(!isAdmin&&isCompleted) {
-                validate(!currentUser.getId().equals(pdlId),"User is unauthorized to do this operation");
+            Boolean isCompleted = checkinRecord != null ? checkinRecord.isCompleted() : null;
+            final UUID pdlId = checkinRecord != null ? checkinRecord.getPdlId() : null;
+            validate(checkinId == null || createById == null, "Invalid checkin note %s", checkinNote);
+            validate(checkinNote.getId() != null, "Found unexpected id %s for check in note", checkinNote.getId());
+            validate(checkinRepo.findById(checkinId).isEmpty(), "CheckIn %s doesn't exist", checkinId);
+            validate(memberRepo.findById(createById).isEmpty(), "Member %s doesn't exist", createById);
+            if (!isAdmin && isCompleted) {
+                validate(!currentUser.getId().equals(pdlId), "User is unauthorized to do this operation");
             }
 
             checkinNoteRet = checkinNoteRepository.save(checkinNote);
@@ -66,16 +68,16 @@ public class CheckinNoteServicesImpl implements CheckinNoteServices {
 
     @Override
     public CheckinNote read(@NotNull UUID id) {
-        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
-        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
-        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
-        CheckinNote checkInNoteResult =  checkinNoteRepository.findById(id).orElse(null);
+        String workEmail = securityService != null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail != null ? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService != null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
+        CheckinNote checkInNoteResult = checkinNoteRepository.findById(id).orElse(null);
         validate(checkInNoteResult == null, "Invalid checkin note id %s", id);
-        if(!isAdmin) {
+        if (!isAdmin) {
             CheckIn checkinRecord = checkinRepo.findById(checkInNoteResult.getCheckinid()).orElse(null);
-            final UUID pdlId = checkinRecord!=null?checkinRecord.getPdlId():null;
-            final UUID createById = checkinRecord!=null?checkinRecord.getTeamMemberId():null;
-            validate(!currentUser.getId().equals(pdlId)&&!currentUser.getId().equals(createById),"User is unauthorized to do this operation");
+            final UUID pdlId = checkinRecord != null ? checkinRecord.getPdlId() : null;
+            final UUID createById = checkinRecord != null ? checkinRecord.getTeamMemberId() : null;
+            validate(!currentUser.getId().equals(pdlId) && !currentUser.getId().equals(createById), "User is unauthorized to do this operation");
         }
 
         return checkInNoteResult;
@@ -85,23 +87,23 @@ public class CheckinNoteServicesImpl implements CheckinNoteServices {
     @Override
     public CheckinNote update(CheckinNote checkinNote) {
         CheckinNote checkinNoteRet = null;
-        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
-        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
-        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
+        String workEmail = securityService != null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail != null ? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService != null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
 
         if (checkinNote != null) {
             final UUID id = checkinNote.getId();
             final UUID checkinId = checkinNote.getCheckinid();
             final UUID createById = checkinNote.getCreatedbyid();
             CheckIn checkinRecord = checkinRepo.findById(checkinId).orElse(null);
-            Boolean isCompleted  = checkinRecord!=null ?checkinRecord.isCompleted():null;
-            final UUID pdlId = checkinRecord!=null?checkinRecord.getPdlId():null;
-            validate(checkinId==null||createById==null, "Invalid checkin note %s", checkinNote);
-            validate(id == null || checkinNoteRepository.findById(id).isEmpty(),"Unable to locate checkin note to update with id %s",checkinNote.getId());
-            validate(checkinRepo.findById(checkinId).isEmpty(),"CheckIn %s doesn't exist",checkinId);
-            validate(memberRepo.findById(createById).isEmpty(),"Member %s doesn't exist",createById);
-            if(!isAdmin&&isCompleted) {
-                validate(!currentUser.getId().equals(pdlId),"User is unauthorized to do this operation");
+            Boolean isCompleted = checkinRecord != null ? checkinRecord.isCompleted() : null;
+            final UUID pdlId = checkinRecord != null ? checkinRecord.getPdlId() : null;
+            validate(checkinId == null || createById == null, "Invalid checkin note %s", checkinNote);
+            validate(id == null || checkinNoteRepository.findById(id).isEmpty(), "Unable to locate checkin note to update with id %s", checkinNote.getId());
+            validate(checkinRepo.findById(checkinId).isEmpty(), "CheckIn %s doesn't exist", checkinId);
+            validate(memberRepo.findById(createById).isEmpty(), "Member %s doesn't exist", createById);
+            if (!isAdmin && isCompleted) {
+                validate(!currentUser.getId().equals(pdlId), "User is unauthorized to do this operation");
             }
 
             checkinNoteRet = checkinNoteRepository.update(checkinNote);
@@ -112,33 +114,29 @@ public class CheckinNoteServicesImpl implements CheckinNoteServices {
 
     @Override
     public Set<CheckinNote> findByFields(UUID checkinid, UUID createbyid) {
-        Set<CheckinNote> checkinNote = new HashSet<>();
-        String workEmail = securityService!=null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
-        MemberProfile currentUser = workEmail!=null? currentUserServices.findOrSaveUser(null, workEmail) : null;
-        Boolean isAdmin = securityService!=null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
+        String workEmail = securityService != null ? securityService.getAuthentication().get().getAttributes().get("email").toString() : null;
+        MemberProfile currentUser = workEmail != null ? currentUserServices.findOrSaveUser(null, workEmail) : null;
+        Boolean isAdmin = securityService != null ? securityService.hasRole(RoleType.Constants.ADMIN_ROLE) : false;
 
-        checkinNoteRepository.findAll().forEach(checkinNote::add);
+        if (checkinid != null) {
+            CheckIn checkinRecord = checkinRepo.findById(checkinid).orElse(null);
+            final UUID pdlId = checkinRecord != null ? checkinRecord.getPdlId() : null;
+            final UUID teamMemberId = checkinRecord != null ? checkinRecord.getTeamMemberId() : null;
+            validate(!currentUser.getId().equals(pdlId) && !currentUser.getId().equals(teamMemberId) && !isAdmin, "User is unauthorized to do this operation");
+        } else if (createbyid != null) {
+            MemberProfile memberRecord = memberRepo.findById(createbyid).orElse(null);
+            validate(!currentUser.getId().equals(memberRecord.getId()) && !isAdmin, "User is unauthorized to do this operation");
+        } else {
+            validate(!isAdmin, "User is unauthorized to do this operation");
+        }
 
-            if(checkinid!=null) {
-                CheckIn checkinRecord = checkinRepo.findById(checkinid).orElse(null);
-                final UUID pdlId = checkinRecord!=null?checkinRecord.getPdlId():null;
-                final UUID teamMemberId = checkinRecord!=null?checkinRecord.getTeamMemberId():null;
-                validate(!currentUser.getId().equals(pdlId)&&!currentUser.getId().equals(teamMemberId)&&!isAdmin,"User is unauthorized to do this operation");
-                checkinNote.retainAll(checkinNoteRepository.findByCheckinid(checkinid));
+        Set<CheckinNote> checkinNote = new HashSet<>(checkinNoteRepository.search(nullSafeUUIDToString(checkinid), nullSafeUUIDToString(createbyid)));
 
-            } else if(createbyid!=null) {
-                MemberProfile memberRecord = memberRepo.findById(createbyid).orElse(null);
-                validate(!currentUser.getId().equals(memberRecord.getId())&&!isAdmin,"User is unauthorized to do this operation");
-                checkinNote.retainAll(checkinNoteRepository.findByCreatedbyid(createbyid));
-
-            } else {
-                validate(!isAdmin,"User is unauthorized to do this operation");
-            }
         return checkinNote;
     }
 
     private void validate(@NotNull boolean isError, @NotNull String message, Object... args) {
-        if(isError) {
+        if (isError) {
             throw new CheckinNotesBadArgException(String.format(message, args));
         }
     }
