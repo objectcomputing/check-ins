d3263c006cbc237004c37f0b621a83290d9b85af
warnerj <warnerj@objectcomputing.com>
2020-10-05 14:14:52 -0500

    change agendaItem controller to use non-blocking interactions with the database

--- server/src/main/java/com/objectcomputing/checkins/services/agenda_item/AgendaItemController.java
+++ server/src/main/java/com/objectcomputing/checkins/services/agenda_item/AgendaItemController.java
@@ -7,12 +7,19 @@ import io.micronaut.http.annotation.Error;
 import io.micronaut.http.annotation.*;
 import io.micronaut.http.hateoas.JsonError;
 import io.micronaut.http.hateoas.Link;
+import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import io.netty.channel.EventLoopGroup;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
@@ -20,15 +27,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
 
 @Controller("/services/agenda-item")
 @Secured(SecurityRule.IS_AUTHENTICATED)
 @Produces(MediaType.APPLICATION_JSON)
 @Tag(name = "agenda-item")
 public class AgendaItemController {
+    private static final Logger LOG = LoggerFactory.getLogger(AgendaItemController.class);
 
     @Inject
     private AgendaItemServices agendaItemServices;
+
+    private EventLoopGroup eventLoopGroup;
+    private ExecutorService ioExecutorService;
+
+    public AgendaItemController(AgendaItemServices agendaItemServices,
+                                EventLoopGroup eventLoopGroup,
+                                @Named(TaskExecutors.IO) ExecutorService ioExecutorService) {
+        this.agendaItemServices = agendaItemServices;
+        this.eventLoopGroup = eventLoopGroup;
+        this.ioExecutorService = ioExecutorService;
+    }
 
     @Error(exception = AgendaItemBadArgException.class)
     public HttpResponse<?> handleBadArgs(HttpRequest<?> request, AgendaItemBadArgException e) {
@@ -46,14 +66,21 @@ public class AgendaItemController {
      * @return {@link HttpResponse <AgendaItem>}
      */
     @Post()
-    public HttpResponse<AgendaItem> createAgendaItem(@Body @Valid AgendaItemCreateDTO agendaItem,
-                                                     HttpRequest<AgendaItemCreateDTO> request) {
-        AgendaItem newAgendaItem = agendaItemServices.save(new AgendaItem(agendaItem.getCheckinid(),
-                agendaItem.getCreatedbyid(), agendaItem.getDescription()));
-        return HttpResponse
-                .created(newAgendaItem)
-                .headers(headers -> headers.location(
-                        URI.create(String.format("%s/%s", request.getPath(), newAgendaItem.getId()))));
+    public Single<HttpResponse<AgendaItem>> createAgendaItem(@Body @Valid AgendaItemCreateDTO agendaItem,
+                                                             HttpRequest<AgendaItemCreateDTO> request) {
+        LOG.info("Entering controller on main event loop");
+        return Single.fromCallable(() -> agendaItemServices.save(new AgendaItem(agendaItem.getCheckinid(),
+                agendaItem.getCreatedbyid(), agendaItem.getDescription())))
+                .observeOn(Schedulers.from(eventLoopGroup))
+                .map(createdAgendaItem -> {
+                    //Using code block rather than lambda so we can log what thread we're in
+                    LOG.info("Back on the main event loop in the controller");
+                    return (HttpResponse<AgendaItem>) HttpResponse
+                            .created(createdAgendaItem)
+                            .headers(headers -> headers.location(
+                                    URI.create(String.format("%s/%s", request.getPath(), createdAgendaItem.getId()))));
+                }).subscribeOn(Schedulers.from(ioExecutorService));
+
     }
 
     /**
@@ -63,13 +90,21 @@ public class AgendaItemController {
      * @return {@link HttpResponse< AgendaItem >}
      */
     @Put()
-    public HttpResponse<?> updateAgendaItem(@Body @Valid AgendaItem agendaItem, HttpRequest<AgendaItem> request) {
-        AgendaItem updatedAgendaItem = agendaItemServices.update(agendaItem);
-        return HttpResponse
-                .ok()
-                .headers(headers -> headers.location(
-                        URI.create(String.format("%s/%s", request.getPath(), updatedAgendaItem.getId()))))
-                .body(updatedAgendaItem);
+    public Single<HttpResponse<AgendaItem>> updateAgendaItem(@Body @Valid AgendaItem agendaItem, HttpRequest<AgendaItem> request) {
+//        AgendaItem updatedAgendaItem = agendaItemServices.update(agendaItem);
+        LOG.info("Entering controller on main event loop");
+        if (agendaItem == null) {
+            return Single.just(HttpResponse.ok());
+        }
+        return Single.fromCallable(() -> agendaItemServices.update(agendaItem))
+                .observeOn(Schedulers.from(eventLoopGroup))
+                .map(updatedAgendaItem ->
+                        (HttpResponse<AgendaItem>) HttpResponse
+                                .ok()
+                                .headers(headers -> headers.location(
+                                        URI.create(String.format("%s/%s", request.getPath(), updatedAgendaItem.getId()))))
+                                .body(updatedAgendaItem))
+                .subscribeOn(Schedulers.from(ioExecutorService));
 
     }
 
@@ -92,8 +127,20 @@ public class AgendaItemController {
      * @return {@link AgendaItem}
      */
     @Get("/{id}")
-    public AgendaItem readAgendaItem(UUID id) {
-        return agendaItemServices.read(id);
+    public Single<HttpResponse<AgendaItem>> readAgendaItem(UUID id) {
+        LOG.info("Entering controller on main event loop");
+        return Single.fromCallable(() -> {
+            AgendaItem result = agendaItemServices.read(id);
+            if (result == null) {
+                throw new AgendaItemNotFoundException("No agenda item for UUID");
+            }
+            return result;
+        })
+        .observeOn(Schedulers.from(eventLoopGroup))
+        .map(agendaItem -> {
+            LOG.info("Successful find");
+            return (HttpResponse<AgendaItem>)HttpResponse.ok(agendaItem);
+        }).subscribeOn(Schedulers.from(ioExecutorService));
     }
 
     /**
@@ -104,9 +151,15 @@ public class AgendaItemController {
      * @return {@link List < CheckIn > list of checkins}
      */
     @Get("/{?checkinid,createdbyid}")
-    public Set<AgendaItem> findAgendaItems(@Nullable UUID checkinid,
-                                           @Nullable UUID createdbyid) {
-        return agendaItemServices.findByFields(checkinid, createdbyid);
+    public Single<HttpResponse<Set<AgendaItem>>> findAgendaItems(@Nullable UUID checkinid,
+                                                                 @Nullable UUID createdbyid) {
+        LOG.info("Entering controller on main event loop");
+        return Single.fromCallable(() -> agendaItemServices.findByFields(checkinid, createdbyid))
+                .observeOn(Schedulers.from(eventLoopGroup))
+                .map(agendaItems -> {
+                    LOG.info("Mapping on main event loop");
+                    return (HttpResponse<Set<AgendaItem>>) HttpResponse.ok(agendaItems);
+                }).subscribeOn(Schedulers.from(ioExecutorService));
     }
 
     /**
@@ -116,28 +169,31 @@ public class AgendaItemController {
      * @return {@link HttpResponse<List< AgendaItem >}
      */
     @Post("/items")
-    public HttpResponse<?> loadAgendaItems(@Body @Valid @NotNull List<AgendaItemCreateDTO> agendaItems,
-                                           HttpRequest<List<AgendaItem>> request) {
-        List<String> errors = new ArrayList<>();
-        List<AgendaItem> agendaItemsCreated = new ArrayList<>();
-        for (AgendaItemCreateDTO agendaItemDTO : agendaItems) {
-            AgendaItem agendaItem = new AgendaItem(agendaItemDTO.getCheckinid(),
-                    agendaItemDTO.getCreatedbyid(), agendaItemDTO.getDescription());
-            try {
-                agendaItemServices.save(agendaItem);
-                agendaItemsCreated.add(agendaItem);
-            } catch (AgendaItemBadArgException e) {
-                errors.add(String.format("Member %s's agenda item was not added to CheckIn %s because: %s", agendaItem.getCreatedbyid(),
-                        agendaItem.getCheckinid(), e.getMessage()));
+    public Single<HttpResponse<?>> loadAgendaItems(@Body @Valid @NotNull List<AgendaItemCreateDTO> agendaItems,
+                                                   HttpRequest<List<AgendaItem>> request) {
+        return Single.fromCallable(() -> {
+            List<String> errors = new ArrayList<>();
+            List<AgendaItem> agendaItemsCreated = new ArrayList<>();
+            for (AgendaItemCreateDTO agendaItemDTO : agendaItems) {
+                AgendaItem agendaItem = new AgendaItem(agendaItemDTO.getCheckinid(),
+                        agendaItemDTO.getCreatedbyid(), agendaItemDTO.getDescription());
+                try {
+                    agendaItemServices.save(agendaItem);
+                    agendaItemsCreated.add(agendaItem);
+                } catch (AgendaItemBadArgException e) {
+                    errors.add(String.format("Member %s's agenda item was not added to CheckIn %s because: %s", agendaItem.getCreatedbyid(),
+                            agendaItem.getCheckinid(), e.getMessage()));
+                }
             }
-        }
-        if (errors.isEmpty()) {
-            return HttpResponse.created(agendaItemsCreated)
-                    .headers(headers -> headers.location(request.getUri()));
-        } else {
-            return HttpResponse.badRequest(errors)
-                    .headers(headers -> headers.location(request.getUri()));
-        }
+            if (errors.isEmpty()) {
+                return agendaItemsCreated;
+            }else {
+                return HttpResponse.badRequest(errors).headers(headers ->
+                        headers.location(request.getUri()));
+            }
+//            throw new AgendaItemsBulkLoadException(errors);
+        }).map(agendaItemsCreated -> HttpResponse.created(agendaItemsCreated)
+                .headers(headers -> headers.location(request.getUri())));
     }
 
 
