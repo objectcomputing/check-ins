d0a3d3bd36be931c751cbe93a73b7a89627adbe1
Zack <zack@jeffandbetsy.net>
2020-10-05 10:55:14 -0500

    Added Avatar component to storybook

---
+++ web-ui/src/components/avatar/Avatar.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Avatar from './Avatar';
+import image from '../../logo.svg';
+
+export default {
+    title: 'Avatar',
+    component: Avatar
+};
+
+const Template = (args) => 
+    <Avatar {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+    loggedIn:  true,
+    profile: {image_url: image}
+};
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+    loggedIn:  false
+};
