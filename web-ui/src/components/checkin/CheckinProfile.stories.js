11eff65d2c4aed519afadc12350b814f0b4145bc
Zack <zack@jeffandbetsy.net>
2020-10-09 17:51:30 -0500

    Added Checkin Profile component to storybook

---
+++ web-ui/src/components/checkin/CheckinProfile.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import CheckinProfile from './CheckinProfile';
+
+export default {
+    title: 'CheckinProfile',
+    component: CheckinProfile
+};
+
+const Template = (args) =>
+    <CheckinProfile {...args} />;
+
+export const Profile = Template.bind({});
+Profile.args = {
+    state: {
+        userProfile: {
+            name: "Test User",
+            imageUrl: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Finsertcoin%2Ffiles%2F2017%2F02%2Flego-batman1.jpg",
+            memberProfile: {
+                pdlID: 123,
+                workEmail: "testuser@objectcomputing.com",
+                role: "Software Engineer",
+            }
+        }
+    }
+};
+
+export const NoProfile = Template.bind({});
+NoProfile.args = {
+
+}
+
