4f8531942c631a51996705807c1520a7db5ab140
Suman Maroju <sumanmaroju2020@u.northwestern.edu>
2020-10-12 02:52:43 -0500

    update for PR comments

--- server/src/main/java/com/objectcomputing/checkins/services/checkindocument/CheckinDocumentController.java
+++ server/src/main/java/com/objectcomputing/checkins/services/checkindocument/CheckinDocumentController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import javax.annotation.Nullable;
+import javax.inject.Named;
 import javax.validation.Valid;
 
 import com.objectcomputing.checkins.services.role.RoleType;
@@ -28,25 +29,26 @@ import java.util.concurrent.ExecutorService;
 import io.netty.channel.EventLoopGroup;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
+import io.micronaut.scheduling.TaskExecutors;
 
 @Controller("/services/checkin-document")
 @Secured({RoleType.Constants.ADMIN_ROLE, RoleType.Constants.PDL_ROLE})
 @Produces(MediaType.APPLICATION_JSON)
 @Tag(name = "checkin document")
 
-    public class CheckinDocumentController {
+public class CheckinDocumentController {
         
-        private final CheckinDocumentServices checkinDocumentService;
-        private final EventLoopGroup eventLoopGroup;
-        private final ExecutorService ioExecutorService;
-    
-        public CheckinDocumentController(CheckinDocumentServices checkinDocumentService,
-                                       EventLoopGroup eventLoopGroup,
-                                       ExecutorService ioExecutorService){
-            this.checkinDocumentService = checkinDocumentService;
-            this.eventLoopGroup = eventLoopGroup;
-            this.ioExecutorService = ioExecutorService;
-        }
+    private final CheckinDocumentServices checkinDocumentService;
+    private final EventLoopGroup eventLoopGroup;
+    private final ExecutorService ioExecutorService;
+
+    public CheckinDocumentController(CheckinDocumentServices checkinDocumentService,
+                                    EventLoopGroup eventLoopGroup,
+                                    @Named(TaskExecutors.IO) ExecutorService ioExecutorService){
+        this.checkinDocumentService = checkinDocumentService;
+        this.eventLoopGroup = eventLoopGroup;
+        this.ioExecutorService = ioExecutorService;
+    }
     
     @Error(exception = CheckinDocumentBadArgException.class)
     public HttpResponse<?> handleBadArgs(HttpRequest<?> request, CheckinDocumentBadArgException e) {
@@ -68,8 +70,7 @@ import io.reactivex.schedulers.Schedulers;
     public Single<HttpResponse<Set<CheckinDocument>>> findCheckinDocument(@Nullable UUID checkinsId) {
         return Single.fromCallable(() -> checkinDocumentService.read(checkinsId))
                 .observeOn(Schedulers.from(eventLoopGroup))
-                .map(checkinDocuments -> {
-                    return (HttpResponse<Set<CheckinDocument>>) HttpResponse.ok(checkinDocuments);
+                .map(checkinDocuments -> {return (HttpResponse<Set<CheckinDocument>>) HttpResponse.ok(checkinDocuments);
                 }).subscribeOn(Schedulers.from(ioExecutorService));
     }
 
@@ -85,12 +86,9 @@ import io.reactivex.schedulers.Schedulers;
                                                                     HttpRequest<CheckinDocumentCreateDTO> request) {
         return Single.fromCallable(() -> checkinDocumentService.save(new CheckinDocument(checkinDocument.getCheckinsId(),checkinDocument.getUploadDocId())))
                 .observeOn(Schedulers.from(eventLoopGroup))
-                .map(createdCheckinDocument -> {
-                    //Using code block rather than lambda so we can log what thread we're in
-                    return (HttpResponse<CheckinDocument>) HttpResponse
+                .map(createdCheckinDocument -> {return (HttpResponse<CheckinDocument>) HttpResponse
                     .created(createdCheckinDocument)
-                    .headers(headers -> headers.location(
-                        URI.create(String.format("%s/%s", request.getPath(), createdCheckinDocument.getId()))));
+                    .headers(headers -> headers.location(URI.create(String.format("%s/%s", request.getPath(), createdCheckinDocument.getId()))));
                 }).subscribeOn(Schedulers.from(ioExecutorService));
     }
 
@@ -108,11 +106,9 @@ import io.reactivex.schedulers.Schedulers;
         }
         return Single.fromCallable(() -> checkinDocumentService.update(checkinDocument))
             .observeOn(Schedulers.from(eventLoopGroup))
-            .map(updatedCheckinDocument -> //This lambda expression is the preferred way to do this kind of simple mapping.
-                    (HttpResponse<CheckinDocument>) HttpResponse
+            .map(updatedCheckinDocument -> (HttpResponse<CheckinDocument>) HttpResponse
                     .ok()
-                    .headers(headers -> headers.location(
-                            URI.create(String.format("%s/%s", request.getPath(), updatedCheckinDocument.getId()))))
+                    .headers(headers -> headers.location(URI.create(String.format("%s/%s", request.getPath(), updatedCheckinDocument.getId()))))
                     .body(updatedCheckinDocument))
             .subscribeOn(Schedulers.from(ioExecutorService));
 
