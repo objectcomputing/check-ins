1e80b018db3601b2e520b761d8beff84979a77ea
Suman Maroju <sumanmaroju2020@u.northwestern.edu>
2020-10-29 02:56:59 -0500

    checkin non blocking

--- server/src/main/java/com/objectcomputing/checkins/services/checkins/CheckInController.java
+++ server/src/main/java/com/objectcomputing/checkins/services/checkins/CheckInController.java
@@ -5,8 +5,8 @@ import java.util.Set;
 import java.util.UUID;
 
 import javax.annotation.Nullable;
-import javax.inject.Singleton;
 import javax.validation.Valid;
+import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 
 import io.micronaut.http.HttpRequest;
@@ -21,13 +21,18 @@ import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Put;
 import io.micronaut.http.hateoas.JsonError;
 import io.micronaut.http.hateoas.Link;
-import io.micronaut.security.authentication.Authentication;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 
 import io.micronaut.http.annotation.Error;
+
+import java.util.concurrent.ExecutorService;
+import io.netty.channel.EventLoopGroup;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+import io.micronaut.scheduling.TaskExecutors;
 
 @Controller("/services/check-in")
 @Secured(SecurityRule.IS_AUTHENTICATED)
@@ -36,12 +41,18 @@ import io.micronaut.http.annotation.Error;
 @Tag(name="check-ins")
 public class CheckInController {
 
-    private CheckInServices checkInServices;
+    private final CheckInServices checkInServices;
+    private final EventLoopGroup eventLoopGroup;
+    private final ExecutorService ioExecutorService;
 
-    public CheckInController(CheckInServices checkInServices) {
+        public CheckInController(CheckInServices checkInServices,
+                                    EventLoopGroup eventLoopGroup,
+                                    @Named(TaskExecutors.IO) ExecutorService ioExecutorService){
         this.checkInServices = checkInServices;
+        this.eventLoopGroup = eventLoopGroup;
+        this.ioExecutorService = ioExecutorService;
     }
-
+    
     @Error(exception = CheckInBadArgException.class)
     public HttpResponse<?> handleBadArgs(HttpRequest<?> request, CheckInBadArgException e) {
         JsonError error = new JsonError(e.getMessage())
@@ -58,36 +69,51 @@ public class CheckInController {
      * @return
      */
     @Get("/{?teamMemberId,pdlId,completed}")
-    public Set<CheckIn> findByValue(@Nullable UUID teamMemberId, @Nullable UUID  pdlId, @Nullable Boolean completed) {
-        return checkInServices.findByFields(teamMemberId, pdlId, completed);
+    public Single<HttpResponse<Set<CheckIn>>> findCheckIns(@Nullable UUID teamMemberId, @Nullable UUID  pdlId, @Nullable Boolean completed) {
+        return Single.fromCallable(() -> checkInServices.findByFields(teamMemberId, pdlId, completed))
+                .observeOn(Schedulers.from(eventLoopGroup))
+                .map(createdCheckIn -> (HttpResponse<Set<CheckIn>>) HttpResponse.ok(createdCheckIn))
+                .subscribeOn(Schedulers.from(ioExecutorService));
     }
 
     /**
-     * Save check-in details.
-     * @param checkIn
-     * @return
+     * Create and save a new CheckIn.
+     *
+     * @param checkIn, {@link CheckInCreateDTO}
+     * @return {@link HttpResponse<CheckIn>}
      */
-    @Post()
-    public HttpResponse<CheckIn> createCheckIn(@Body @Valid CheckInCreateDTO checkIn, HttpRequest<CheckInCreateDTO> request) {
 
-        CheckIn newMemberCheckIn = checkInServices.save(new CheckIn(checkIn.getTeamMemberId(), checkIn.getPdlId(), checkIn.getCheckInDate(), checkIn.isCompleted()));
-        return HttpResponse.created(newMemberCheckIn)
-                .headers(headers -> headers.location(URI.create(String.format("%s/%s", request.getPath(), newMemberCheckIn.getId()))));
+    @Post("/")
+    public Single<HttpResponse<CheckIn>> createCheckIn(@Body @Valid CheckInCreateDTO checkIn,
+                                                                    HttpRequest<CheckInCreateDTO> request) {
+        return Single.fromCallable(() -> checkInServices.save(new CheckIn(checkIn.getTeamMemberId(), checkIn.getPdlId(), checkIn.getCheckInDate(), checkIn.isCompleted())))
+                .observeOn(Schedulers.from(eventLoopGroup))
+                .map(createdCheckIn -> {return (HttpResponse<CheckIn>) HttpResponse
+                    .created(createdCheckIn)
+                    .headers(headers -> headers.location(URI.create(String.format("%s/%s", request.getPath(), createdCheckIn.getId()))));
+                }).subscribeOn(Schedulers.from(ioExecutorService));
     }
 
     /**
-     * Update check in details
-     * @param checkIn
-     * @return
+     * Update a CheckIn
+     *
+     * @param checkIn, {@link CheckIn}
+     * @return {@link HttpResponse<CheckIn>}
      */
     @Put("/")
-    public HttpResponse<?> update(@Body @Valid CheckIn checkIn, HttpRequest<CheckInCreateDTO> request) {
+    public Single<HttpResponse<CheckIn>> update(@Body @Valid CheckIn checkIn,
+                                            HttpRequest<CheckIn> request) {
+        if (checkIn == null) {
+            return Single.just(HttpResponse.ok());
+        }
+        return Single.fromCallable(() -> checkInServices.update(checkIn))
+            .observeOn(Schedulers.from(eventLoopGroup))
+            .map(updatedCheckIn -> (HttpResponse<CheckIn>) HttpResponse
+                    .ok()
+                    .headers(headers -> headers.location(URI.create(String.format("%s/%s", request.getPath(), updatedCheckIn.getId()))))
+                    .body(updatedCheckIn))
+            .subscribeOn(Schedulers.from(ioExecutorService));
 
-        CheckIn updatedMemberCheckIn = checkInServices.update(checkIn);
-        return HttpResponse
-                .ok()
-                .headers(headers -> headers.location(URI.create(String.format("%s/%s", request.getPath(), updatedMemberCheckIn.getId()))))
-                .body(updatedMemberCheckIn);
     }
 
     /**
