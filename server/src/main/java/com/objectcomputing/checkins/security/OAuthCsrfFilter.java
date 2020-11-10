7b45b36e6fc89fda1f115839f702eb0bff84bebd
Mohit Bhatia <bhatiam@objectcomputing.com>
2020-11-09 22:33:24 -0600

    removed sysouts

--- server/src/main/java/com/objectcomputing/checkins/security/OAuthCsrfFilter.java
+++ server/src/main/java/com/objectcomputing/checkins/security/OAuthCsrfFilter.java
@@ -3,14 +3,11 @@ package com.objectcomputing.checkins.security;
 import io.micronaut.core.async.publisher.Publishers;
 import io.micronaut.http.*;
 import io.micronaut.http.annotation.Filter;
-import io.micronaut.http.annotation.Header;
 import io.micronaut.http.cookie.Cookie;
 import io.micronaut.http.filter.OncePerRequestHttpServerFilter;
 import io.micronaut.http.filter.ServerFilterChain;
-import io.netty.handler.codec.http.HttpHeaderNames;
 import org.reactivestreams.Publisher;
 
-import java.util.*;
 
 
 @Filter(value = {"/services/*", "/services/*"})
@@ -20,15 +17,13 @@ public class OAuthCsrfFilter extends OncePerRequestHttpServerFilter {
     protected Publisher<MutableHttpResponse<?>> doFilterOnce(HttpRequest<?> request, ServerFilterChain chain) {
         HttpHeaders headers = request.getHeaders();
 
-        String connectValue1 = headers.get("cookie", String.class).orElse("").toLowerCase(Locale.ENGLISH);
-        System.out.println("request paramater is:::"+connectValue1);
+        String headerValue = headers.get("X-CSRF-Header", String.class).orElse("");
         String cookieValue = request.getCookies().findCookie("_csrf").map(Cookie::getValue).orElse(null);
-        System.out.println("request cookie is:::"+cookieValue);
 
 
-//        if (cookieValue == null || !cookieValue.equals(requestParameter)) {
-//            return Publishers.just(HttpResponse.status(HttpStatus.FORBIDDEN));
-//        }
+        if (!cookieValue.equals(headerValue)) {
+            return Publishers.just(HttpResponse.status(HttpStatus.FORBIDDEN));
+        }
 
         return chain.proceed(request);
     }
