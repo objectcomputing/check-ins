f93c1eee30ac1016aee1272b238eccd9b87b1215
Michael Kimberlin <mkimberlin@gmail.com>
2020-11-13 10:40:43 -0600

    Change sizes and moved under standard title

--- web-ui/src/components/checkin/CheckinCycle.stories.js
+++ web-ui/src/components/checkin/CheckinCycle.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CheckinCycle from './CheckinCycle';
 
 export default {
-  title: 'CheckinCycle',
+  title: 'Check-Ins/CheckinCycle',
   component: CheckinCycle,
 };
 
@@ -12,6 +12,6 @@ export const NoDimensions = Template.bind({});
 
 export const SetDimensions = Template.bind({});
 SetDimensions.args = {
-    style: {height: "30rem", width: "300rem"}
+    style: {height: "5rem", width: "5rem"}
 };
 
