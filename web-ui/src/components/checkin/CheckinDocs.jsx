ddaec71a24a63d08ff7a873f9b9b0c706a9997cb
Zack <zack@jeffandbetsy.net>
2020-11-25 10:57:58 -0600

    Added Checkin Docs component to Storybook

--- web-ui/src/components/checkin/CheckinDocs.jsx
+++ web-ui/src/components/checkin/CheckinDocs.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import FileUploader from "./FileUploader";
 import {
   getFiles,
-  // getAllFiles,
   deleteFile,
   uploadFile,
 } from "../../api/upload";
