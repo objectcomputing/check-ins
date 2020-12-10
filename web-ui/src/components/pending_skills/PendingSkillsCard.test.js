6e31911dd3176a2ac3e221f577e56bbe7a7ecd22
Jesse Hanner <hannerj@objectcomputing.com>
2020-12-10 10:40:56 -0600

    added snapshot

---
+++ web-ui/src/components/pending_skills/PendingSkillsCard.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PendingSkillsCard from "./PendingSkillsCard";
+import { AppContextProvider } from "../../context/AppContext";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const initialState = {
+  state: {
+    userProfile: {
+      memberProfile: {
+        id: "912834091823",
+      },
+      role: ["MEMBER"],
+    },
+    memberProfiles: ["912834091823"],
+  },
+};
+
+const pendingSkill = {
+  extraneous: false,
+  id: "1134511f3e-7ab7-4edf-86f5-ab0b0a0d2ca9",
+  name: "Le test skill",
+  pending: false,
+};
+
+const server = setupServer(
+  rest.get("http://localhost:8080/services/skill", (req, res, ctx) => {
+    return res(ctx.json("Hello World"));
+  }),
+  rest.get("http://localhost:8080/fail", (req, res, ctx) => {
+    return res(ctx.status(500), ctx.json({ message: "Internal Server Error" }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+it("renders correctly", () => {
+  snapshot(
+    <AppContextProvider value={initialState}>
+      <PendingSkillsCard pendingSkill={pendingSkill} />
+    </AppContextProvider>
+  );
+});
