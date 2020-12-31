b33e61e02ca74f11f527371c8cf595ca712e1bd0
Holly Williams <williamsh@OCI1761MBP.local>
2020-12-31 14:22:20 -0600

    removed unused exceptions

--- server/src/main/java/com/objectcomputing/checkins/services/role/RoleBadArgException.java
+++
@@ -1,7 +0,0 @@
-package com.objectcomputing.checkins.services.role;
-
-public class RoleBadArgException extends RuntimeException {
-    public RoleBadArgException(String message) {
-        super(message);
-    }
-}
