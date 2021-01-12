1b7cb9bb6e5fcfe18e1fbaaff600919b5c5ad749
Holly Williams <williamsh@OCI1761MBP.local>
2021-01-12 10:55:29 -0600

    try to add skeleton when loading

--- web-ui/src/components/team-results/TeamSummaryCard.jsx
+++ web-ui/src/components/team-results/TeamSummaryCard.jsx
@@ -1,126 +1,152 @@
-import React, { useContext, useState } from "react";
+import React, {useContext, useState} from "react";
 
-import {
-  AppContext,
-  UPDATE_TEAMS,
-  UPDATE_TOAST,
-} from "../../context/AppContext";
+import {AppContext, UPDATE_TEAMS, UPDATE_TOAST,} from "../../context/AppContext";
 import EditTeamModal from "./EditTeamModal";
 
-import { Card, CardActions, CardContent, CardHeader } from "@material-ui/core";
-import { Skeleton } from "@material-ui/lab";
+import {Card, CardActions, CardContent, CardHeader} from "@material-ui/core";
+import {Skeleton} from "@material-ui/lab";
 import PropTypes from "prop-types";
-import { deleteTeam } from "../../api/team.js";
+import {deleteTeam} from "../../api/team.js";
 import SplitButton from "../split-button/SplitButton";
+import {getAgendaItem} from "../../api/agenda";
 
 const propTypes = {
-  team: PropTypes.shape({
-    id: PropTypes.string,
-    name: PropTypes.string,
-    description: PropTypes.string,
-  }),
+    team: PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string,
+        description: PropTypes.string,
+    }),
 };
 
 const displayName = "TeamSummaryCard";
+const [isLoading, setIsLoading] = useState(false);
 
-const TeamSummaryCard = ({ team, index }) => {
-  const { state, dispatch } = useContext(AppContext);
-  const { teams, userProfile, csrf } = state;
-  const [open, setOpen] = useState(false);
-  const isAdmin =
-    userProfile && userProfile.role && userProfile.role.includes("ADMIN");
+const TeamSummaryCard = ({team, index}) => {
+    const {state, dispatch} = useContext(AppContext);
+    const {teams, userProfile, csrf} = state;
+    const [open, setOpen] = useState(false);
+    const isAdmin =
+        userProfile && userProfile.role && userProfile.role.includes("ADMIN");
 
-  let leads =
-    team.teamMembers == null
-      ? null
-      : team.teamMembers.filter((teamMember) => teamMember.lead);
-  let nonLeads =
-    team.teamMembers == null
-      ? null
-      : team.teamMembers.filter((teamMember) => !teamMember.lead);
+    let leads =
+        team.teamMembers == null
+            ? null
+            : team.teamMembers.filter((teamMember) => teamMember.lead);
+    let nonLeads =
+        team.teamMembers == null
+            ? null
+            : team.teamMembers.filter((teamMember) => !teamMember.lead);
 
-  const isTeamLead =
-    leads === null
-      ? false
-      : leads.some((lead) => lead.memberid === userProfile.memberProfile.id);
+    const isTeamLead =
+        leads === null
+            ? false
+            : leads.some((lead) => lead.memberid === userProfile.memberProfile.id);
 
-  const handleOpen = () => setOpen(true);
+    const handleOpen = () => setOpen(true);
 
-  const handleClose = () => setOpen(false);
+    const handleClose = () => setOpen(false);
 
-  const deleteATeam = async (id) => {
-    if (id && csrf) {
-      const result = await deleteTeam(id, csrf);
-      if (result && result.payload && result.payload.status === 200) {
-        window.snackDispatch({
-          type: UPDATE_TOAST,
-          payload: {
-            severity: "success",
-            toast: "Team deleted",
-          },
-        });
-        let newTeams = teams.filter((team) => {
-          return team.id !== id;
-        });
-        dispatch({
-          type: UPDATE_TEAMS,
-          payload: newTeams,
-        });
-      }
-    }
-  };
+    //fix this mess
+    // i added this
+    const getTeams = async (checkinId, csrf) => {
+        setIsLoading(true);
+        let res = await getAgendaItem(checkinId, null, csrf);
+        let agendaItemList;
+        if (res && res.payload) {
+            agendaItemList =
+                res.payload.data && !res.error ? res.payload.data : undefined;
+            if (agendaItemList) {
+                agendaItemList.sort((a, b) => {
+                    return a.priority - b.priority;
+                });
+                setAgendaItems(agendaItemList);
+            }
+        }
+        setIsLoading(false);
+    };
 
-  const options =
-    isAdmin || isTeamLead ? ["Edit Team", "Delete Team"] : ["Edit Team"];
+    const deleteATeam = async (id) => {
+        if (id && csrf) {
+            const result = await deleteTeam(id, csrf);
+            if (result && result.payload && result.payload.status === 200) {
+                window.snackDispatch({
+                    type: UPDATE_TOAST,
+                    payload: {
+                        severity: "success",
+                        toast: "Team deleted",
+                    },
+                });
+                let newTeams = teams.filter((team) => {
+                    return team.id !== id;
+                });
+                dispatch({
+                    type: UPDATE_TEAMS,
+                    payload: newTeams,
+                });
+            }
+        }
+    };
 
-  const handleAction = (e, index) =>
-    index === 0 ? handleOpen() : deleteATeam(team.id);
+    const options =
+        isAdmin || isTeamLead ? ["Edit Team", "Delete Team"] : ["Edit Team"];
 
-  return (
-    <Card className="summary-card">
-      <CardHeader title={team.name} subheader={team.description} />
-      <CardContent>
-        {team.teamMembers == null ? (
-          <React.Fragment>
-              <strong>Team Leads: </strong>
-              <br />
-              <strong>Team Members: </strong>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <strong>Team Leads: </strong>
-            {leads.map((lead, index) => {
-              return index !== leads.length - 1 ? `${lead.name}, ` : lead.name;
-            })}
-            <br />
-            <strong>Team Members: </strong>
-            {nonLeads.map((member, index) => {
-              return index !== nonLeads.length - 1
-                ? `${member.name}, `
-                : member.name;
-            })}
-          </React.Fragment>
-        )}
-      </CardContent>
-      <CardActions>
-        <SplitButton options={options} onClick={handleAction} />
-      </CardActions>
-      <EditTeamModal
-        team={team}
-        open={open}
-        onClose={handleClose}
-        onSave={(team) => {
-          const copy = [...teams];
-          copy[index] = team;
-          dispatch({
-            type: UPDATE_TEAMS,
-            payload: copy,
-          });
-          handleClose();
-        }}
-      />
-    </Card>
-  );
+    const handleAction = (e, index) =>
+        index === 0 ? handleOpen() : deleteATeam(team.id);
+
+    return (
+        <Card className="summary-card">
+            <CardHeader title={team.name} subheader={team.description}/>
+            <CardContent>
+                {/*{isLoading ? (*/}
+                {/*    <div className="skeleton">*/}
+                {/*        <Skeleton variant="text" height={"2rem"}/>*/}
+                {/*        <Skeleton variant="text" height={"2rem"}/>*/}
+                {/*    </div>*/}
+                {/*) : (*/}
+                    {team.teamMembers == null ? (
+                            <React.Fragment>
+                                <strong>Team Leads: </strong>
+                                <br/>
+                                <strong>Team Members: </strong>
+                            </React.Fragment>
+                        ) : (
+                            <React.Fragment>
+                                <strong>Team Leads: </strong>
+                                {leads.map((lead, index) => {
+                                    return index !== leads.length - 1 ? `${lead.name}, ` : lead.name;
+                                })}
+                                <br/>
+                                <strong>Team Members: </strong>
+                                {nonLeads.map((member, index) => {
+                                    return index !== nonLeads.length - 1
+                                        ? `${member.name}, `
+                                        : member.name;
+                                })}
+                            </React.Fragment>
+                    //     )
+                    // }
+                )}
+
+            </CardContent>
+            <CardActions>
+                <SplitButton options={options} onClick={handleAction}/>
+            </CardActions>
+            <EditTeamModal
+                team={team}
+                open={open}
+                onClose={handleClose}
+                onSave={(team) => {
+                    const copy = [...teams];
+                    copy[index] = team;
+                    dispatch({
+                        type: UPDATE_TEAMS,
+                        payload: copy,
+                    });
+                    handleClose();
+                }}
+            />
+        </Card>
+    );
 };
 
 TeamSummaryCard.displayName = displayName;
