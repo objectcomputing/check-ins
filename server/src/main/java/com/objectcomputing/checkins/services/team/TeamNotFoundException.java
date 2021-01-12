19375103f11cc5fc18db4a4a690dbf9556860621
hwilliams314 <48927697+hwilliams314@users.noreply.github.com>
2021-01-12 08:12:22 -0600

    team part finished (#576)
    Co-authored-by: Holly Williams <williamsh@OCI1761MBP.local>

--- server/src/main/java/com/objectcomputing/checkins/services/team/TeamNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.objectcomputing.checkins.services.team;
-
-public class TeamNotFoundException extends RuntimeException {
-    private static final String DEFAULT_MESSAGE = "No such team found";
-    public TeamNotFoundException() {this(DEFAULT_MESSAGE);}
-    public TeamNotFoundException(String message) { super(message); }
-}
