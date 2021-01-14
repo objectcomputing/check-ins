1e34d9d26e21752249e8b50cd5682148bd137d7e
Michael Kimberlin <mkimberlin@gmail.com>
2021-01-14 17:36:54 -0600

    Added a couple of more tests

--- web-ui/src/components/slider/Slider.test.js
+++ web-ui/src/components/slider/Slider.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Slider from "./Slider";
 import renderer from "react-test-renderer";
+import { mount } from "enzyme";
 
 jest.mock('react-dom', () => ({
   findDOMNode: jest.fn()
@@ -19,12 +20,31 @@ it("renders slider with title", () => {
   snapshot(<Slider title="Some skill" />);
 });
 
-// it("renders slider correctly", () => {
-//   const wrapper = mount(
-//     <Slider title="Some skill" />
-//   );
-//   const find = wrapper.find("#questions-input-1");
+it("value change is reported to the handler correctly", () => {
+  const onChange = jest.fn();
 
-//   find.at(0).simulate("input", { target: { value: "sometext" } });
-//   expect(onAnswer.mock.calls.length).toBe(1);
-// });
+  const wrapper = mount(
+    <Slider title="Some skill" onChange={onChange} />
+  );
+  const slider = wrapper.find(Slider).at(0);
+  slider.simulate("change", { target: { value: 5 } });
+  slider.simulate("change", { target: { value: 4 } });
+  setTimeout(() => expect(onChange.mock.calls.length).toBe(2), 0);
+});
+
+it("value change is reported to all handlers correctly", () => {
+  const onChange = jest.fn();
+  const onChangeCommitted = jest.fn();
+
+  const wrapper = mount(
+    <Slider title="Some skill" onChange={onChange} onChangeCommitted={onChangeCommitted} />
+  );
+
+  const slider = wrapper.find(Slider).at(0);
+  slider.simulate("change", { target: { value: 5 } });
+  slider.find(".MuiSlider-mark").at(2).simulate("click", { target: { value: 3 } });
+  setTimeout(() => {
+    expect(onChange.mock.calls.length).toBe(2);
+    expect(onChangeCommitted.mock.calls.length).toBe(1);
+  }, 0);
+});
