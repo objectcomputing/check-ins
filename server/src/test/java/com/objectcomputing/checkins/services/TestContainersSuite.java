2593ab6d2361a364689650f4ec279dd7ceb03bcd
Michael Kimberlin <mkimberlin@gmail.com>
2021-02-04 09:58:34 -0600

    One more test containers attempt

--- server/src/test/java/com/objectcomputing/checkins/services/TestContainersSuite.java
+++ server/src/test/java/com/objectcomputing/checkins/services/TestContainersSuite.java
@@ -4,6 +4,7 @@ import com.objectcomputing.checkins.services.fixture.RepositoryFixture;
 import io.micronaut.context.env.Environment;
 import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.annotation.MicronautTest;
+import io.micronaut.test.support.TestPropertyProvider;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,10 +13,12 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 @MicronautTest(environments = {Environment.TEST}, transactional = false)
 @Testcontainers
-public abstract class TestContainersSuite implements RepositoryFixture {
+public abstract class TestContainersSuite implements RepositoryFixture, TestPropertyProvider {
 
     @Container
     private static final PostgreSQLContainer<?> postgres;
@@ -52,6 +55,27 @@ public abstract class TestContainersSuite implements RepositoryFixture {
         if(shouldResetDBAfterEachTest) {
             flyway.clean();
         }
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        HashMap<String, String> properties = new HashMap<>();
+        properties.put("datasources.default.url", getJdbcUrl());
+        properties.put("datasources.default.username", getUsername());
+        properties.put("datasources.default.password", getPassword());
+        return properties;
+    }
+
+    static String getJdbcUrl() {
+        return postgres.getJdbcUrl();
+    }
+
+    static String getUsername() {
+        return postgres.getUsername();
+    }
+
+    static String getPassword() {
+        return postgres.getPassword();
     }
 
     @Override
