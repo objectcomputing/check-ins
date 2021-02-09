6b3a397fed51085dd194a4401f0cdf6fd55fe315
Mike Moshiri <mahyarmoshiri.mm@gmail.com>
2021-02-08 23:46:48 -0600

    refactored checkin document to use global exceptions

--- server/src/main/java/com/objectcomputing/checkins/services/checkindocument/CheckinDocumentBadArgException.java
+++
@@ -1,7 +0,0 @@
-package com.objectcomputing.checkins.services.checkindocument;
-
-public class CheckinDocumentBadArgException extends RuntimeException {
-    public CheckinDocumentBadArgException(String message) {
-        super(message);
-    }
-}
