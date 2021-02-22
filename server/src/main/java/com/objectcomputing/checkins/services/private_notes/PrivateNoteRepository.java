082ad58681a99470c8f8c41578c3b04e3522e21a
hwilliams314 <48927697+hwilliams314@users.noreply.github.com>
2021-02-22 14:28:48 -0600

    added encryption for description (#711)
    Co-authored-by: Holly Williams <williamsh@OCI1761MBP.local>

--- server/src/main/java/com/objectcomputing/checkins/services/private_notes/PrivateNoteRepository.java
+++ server/src/main/java/com/objectcomputing/checkins/services/private_notes/PrivateNoteRepository.java
@@ -1,16 +1,11 @@
 package com.objectcomputing.checkins.services.private_notes;
 
-import com.objectcomputing.checkins.services.checkin_notes.CheckinNote;
 import io.micronaut.data.annotation.Query;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
