8d4ed7cd1e7b83f8f731381a8e1bd55d5d0eda6e
Mike Moshiri <mahyarmoshiri.mm@gmail.com>
2021-02-27 18:50:11 -0600

    Refactored guild member to use global exceptions

--- server/src/main/java/com/objectcomputing/checkins/services/guild/member/GuildMemberBadArgException.java
+++
@@ -1,7 +0,0 @@
-package com.objectcomputing.checkins.services.guild.member;
-
-public class GuildMemberBadArgException extends RuntimeException {
-    public GuildMemberBadArgException(String message) {
-        super(message);
-    }
-}
