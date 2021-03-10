f42e721fbd4f61401df28908b2a0a5d8d63cf215
Mike Moshiri <mahyarmoshiri.mm@gmail.com>
2021-03-10 15:15:01 -0600

    created a team member history table.

--- server/src/main/resources/db/common/V43__create_member_history_table.sql
+++ server/src/main/resources/db/common/V43__create_member_history_table.sql
@@ -1,7 +1,9 @@
 drop table if exists member_history;
 
 CREATE TABLE member_history(
-    memberid varchar REFERENCES member_profile(uuid),
-    teamId varchar REFERENCES team(uuid),
+    memberId varchar REFERENCES member_profile(id),
+    teamId varchar REFERENCES team(id),
     change varchar,
-    dateTime date,
+    dateTime date
+
+);
