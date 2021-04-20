8e5e2b5dfe133f11181512c11d6f31cc54b92820
Joe Warner <warnerj@objectcomputing.com>
2021-04-20 11:25:31 -0500

    Remove date initiator from feedback fixture

--- server/src/test/java/com/objectcomputing/checkins/services/fixture/FeedbackFixture.java
+++ server/src/test/java/com/objectcomputing/checkins/services/fixture/FeedbackFixture.java
@@ -8,6 +8,6 @@ import java.time.LocalDateTime;
 public interface FeedbackFixture extends RepositoryFixture {
     default Feedback createFeedback(String content, MemberProfile to, MemberProfile from, boolean confidential) {
         return getFeedbackRepository().save(new Feedback(content, to.getId(), from.getId(),
-                confidential, LocalDateTime.now(), null));
+                confidential));
     }
 }
