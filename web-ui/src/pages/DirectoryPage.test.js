7717136dde8071a01f92f5ba882c40a41ae75a60
Jesse Hanner <hannerj@objectcomputing.com>
2021-05-04 11:52:08 -0500

    added DirectoryPage test to fix code coverage

---
+++ web-ui/src/pages/DirectoryPage.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DirectoryPage from "./DirectoryPage";
+import { AppContextProvider } from "../context/AppContext";
+
+it("renders correctly", () => {
+  <AppContextProvider>
+    snapshot(
+    <DirectoryPage />
+    );
+  </AppContextProvider>;
+});
