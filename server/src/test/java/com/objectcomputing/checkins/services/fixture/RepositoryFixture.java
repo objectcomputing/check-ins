5858733af2bbe9f9648d82af7a386753d4b22c73
Joe Warner <warnerj@objectcomputing.com>
2021-05-05 07:51:59 -0500

    Add fixture and update respoitory fixture

--- server/src/test/java/com/objectcomputing/checkins/services/fixture/RepositoryFixture.java
+++ server/src/test/java/com/objectcomputing/checkins/services/fixture/RepositoryFixture.java
@@ -12,6 +12,7 @@ import com.objectcomputing.checkins.services.member_skill.MemberSkillRepository;
 import com.objectcomputing.checkins.services.memberprofile.MemberProfileRepository;
 import com.objectcomputing.checkins.services.private_notes.PrivateNoteRepository;
 import com.objectcomputing.checkins.services.pulseresponse.PulseResponseRepository;
+import com.objectcomputing.checkins.services.question_category.QuestionCategoryRepository;
 import com.objectcomputing.checkins.services.questions.QuestionRepository;
 import com.objectcomputing.checkins.services.role.RoleRepository;
 import com.objectcomputing.checkins.services.skills.SkillRepository;
@@ -97,5 +98,9 @@ public interface RepositoryFixture {
 
     default FeedbackRepository getFeedbackRepository() {
         return getEmbeddedServer().getApplicationContext().getBean(FeedbackRepository.class);
+    }
+
+    default QuestionCategoryRepository getQuestionCategoryRepository() {
+        return getEmbeddedServer().getApplicationContext().getBean(QuestionCategoryRepository.class);
     }
 }
