9ab8b08bd62bd8a0ee7f17b5972c5904eb3f84a0
Joe Warner <warnerj@objectcomputing.com>
2021-05-14 08:10:48 -0500

    Add constructor to include category id

--- server/src/main/java/com/objectcomputing/checkins/services/questions/Question.java
+++ server/src/main/java/com/objectcomputing/checkins/services/questions/Question.java
@@ -1,12 +1,11 @@
 package com.objectcomputing.checkins.services.questions;
 
-import com.sun.istack.Nullable;
 import io.micronaut.data.annotation.AutoPopulated;
 import io.micronaut.data.annotation.TypeDef;
 import io.micronaut.data.model.DataType;
-import io.micronaut.http.annotation.Body;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import javax.annotation.Nullable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -19,6 +18,11 @@ import java.util.UUID;
 @Entity
 @Table(name ="questions")
 public class Question {
+
+    public Question(@NotBlank String text, @Nullable UUID categoryId) {
+        this.text = text;
+        this.categoryId = categoryId;
+    }
 
     public Question(@NotBlank String text) {
         this.text = text;
