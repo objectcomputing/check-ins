b5ef0f836c46a075444c3ebaee9436f078b9021c
Joe Warner <warnerj@objectcomputing.com>
2021-05-28 14:23:47 -0500

    Correct column name for memberId reference

--- server/src/main/resources/db/common/V52__create_settings_table.sql
+++ server/src/main/resources/db/common/V52__create_settings_table.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS settings;
 CREATE TABLE settings (
     id varchar PRIMARY KEY,
     name varchar,
-    userid varchar REFERENCES member_profile(uuid),
+    userid varchar REFERENCES member_profile(id),
     value varchar
 );
