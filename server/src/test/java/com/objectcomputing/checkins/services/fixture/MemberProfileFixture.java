8f51a076c58b51328397a481e7cc75468d98fc9d
Mohit Bhatia <bhatiam@objectcomputing.com>
2021-06-02 16:11:51 -0500

    change for birthday service

--- server/src/test/java/com/objectcomputing/checkins/services/fixture/MemberProfileFixture.java
+++ server/src/test/java/com/objectcomputing/checkins/services/fixture/MemberProfileFixture.java
@@ -11,7 +11,7 @@ public interface MemberProfileFixture extends RepositoryFixture {
                 null, "Comedic Relief", null, "New York, New York",
                 "billm@objectcomputing.com", "mr-bill-employee", LocalDate.now(),
                 "is a clay figurine clown star of a parody of children's clay animation shows",
-                null, null));
+                null, null,LocalDate.now()));
     }
 
     default MemberProfile createADefaultMemberProfileForPdl(MemberProfile memberProfile) {
@@ -19,7 +19,7 @@ public interface MemberProfileFixture extends RepositoryFixture {
                 null, "Comedic Relief PDL", memberProfile.getId(), "New York, New York",
                 "billmpdl@objectcomputing.com", "mr-bill-employee-pdl",
                 LocalDate.now(), "is a clay figurine clown star of a parody of children's clay animation shows",
-                memberProfile.getId(), null));
+                memberProfile.getId(), null,null));
     }
 
     // this user is not connected to other users in the system
@@ -28,7 +28,7 @@ public interface MemberProfileFixture extends RepositoryFixture {
                 null, "Comedic Relief", null, "New York, New York",
                 "nobody@objectcomputing.com", "mr-bill-employee-unrelated",
                 LocalDate.now(), "is a clay figurine clown star of a parody of children's clay animation shows",
-                null, null));
+                null, null,null));
     }
 
     default MemberProfile createASecondDefaultMemberProfileForPdl(MemberProfile memberProfile) {
@@ -36,7 +36,7 @@ public interface MemberProfileFixture extends RepositoryFixture {
                 null, "Bully Relief PDL", memberProfile.getId(), "New York, New York",
                 "sluggopdl@objectcomputing.com", "sluggo-employee-pdl",
                 LocalDate.now(), "is the bully in a clay figurine clown star of a parody of children's clay animation shows",
-                memberProfile.getId(), null));
+                memberProfile.getId(), null,null));
     }
 
     default MemberProfile createAPastTerminatedMemberProfile() {
@@ -44,7 +44,7 @@ public interface MemberProfileFixture extends RepositoryFixture {
                 null, "Bully Relief PDL", null, "New York, New York",
                 "sluggopdl@objectcomputing.com", "sluggo-employee-pdl-past-terminated",
                 LocalDate.now(), "is the bully in a clay figurine clown star of a parody of children's clay animation shows",
-                null, LocalDate.now().minusDays(7)));
+                null, LocalDate.now().minusDays(7),null));
     }
 
     default MemberProfile createAFutureTerminatedMemberProfile() {
@@ -52,7 +52,7 @@ public interface MemberProfileFixture extends RepositoryFixture {
                 null, "Bully Relief PDL", null, "New York, New York",
                 "sluggopdl@objectcomputing.com", "sluggo-employee-pdl-future terminated",
                 LocalDate.now(), "is the bully in a clay figurine clown star of a parody of children's clay animation shows",
-                null, LocalDate.now().plusDays(7)));
+                null, LocalDate.now().plusDays(7),null));
 
     }
 
