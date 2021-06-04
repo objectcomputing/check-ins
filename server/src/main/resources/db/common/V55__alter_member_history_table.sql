ddfc7dd9c823bb2e2531d8d463625786c871599d
Mahyar Moshiri <moshirim@OCI1868MBP16INCH2019.local>
2021-06-04 17:07:56 -0500

    Fixed SQL numbering

--- server/src/main/resources/db/common/V55__alter_member_history_table.sql
+++ server/src/main/resources/db/common/V55__alter_member_history_table.sql
@@ -1,5 +1,12 @@
 ALTER TABLE member_history
+<<<<<<< HEAD
 ADD COLUMN id varchar PRIMARY KEY;
 
 ALTER TABLE member_history
 RENAME COLUMN dateTime TO date;
+=======
+DROP CONSTRAINT member_history_teamid_fkey;
+
+ALTER TABLE member_history
+ADD COLUMN teamName varchar;
+>>>>>>> 22b24b3b (Fixed SQL numbering)
--- server/src/main/resources/db/common/V56__alter_member_history_table.sql
+++ server/src/main/resources/db/common/V56__alter_member_history_table.sql
@@ -1,5 +1,12 @@
 ALTER TABLE member_history
+<<<<<<< HEAD
 DROP CONSTRAINT member_history_teamid_fkey;
 
 ALTER TABLE member_history
 ADD COLUMN teamName varchar;
+=======
+ADD COLUMN id varchar PRIMARY KEY;
+
+ALTER TABLE member_history
+RENAME COLUMN dateTime TO date;
+>>>>>>> 22b24b3b (Fixed SQL numbering)
