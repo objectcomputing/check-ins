5a8ddf05f50b76a7630a4f756a34de833e2a9219
pieperm <michaelepieper@gmail.com>
2021-06-10 10:09:49 -0500

    Fix ordering of steps, fix indents, update snapshot for request page

--- web-ui/src/pages/FeedbackRequestPage.jsx
+++ web-ui/src/pages/FeedbackRequestPage.jsx
@@ -10,75 +10,73 @@ import "./FeedbackRequestPage.css";
 import FeedbackRecipientSelector from "../components/feedback_recipient_selector/FeedbackRecipientSelector";
 
 const useStyles = makeStyles({
-    root: {
-        background: 'transparent'
-    },
-
+  root: {
+    background: "transparent"
+  },
 });
 
-
 function getSteps() {
-    return ["Select recipients", "Select template", "Set due date", "Done!"];
+  return ["Select template", "Select recipients", "Set due date", "Done!"];
 }
 
 const FeedbackRequestPage = () => {
-    const [activeStep, setActiveStep] = useState(0);
-    const steps = getSteps();
-    const classes = useStyles();
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = getSteps();
+  const classes = useStyles();
 
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
 
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
-    return (
-        <div className="feedback-request-page">
-            <div className="header-container">
-                <Typography variant="h4">Feedback Request for <b>John Doe</b></Typography>
-                <div>
-                    {activeStep === steps.length ? (
-                        <div>
-                            <Typography>
-                                All steps completed!
-                            </Typography>
-                        </div>
-                    ) : (
-                        <div>
-                            <Button disabled={activeStep === 0} onClick={handleBack}>
-                                Back
-                            </Button>
-
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={handleNext}
-                            >
-                                {activeStep === steps.length - 1 ? "Finish" : "Next"}
-                            </Button>
-                        </div>
-                    )}
-                </div>
+  return (
+    <div className="feedback-request-page">
+      <div className="header-container">
+        <Typography variant="h4">Feedback Request for <b>John Doe</b></Typography>
+        <div>
+          {activeStep === steps.length ? (
+            <div>
+              <Typography>
+                All steps completed!
+              </Typography>
             </div>
-            <Stepper activeStep={activeStep} className={classes.root}>
-                {steps.map((label) => {
-                    const stepProps = {};
-                    const labelProps = {};
-                    return (
-                        <Step key={label} {...stepProps}>
-                            <StepLabel {...labelProps} key = {label}>{label}</StepLabel>
-                        </Step>
-                    );
-                })}
-            </Stepper>
-            <div className="current-step-content">
-              {activeStep === 1 && <FeedbackRecipientSelector/>}
+          ) : (
+            <div>
+              <Button disabled={activeStep === 0} onClick={handleBack}>
+                Back
+              </Button>
+
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNext}
+              >
+                {activeStep === steps.length - 1 ? "Finish" : "Next"}
+              </Button>
             </div>
+          )}
         </div>
-        
-    );
+      </div>
+      <Stepper activeStep={activeStep} className={classes.root}>
+        {steps.map((label) => {
+          const stepProps = {};
+          const labelProps = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps} key={label}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      <div className="current-step-content">
+        {activeStep === 1 && <FeedbackRecipientSelector/>}
+      </div>
+    </div>
+
+  );
 }
 
 export default FeedbackRequestPage;
