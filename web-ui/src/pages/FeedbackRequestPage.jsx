5c7f61b1e8591b401b5737a8204017b9094f3ec3
Jefferson Brown <jeffedwardbrown@gmail.com>
2021-06-11 16:38:08 -0500

    Feature 993 commit

--- web-ui/src/pages/FeedbackRequestPage.jsx
+++ web-ui/src/pages/FeedbackRequestPage.jsx
@@ -1,84 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { useParams, useHistory } from "react-router-dom";
-import { useLocation } from 'react-router-dom'
-import queryString from 'query-string'
+import {Link, useLocation} from 'react-router-dom';
+import queryString from 'query-string';
+import { Redirect } from "react-router-dom"
 
 import "./FeedbackRequestPage.css";
 
 const useStyles = makeStyles({
     root: {
-        background: 'transparent'
+        background: "transparent"
     },
-
 });
 
-
 function getSteps() {
-    return ["Select recipients", "Select template", "Set due date", "Done!"];
+    return ["Select template", "Select recipients", "Set due date", "Done!"];
 }
 
 const FeedbackRequestPage = () => {
-    const [activeStep, setActiveStep] = useState(1);
+
     const steps = getSteps();
     const classes = useStyles();
-    const history = useHistory();
-    const [urlStep, setURLStep] = useState(useLocation());
-
-    useEffect(() => {
-        console.log(urlStep);
-        const params = new URLSearchParams();
-        // if (urlStep === undefined) {
-        //     console.log("undefined")
-        //     params.delete("step")
-        //     setActiveStep(0);
-        // } else {
-            console.log("uwrgiuagiubruisb")
-            const value = queryString.parse(urlStep.search);
-            // setActiveStep(parseInt(value.step));
-            params.append("step", `${activeStep}`)
-            history.push({ search: params.toString()})
-        //}
-    },[activeStep]);
-
-    useEffect(() => {
-        const params = new URLSearchParams();
-        if (urlStep === undefined) {
-            console.log("undefined")
-            params.delete("step")
-            setActiveStep(0);
-
-        } else {
-            const value = queryString.parse(urlStep.search);
-            setActiveStep(parseInt(value.step));
-        }
-        //const value = queryString.parse(urlStep.search);
-    },[])
-
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setURLStep((prev) => {
-            return {
-                ...prev,
-                search: `?step=${activeStep}`
-            }
-        })
-     };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-        setURLStep((prev) => {
-            return {
-                ...prev,
-                search: `?step=${activeStep}`
-            }
-        })
-     };
+    const urlStep = useLocation();
+    let activeStep = urlStep?.search ? parseInt(queryString.parse(urlStep.search).step) : 1;
+    if (activeStep < 1 || activeStep > getSteps().length) {
+        return (
+            <Redirect to="/feedback/request?step=1" />
+        );
+    }
+    console.log(activeStep);
 
     return (
         <div className="feedback-request-page">
@@ -93,17 +47,27 @@ const FeedbackRequestPage = () => {
                         </div>
                     ) : (
                         <div>
-                            <Button disabled={activeStep === 1} onClick={handleBack}>
-                                Back
-                            </Button>
+                            <Link
+                                className={`no-underline-link ${activeStep <= 1 ? 'disabled-link' : ''}`}
+                                to={`?step=${activeStep - 1}`}
+                                >
+                                <Button
+                                    disabled={activeStep <= 1} >
+                                    Back
+                                </Button>
+                            </Link>
 
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={handleNext}
-                            >
-                                {activeStep === steps.length ? "Finish" : "Next"}
-                            </Button>
+
+                            <Link
+                                className={`no-underline-link ${activeStep > getSteps().length ? 'disabled-link no-underline-link' : ''}`}
+                                to={`?step=${activeStep + 1}`}>
+                                <Button
+                                    disabled={activeStep > getSteps().length}
+                                    variant="contained"
+                                    color="primary">
+                                    {activeStep === steps.length ? "Submit" : "Next"}
+                                </Button>
+                            </Link>
                         </div>
                     )}
                 </div>
