871ab15885e8ce17f8dbe181d456d515e0ad20f5
Mahyar Moshiri <moshirim@OCI1868MBP16INCH2019.localdomain>
2021-06-15 09:56:37 -0500

    Used mterial ui image for the confirmation checkmark.

--- web-ui/src/components/feedback_request_confirmation/FeedbackRequestConfirmation.jsx
+++ web-ui/src/components/feedback_request_confirmation/FeedbackRequestConfirmation.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import checkmark from "./checkmark.png";
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+
 
 import "./FeedbackRequestConfirmation.css";
+import {green} from "@material-ui/core/colors";
 
 const formatNamesAsList = (names) => {
   return names.join(", ")
@@ -11,7 +13,7 @@ const formatNamesAsList = (names) => {
 const FeedbackRequestConfirmation = (props) => {
   return (
     <div className="request-confirmation">
-      <img className="checkmark-image" alt="Checkmark" src={checkmark}/>
+      <CheckCircleIcon style={{ color: green[500] , fontSize: 400 }}>checkmark-image</CheckCircleIcon>
       <Typography variant="h3"><b>Feedback scheduled for today</b></Typography>
       <Typography className="recipients-list" variant="h6"><b>From:</b> {formatNamesAsList(["Alice", "Bob", "Eve"])}</Typography>
     </div>
