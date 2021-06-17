bdff91df58ba4b1d040586619d64082325c6a9d4
Jefferson Brown <jeffedwardbrown@gmail.com>
2021-06-17 10:11:44 -0500

    Update from feedback

--- web-ui/src/components/template-card/TemplateCard.js
+++ web-ui/src/components/template-card/TemplateCard.js
@@ -1,52 +1,124 @@
-import React from "react";
-import PropTypes from "prop-types";
+import "./TemplateCard.css";
+import React, {useState} from "react";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import CardActions from '@material-ui/core/CardActions';
+import Fullscreen from '@material-ui/icons/Fullscreen';
+import FullscreenExit from '@material-ui/icons/FullscreenExit';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
 
-import "./TemplateCard.css"
-
-const propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    creator: PropTypes.string.isRequired
-}
-
-const cutText = (text, maxCharacters) => {
-    let shortenedText = text;
-    if (text.length > maxCharacters) {
-        shortenedText = `${text.substring(0, maxCharacters)}...`;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 1000,
+    },
+    appBar: {
+        position: 'relative',
+    },
+    media: {
+        height: 0,
+    },
+    expand: {
+        justifyContent: "right",
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        justifyContent: "right",
     }
-    return shortenedText;
-}
+}));
 
-const TemplateCard = (props) => {
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const TemplateCard = ({templateName = "Ad Hoc", description = "Ask a single question", creator = "Admin", questions = []}) => {
+    const classes = useStyles();
+
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    }
+
     return (
-        <Card className = 'feedback-template-card'>
+        <Card className={ `feedback-template-card ${classes.root}` }>
+            <CardActions disableSpacing>
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    {!expanded ? <Fullscreen/> : <FullscreenExit/>}
+
+                </IconButton>
+            </CardActions>
             <CardContent>
                 <div className='card-content'>
-                    <div>
-                        <div className='template-name'>
-                            {cutText(props.title, 20)}
+                    <div className='templateName'>
+                        {templateName}
+                    </div>
+                    <div className='description-and-creator'>
+                        <div className='description'>
+                            {description}
                         </div>
-                        <div className='description-and-creator'>
-                            <div className='description'>
-                                {cutText(props.description, 90)}
+
+                        <div className='creator-wrapper'>
+                            Created by:
+                            <div className='creator'>
+                                {creator}
                             </div>
                         </div>
                     </div>
-                    <div className='creator-wrapper'>
-                        Created by:
-                        <div className='creator'>
-                            {props.creator}
-                        </div>
-                    </div>
-               </div>
+                </div>
+
             </CardContent>
+            <Dialog fullScreen open={expanded} onClose={handleExpandClick} TransitionComponent={Transition}>
+                <AppBar className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" onClick={handleExpandClick} aria-label="close">
+                            <CloseIcon/>
+                        </IconButton>
+                        <div className={classes.title}>
+                            <Typography variant="h6">
+                                {templateName}
+                            </Typography>
+                            <Typography variant="subtitle2">
+                                {description}
+                            </Typography>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <List>
+                    {questions.map((question, index) => (
+                        <React.Fragment>
+                            <ListItem button>
+                                <ListItemText primary={`Question ${index + 1}`} secondary={question}/>
+                            </ListItem>
+                            <Divider/>
+                        </React.Fragment>
+                    ))}
+                </List>
+            </Dialog>
         </Card>
     );
 
-}
+};
 
-TemplateCard.propTypes = propTypes;
 
 export default TemplateCard;
--- web-ui/src/components/template-card/TemplateCard.stories.js
+++ web-ui/src/components/template-card/TemplateCard.stories.js
@@ -3,21 +3,17 @@ import TemplateCard from "./TemplateCard";
 
 export default {
   title: "Check Ins/TemplateCard",
-  component: TemplateCard
+  component: TemplateCard,
 };
-
 const Template = (args) => <TemplateCard {...args} />;
 
-export const TemplateCardDefault = Template.bind({});
-TemplateCardDefault.args = {
-  title: "",
-  description: "",
-  creator: ""
-};
+export const DefaultTemplate = Template.bind({});
+DefaultTemplate.args = {};
 
-export const SetTemplateCard = Template.bind({});
-SetTemplateCard.args = {
-  title: "Template",
-  description: "An example feedback template",
-  creator: "Admin"
+export const PeerFeedbackTemplate = Template.bind({});
+PeerFeedbackTemplate.args = {
+  templateName: "Peer Feedback",
+  description: "A simple feedback template",
+  creator: "Bob Jones",
+  questions: ["Test 1", "Test 2"]
 };
