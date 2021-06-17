c6cdc5c8b196b2b0e67ad153a4a407807cdb8ad5
Jefferson Brown <jeffedwardbrown@gmail.com>
2021-06-17 15:24:04 -0500

    Updated Snapshots for issue 973

--- web-ui/src/components/feedback_request/Feedback_recipient_card.test.js
+++ web-ui/src/components/feedback_request/Feedback_recipient_card.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FeedbackRecipientCard from "./Feedback_recipient_card";
+import {AppContextProvider} from "../../context/AppContext";
+
 
 it("renders the recipient card", () => {
-  snapshot(<FeedbackRecipientCard/>);
+  snapshot(<AppContextProvider><FeedbackRecipientCard/></AppContextProvider>);
 });
