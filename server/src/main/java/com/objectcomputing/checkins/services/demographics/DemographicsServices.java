f5237fa7bb0a6f6303e68b32440cbbb6a3c00b4b
Joe Warner <warnerj@objectcomputing.com>
2021-07-02 08:26:28 -0500

    Add methods to services interface

--- server/src/main/java/com/objectcomputing/checkins/services/demographics/DemographicsServices.java
+++ server/src/main/java/com/objectcomputing/checkins/services/demographics/DemographicsServices.java
@@ -1,4 +1,28 @@
 package com.objectcomputing.checkins.services.demographics;
 
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
 public interface DemographicsServices {
+
+    Demographics getById(UUID id);
+
+    Set<Demographics> findByValues(UUID memberId,
+                                   String gender,
+                                   String degreeLevel,
+                                   Integer industryTenure,
+                                   boolean personOfColor,
+                                   boolean veteran,
+                                   Integer militaryTenure,
+                                   String militaryBranch);
+
+    Demographics updateDemographics(Demographics demographics);
+
+    Demographics saveDemographics(Demographics demographics);
+
+    List<Demographics> findAll();
+
+    Boolean deleteDemographics(UUID id);
 }
