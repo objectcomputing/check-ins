c3a4a8603f781ca4c164bb06e8028b1b7da5ff82
Joe Warner <warnerj@objectcomputing.com>
2021-07-06 09:40:50 -0500

    Multi search query in progress

--- server/src/main/java/com/objectcomputing/checkins/services/demographics/DemographicsRepository.java
+++ server/src/main/java/com/objectcomputing/checkins/services/demographics/DemographicsRepository.java
@@ -1,4 +1,25 @@
 package com.objectcomputing.checkins.services.demographics;
 
-public interface DemographicsRepository {
+import io.micronaut.data.annotation.Query;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.repository.CrudRepository;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
+
+@JdbcRepository(dialect = Dialect.POSTGRES)
+public interface DemographicsRepository extends CrudRepository<Demographics, UUID> {
+    List<Demographics> findAll();
+
+    @Query(value = "", nativeQuery = true)
+    List<Demographics> search(@Nullable String memberId,
+                              @Nullable String gender,
+                              @Nullable String degreeLevel,
+                              @Nullable Integer industryTenure,
+                              @Nullable Boolean personOfColor,
+                              @Nullable Boolean veteran,
+                              @Nullable Integer militaryTenure,
+                              @Nullable String militaryBranch);
 }
