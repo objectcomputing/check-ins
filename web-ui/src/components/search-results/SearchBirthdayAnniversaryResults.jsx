f0d6638787813f20873e6b2aae90f94a1321a351
Jesse Hanner <hannerj@objectcomputing.com>
2021-07-07 16:37:37 -0500

    made some fixes

--- web-ui/src/components/search-results/SearchBirthdayAnniversaryResults.jsx
+++ web-ui/src/components/search-results/SearchBirthdayAnniversaryResults.jsx
@@ -2,107 +2,117 @@ import React, { useContext } from "react";
 
 import { AppContext } from "../../context/AppContext";
 import { getAvatarURL } from "../../api/api";
-import { selectProfile, selectSkill } from "../../context/selectors";
+import { selectProfile } from "../../context/selectors";
 
 import {
   Avatar,
   Card,
   CardHeader,
-  Chip,
+  Container,
   List,
-  ListItem,
   Typography,
 } from "@material-ui/core";
 
-
-const SearchBirthdayAnniversaryResults = ({searchBirthdayResults, searchAnniversaryResults}) => {
-
+const SearchBirthdayAnniversaryResults = ({
+  searchBirthdayResults,
+  searchAnniversaryResults,
+}) => {
   searchAnniversaryResults = searchAnniversaryResults.sort((a, b) => {
-  return a.anniversary.localeCompare(b.anniversary);
-  })
+    return a.anniversary.localeCompare(b.anniversary);
+  });
   const { state } = useContext(AppContext);
   const getMemberProfile = (member) => selectProfile(state, member.userId);
 
-
-  const birthdayMap = () => {
-  return (searchBirthdayResults.length > 0 && (
-          searchBirthdayResults.map((member, index) => {
-            let profile=getMemberProfile(member);
-            return (
-              <Card className={"member-skills-card"} key={`card-${member.userId}`}>
-                <CardHeader
-                  title={
-                    <Typography variant="h5" component="h2">
-                      {getMemberProfile(member).name || ""}
-                    </Typography>
-                  }
-                  subheader={
-                    <Typography color="textSecondary" component="h3">
-
-                      {getMemberProfile(member).title || ""}
-                      <br/>
-                      Birthday: {member.birthDay || ""}
-                    </Typography>
-                  }
-                  disableTypography
-                  avatar={
-                    <Avatar
-                      className={"large"}
-                      src={getAvatarURL(
-                        getMemberProfile(member).workEmail || ""
-                      )}
-                    />
-                  }
+  const BirthdayMap = () => {
+    if (searchBirthdayResults.length > 0) {
+      return searchBirthdayResults.map((member, index) => {
+        return (
+          <Card className={"member-skills-card"} key={`card-${member.userId}`}>
+            <CardHeader
+              title={
+                <Typography variant="h5" component="h2">
+                  {getMemberProfile(member).name || ""}
+                </Typography>
+              }
+              subheader={
+                <Typography color="textSecondary" component="h3">
+                  {getMemberProfile(member).title || ""}
+                  <br />
+                  Birthday: {member.birthDay || ""}
+                </Typography>
+              }
+              disableTypography
+              avatar={
+                <Avatar
+                  className={"large"}
+                  src={getAvatarURL(getMemberProfile(member).workEmail || "")}
                 />
-              </Card>
-            );
-          })
-         ))
-  }
-//   const anniversaryMap = () => {
-//
-//   }
+              }
+            />
+          </Card>
+        );
+      });
+    } else return null;
+  };
 
+  const AnniversaryMap = () => {
+    if (searchBirthdayResults.length > 0) {
+      return (
+        searchAnniversaryResults.length > 0 &&
+        searchAnniversaryResults.map((member, index) => {
+          return (
+            <Card
+              className={"member-skills-card"}
+              key={`card-${member.userId}`}
+            >
+              <CardHeader
+                title={
+                  <Typography variant="h5" component="h2">
+                    {getMemberProfile(member).name || ""}
+                  </Typography>
+                }
+                subheader={
+                  <Typography color="textSecondary" component="h3">
+                    {getMemberProfile(member).title || ""}
+                    <br />
+                    Anniversary: {member.anniversary || ""}
+                    <br />
+                    Tenure: {member.yearsOfService || ""}
+                  </Typography>
+                }
+                disableTypography
+                avatar={
+                  <Avatar
+                    className={"large"}
+                    src={getAvatarURL(getMemberProfile(member).workEmail || "")}
+                  />
+                }
+              />
+            </Card>
+          );
+        })
+      );
+    } else return null;
+  };
 
   return (
     <div className="results-section">
       <List>
-      {searchBirthdayResults.length > 0 && <h2>Birthdays</h2>}
-      {searchBirthdayResults.length > 0 && <birthdayMap/>}
-        {searchAnniversaryResults.length > 0 && <h2>Anniversaries</h2>}
-        { searchAnniversaryResults.length > 0 && (
-          searchAnniversaryResults.map((member, index) => {
-            let profile=getMemberProfile(member);
-            return (
-              <Card className={"member-skills-card"} key={`card-${member.userId}`}>
-                <CardHeader
-                  title={
-                    <Typography variant="h5" component="h2">
-                      {getMemberProfile(member).name || ""}
-                    </Typography>
-                  }
-                  subheader={
-                    <Typography color="textSecondary" component="h3">
-                      {getMemberProfile(member).title || ""}
-                      <br/>
-                      Anniversary: {member.anniversary || ""}
-                      <br/>
-                      Tenure: {member.yearsOfService || ""}
-                    </Typography>
-                  }
-                  disableTypography
-                  avatar={
-                    <Avatar
-                      className={"large"}
-                      src={getAvatarURL(
-                        getMemberProfile(member).workEmail || ""
-                      )}
-                    />
-                  }
-                />
-              </Card>
-            );
-          })
+        {searchBirthdayResults.length > 0 && (
+          <Card>
+            <CardHeader title="Birthdays" />
+            <Container fixed>
+              <BirthdayMap />
+            </Container>
+          </Card>
+        )}
+        {searchAnniversaryResults.length > 0 && (
+          <Card>
+            <CardHeader title="Anniversaries" />
+            <Container fixed>
+              <AnniversaryMap />
+            </Container>
+          </Card>
         )}
       </List>
     </div>
--- web-ui/src/pages/BirthdayAnniversaryReportPage.jsx
+++ web-ui/src/pages/BirthdayAnniversaryReportPage.jsx
@@ -11,20 +11,34 @@ import { getAnniversary, getBirthday } from "../api/birthdayanniversary.js";
 import { UPDATE_TOAST } from "../context/actions";
 import SearchBirthdayAnniversaryResults from "../components/search-results/SearchBirthdayAnniversaryResults";
 
- import {
-   selectCsrfToken
- } from "../context/selectors";
+import { selectCsrfToken } from "../context/selectors";
+
+const months = [
+  {
+    month: "January",
+  },
+  { month: "February" },
+  { month: "March" },
+  { month: "April" },
+  { month: "May" },
+  { month: "June" },
+  { month: "July" },
+  { month: "August" },
+  { month: "September" },
+  { month: "October" },
+  { month: "November" },
+  { month: "December" },
+];
 
 const BirthdayAnniversaryReportPage = () => {
-  const months = ['January', 'February', 'March','April','May','June',"July",'August','September','October','November','December'];
-  const { state, dispatch } = useContext(AppContext);
+  const { state } = useContext(AppContext);
   const csrf = selectCsrfToken(state);
   const [anniversary, setAnniversary] = useState(true);
   const [birthday, setBirthday] = useState(true);
   const [searchBirthdayResults, setSearchBirthdayResults] = useState([]);
   const [searchAnniversaryResults, setSearchAnniversaryResults] = useState([]);
-  const [searchMonth, setSearchMonth] = useState(null);
   const currentMonth = new Date().getMonth();
+  const [selectedMonth, setSelectedMonth] = useState(months[currentMonth]);
 
   const handleBirthday = () => {
     setBirthday(!birthday);
@@ -34,53 +48,39 @@ const BirthdayAnniversaryReportPage = () => {
     setAnniversary(!anniversary);
   };
 
-   const handleSearch = async (searchMembers) => {
-   let anniversaryResults;
-   let birthdayResults;
-   console.log(searchMembers);
-   if(!birthday && !anniversary){
-         console.log("loop0",birthday);
-         window.snackDispatch({
-           type: UPDATE_TOAST,
-           payload: {
-             severity: "error",
-             toast: "You must select one of the below check boxes",
-           },
-         });
-         return;
-       }
-   let res;
-   if(!birthday) {
-     console.log("loop1",birthday);
-     console.log(anniversary);
-     anniversaryResults = await getAnniversary(searchMembers, csrf);
-     setSearchAnniversaryResults(anniversaryResults.payload.data);
-     setSearchBirthdayResults([]);
-     console.log(await getAnniversary(searchMembers, csrf));
-     }
-   else if(!anniversary)
-     {
-     console.log("loop2",birthday);
-     console.log(anniversary);
-     birthdayResults = await getBirthday(searchMembers, csrf);
-     setSearchBirthdayResults(birthdayResults.payload.data);
-     setSearchAnniversaryResults([]);
-     console.log(birthdayResults);
-     }
-   else {
-    console.log("loop3",birthday);
-    anniversaryResults = await getAnniversary(searchMembers, csrf);
-    birthdayResults = await getBirthday(searchMembers, csrf);
-    setSearchBirthdayResults(birthdayResults.payload.data);
-    setSearchAnniversaryResults(anniversaryResults.payload.data);
-    console.log(anniversary);
-    console.log(anniversaryResults);
-   }
-   };
+  const handleSearch = async (monthToSearch) => {
+    let anniversaryResults;
+    let birthdayResults;
+    const { month } = monthToSearch;
+    if (!birthday && !anniversary) {
+      window.snackDispatch({
+        type: UPDATE_TOAST,
+        payload: {
+          severity: "error",
+          toast: "You must select one of the below check boxes",
+        },
+      });
+      return;
+    }
+    if (!birthday) {
+      anniversaryResults = await getAnniversary(month, csrf);
+      setSearchAnniversaryResults(anniversaryResults.payload.data);
+      setSearchBirthdayResults([]);
+    } else if (!anniversary) {
+      birthdayResults = await getBirthday(month, csrf);
+      setSearchBirthdayResults(birthdayResults.payload.data);
+      setSearchAnniversaryResults([]);
+    } else {
+      anniversaryResults = await getAnniversary(month, csrf);
+      birthdayResults = await getBirthday(month, csrf);
+      setSearchBirthdayResults(birthdayResults.payload.data);
+      setSearchAnniversaryResults(anniversaryResults.payload.data);
+    }
+  };
 
-   function onMonthChange(event, newValue) {
-     setSearchMonth(newValue);
-   }
+  function onMonthChange(event, newValue) {
+    setSelectedMonth(newValue !== null ? newValue : months[currentMonth].month);
+  }
 
   return (
     <div>
@@ -88,12 +88,14 @@ const BirthdayAnniversaryReportPage = () => {
         <Autocomplete
           id="monthSelect"
           options={months}
-          value={searchMonth ? searchMonth : [months[currentMonth]]}
+          value={selectedMonth ? selectedMonth : months[currentMonth].month}
           onChange={onMonthChange}
           getOptionSelected={(option, value) =>
             value ? value.id === option.id : false
           }
-          getOptionLabel={(option) => option}
+          getOptionLabel={(option) =>
+            option.month || months[currentMonth].month
+          }
           renderInput={(params) => (
             <TextField
               {...params}
@@ -104,39 +106,52 @@ const BirthdayAnniversaryReportPage = () => {
           )}
         />
       </div>
-        <div className="skills-search halfWidth">
-          <Button
-            onClick={() => {
-              if (!searchMonth) {
-                window.snackDispatch({
-                  type: UPDATE_TOAST,
-                  payload: {
-                    severity: "error",
-                    toast: "Must select a month",
-                  },
-                });
-                return;
-              }
-              handleSearch(searchMonth);
-            }}
-            color="primary"
-          >
-            Run Search
-          </Button>
-        </div>
+      <div className="birthday-anniversary-search halfWidth">
+        <Button
+          onClick={() => {
+            if (!selectedMonth) {
+              window.snackDispatch({
+                type: UPDATE_TOAST,
+                payload: {
+                  severity: "error",
+                  toast: "Must select a month",
+                },
+              });
+              return;
+            }
+            handleSearch(selectedMonth);
+          }}
+          color="primary"
+        >
+          Run Search
+        </Button>
+      </div>
       <div className="checkbox-row">
         <label htmlFor="birthday">Include Birthdays</label>
-        <input id="birthday" checked={birthday} onClick={handleBirthday} type="checkbox" />
+        <input
+          id="birthday"
+          checked={birthday}
+          onClick={handleBirthday}
+          type="checkbox"
+        />
         <label htmlFor="anniversary">Include Anniversaries</label>
-        <input id="anniversary" checked={anniversary} onClick={handleAnniversary} type="checkbox" />
+        <input
+          id="anniversary"
+          checked={anniversary}
+          onClick={handleAnniversary}
+          type="checkbox"
+        />
       </div>
       <div>
-        {(
+        {
           <div className="search-results">
             <h2>Birthdays and Anniversaries</h2>
-              <SearchBirthdayAnniversaryResults searchBirthdayResults = {searchBirthdayResults} searchAnniversaryResults = {searchAnniversaryResults}/>
+            <SearchBirthdayAnniversaryResults
+              searchBirthdayResults={searchBirthdayResults}
+              searchAnniversaryResults={searchAnniversaryResults}
+            />
           </div>
-        )}
+        }
       </div>
     </div>
   );
