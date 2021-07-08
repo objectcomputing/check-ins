695ddc648f7fb006a42e5af4c186b379c3eab0ad
Julia Smith <42749098+smithj11@users.noreply.github.com>
2021-07-08 11:45:12 -0500

    Updated storybook

--- web-ui/src/components/view_feedback_selector/ViewFeedbackSelector.stories.js
+++ web-ui/src/components/view_feedback_selector/ViewFeedbackSelector.stories.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ViewFeedbackSelector from "./ViewFeedbackSelector";
+import {AppContextProvider} from "../../context/AppContext";
 
 export default {
   title: "Check Ins/ViewFeedbackSelector",
   component: ViewFeedbackSelector,
+  decorators: [(ViewFeedbackSelector) => {
+    return (<AppContextProvider><BrowserRouter><ViewFeedbackSelector/></BrowserRouter></AppContextProvider>);
+  }]
 };
 const Template = (args) => <ViewFeedbackSelector {...args} />;
 
