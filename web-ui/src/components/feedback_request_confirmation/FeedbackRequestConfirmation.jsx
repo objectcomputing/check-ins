91d1ac8394beed5e75eaa795fef1203025aa3ded
Julia Smith <42749098+smithj11@users.noreply.github.com>
2021-07-12 15:25:18 -0500

    Tentatively complete pending build and PR review

--- web-ui/src/components/feedback_request_confirmation/FeedbackRequestConfirmation.jsx
+++ web-ui/src/components/feedback_request_confirmation/FeedbackRequestConfirmation.jsx
@@ -1,46 +1,65 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Typography from "@material-ui/core/Typography";
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import {makeStyles} from '@material-ui/core/styles';
-import {selectProfile} from "../../context/selectors";
-import {AppContext} from "../../context/AppContext";
-import {useLocation, useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { selectProfile } from "../../context/selectors";
+import { AppContext } from "../../context/AppContext";
+import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import DateFnsUtils from "@date-io/date-fns";
 import "./FeedbackRequestConfirmation.css";
-import {green} from "@material-ui/core/colors";
+import { green } from "@material-ui/core/colors";
 
 const dateUtils = new DateFnsUtils();
 
-const formatNamesAsList = (names) => {
-  return names.join(", ")
-}
 const useStyles = makeStyles({
-announcement: {
-  textAlign: "center"
-},
+  announcement: {
+    textAlign: "center"
+  },
 });
 
-    let today = new Date();
-    today = dateUtils.format(today, "yyyy-MM-dd");
+let today = new Date();
+today = dateUtils.format(today, "yyyy-MM-dd");
 
 const FeedbackRequestConfirmation = (props) => {
   const classes = useStyles();
-    const {state} = useContext(AppContext);
-    const location = useLocation();
-    const history = useHistory();
-     const query = queryString.parse(location?.search);
-    console.log(JSON.stringify(query))
-    const forQuery = query.for?.toString();
-     const fromQuery = query.from?.toString();
-     const sendQuery = query.send?.toString();
-    const requestee = selectProfile(state, forQuery);
+  const { state } = useContext(AppContext);
+  const location = useLocation();
+  const query = queryString.parse(location?.search);
+  const forQuery = query.for?.toString();
+  const fromQuery = query.from?.toString();
+  const sendQuery = query.send?.toString();
+  const requestee = selectProfile(state, forQuery);
+  let recipientInfo = getRecipientNames();
 
+  function getRecipientNames() {
+    if (fromQuery !== undefined) {
+      let fromArray = fromQuery.split(',')
+      let recipientProfiles = []
+      if (fromArray.length !== 0) {
+        for (let i = 0; i < fromArray.length; ++i) {
+          let element = fromArray[i]
+          recipientProfiles.push(element)
+        }
+
+      } else {
+        recipientProfiles.push(fromQuery)
+
+      }
+      return recipientProfiles;
+
+    }
+
+  }
   return (
     <div className="request-confirmation">
-      <CheckCircleIcon style={{ color: green[500] , fontSize: '40vh' }}>checkmark-image</CheckCircleIcon>
+      <CheckCircleIcon style={{ color: green[500], fontSize: '40vh' }}>checkmark-image</CheckCircleIcon>
       <Typography className={classes.announcement} variant="h3"><b>Feedback request {sendQuery > today ? " scheduled on: " + sendQuery : " sent"} for {requestee?.name} </b></Typography>
-      <Typography className="recipients-list" variant="h6"><b></b> {formatNamesAsList(["Alice", "Bob", "Eve"])}</Typography>
+      <Typography className="recipients-list" variant="h6"><b>Sent to: </b>
+        {recipientInfo?.map((member, index) =>
+          `${selectProfile(state, member)?.name}${index === recipientInfo.length - 1 ? "" : ', '}`
+        )}
+      </Typography>
     </div>
   );
 }
--- web-ui/src/pages/FeedbackRequestPage.jsx
+++ web-ui/src/pages/FeedbackRequestPage.jsx
@@ -1,19 +1,19 @@
-import React, {useContext, useCallback} from "react";
+import React, { useContext, useCallback } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import {useLocation, useHistory } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import queryString from 'query-string';
 import FeedbackTemplateSelector from "../components/feedback_template_selector/FeedbackTemplateSelector";
 import FeedbackRecipientSelector from "../components/feedback_recipient_selector/FeedbackRecipientSelector";
 import SelectDate from "../components/feedback_date_selector/SelectDate";
 import "./FeedbackRequestPage.css";
-import {AppContext} from "../context/AppContext";
+import { AppContext } from "../context/AppContext";
 import DateFnsUtils from "@date-io/date-fns";
-import {selectProfile} from "../context/selectors";
+import { selectProfile } from "../context/selectors";
 
 const dateUtils = new DateFnsUtils();
 const useStyles = makeStyles((theme) => ({
@@ -58,7 +58,7 @@ function getSteps() {
 }
 
 const FeedbackRequestPage = () => {
-  const {state} = useContext(AppContext);
+  const { state } = useContext(AppContext);
   const steps = getSteps();
   const classes = useStyles();
   const location = useLocation();
@@ -96,15 +96,15 @@ const FeedbackRequestPage = () => {
     let today = new Date();
     today = dateUtils.format(today, "yyyy-MM-dd");
     let timeStamp = Date.parse(dateString)
-    if(dateString < today)
+    if (dateString < today)
       return false;
     else
       return !isNaN(timeStamp);
-   }, []);
+  }, []);
 
   const hasSend = useCallback(() => {
     let isValidPair = false
-    if(dueQuery) {
+    if (dueQuery) {
       isValidPair = dueQuery >= sendQuery
     }
     return (sendQuery && isValidDate(sendQuery) && isValidPair)
@@ -123,15 +123,13 @@ const FeedbackRequestPage = () => {
     }
   }, [activeStep, hasFor, hasTemplate, hasFrom, hasSend, dueQuery, isValidDate]);
 
-  const handleSubmit = useCallback(() => {
+  const handleSubmit = (() => {
     const newLocation = {
-      pathname : "/feedback/request/confirmation",
+      pathname: "/feedback/request/confirmation",
+      search: queryString.stringify(query),
     }
-    query.sendQuery = sendQuery
-    query.from = fromQuery
-    console.log("query on feedback request page "+ JSON.stringify(query))
-    history.push({...newLocation, search: queryString.stringify(query)})
-  }, [history]);
+    history.push(newLocation)
+  });
 
   const onNextClick = useCallback(() => {
     if (!canProceed()) return;
@@ -140,36 +138,38 @@ const FeedbackRequestPage = () => {
       return;
     }
     query.step = `${activeStep + 1}`;
-    history.push({...location, search: queryString.stringify(query)});
-  }, [canProceed, activeStep, steps.length, query, location, history, handleSubmit]);
+    history.push({ ...location, search: queryString.stringify(query) });
+
+// eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [canProceed, activeStep, steps.length, query, location, history]); 
 
   const onBackClick = useCallback(() => {
     if (activeStep === 1) return;
     query.step = `${activeStep - 1}`;
-    history.push({...location, search: queryString.stringify(query)});
+    history.push({ ...location, search: queryString.stringify(query) });
   }, [activeStep, query, location, history]);
 
-    const urlIsValid = useCallback(() => {
-      switch (activeStep) {
-        case 1:
-          return hasFor();
-        case 2:
-          return hasFor() && hasTemplate();
-        case 3:
-          return hasFor() && hasTemplate() && hasFrom();
-        case 4:
-          return hasFor() && hasTemplate() && hasFrom() && hasSend();
-        default:
-          return false;
-      }
-    }, [activeStep, hasFor, hasTemplate, hasFrom, hasSend]);
+  const urlIsValid = useCallback(() => {
+    switch (activeStep) {
+      case 1:
+        return hasFor();
+      case 2:
+        return hasFor() && hasTemplate();
+      case 3:
+        return hasFor() && hasTemplate() && hasFrom();
+      case 4:
+        return hasFor() && hasTemplate() && hasFrom() && hasSend();
+      default:
+        return false;
+    }
+  }, [activeStep, hasFor, hasTemplate, hasFrom, hasSend]);
 
   const handleQueryChange = (key, value) => {
     let newQuery = {
       ...query,
       [key]: value
     }
-    history.push({...location, search: queryString.stringify(newQuery)});
+    history.push({ ...location, search: queryString.stringify(newQuery) });
   }
 
   if (!urlIsValid()) {
@@ -182,11 +182,11 @@ const FeedbackRequestPage = () => {
         <Typography className={classes.requestHeader} variant="h4">Feedback Request for <b>{requestee?.name}</b></Typography>
         <div>
           <Button className={classes.actionButtons} onClick={onBackClick} disabled={activeStep <= 1}
-                  variant="contained">
+            variant="contained">
             Back
           </Button>
           <Button className={classes.actionButtons} onClick={onNextClick}
-                  variant="contained" disabled={!canProceed()} color="primary">
+            variant="contained" disabled={!canProceed()} color="primary">
             {activeStep === steps.length ? "Submit" : "Next"}
           </Button>
         </div>
@@ -205,9 +205,9 @@ const FeedbackRequestPage = () => {
         </Stepper>
       </div>
       <div className="current-step-content">
-        {activeStep === 1 && <FeedbackTemplateSelector changeQuery={(key, value) => handleQueryChange(key, value)} query={templateQuery}/> }
-        {activeStep === 2 && <FeedbackRecipientSelector/>}
-        {activeStep === 3 && <SelectDate/>}
+        {activeStep === 1 && <FeedbackTemplateSelector changeQuery={(key, value) => handleQueryChange(key, value)} query={templateQuery} />}
+        {activeStep === 2 && <FeedbackRecipientSelector />}
+        {activeStep === 3 && <SelectDate />}
       </div>
     </div>
   );
