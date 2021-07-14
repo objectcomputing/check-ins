5e11af413a8f0fedd945be55a15b1c36780612ba
Jefferson Brown <jeffedwardbrown@gmail.com>
2021-07-14 16:43:51 -0500

    Minor additions

--- server/src/main/java/com/objectcomputing/checkins/services/request_notifications/RequestNotifications.java
+++ server/src/main/java/com/objectcomputing/checkins/services/request_notifications/RequestNotifications.java
@@ -1,10 +1,14 @@
 package com.objectcomputing.checkins.services.request_notifications;
 
+import java.time.LocalDate;
+
 @PubSubListener
 public class RequestNotifications {
 
     @Subscription("Example")
     public void Check() {
+        LocalDate today = LocalDate.now();
+
         System.out.println("Work would be done here...");
     }
 
