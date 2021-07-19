6302f79e49608ac93ae3633ad8209205b6495044
pieperm <michaelepieper@gmail.com>
2021-07-19 15:44:11 -0500

    Remove redundant import

--- server/src/test/java/com/objectcomputing/checkins/services/fixture/RepositoryFixture.java
+++ server/src/test/java/com/objectcomputing/checkins/services/fixture/RepositoryFixture.java
@@ -8,7 +8,6 @@ import com.objectcomputing.checkins.services.checkins.CheckInRepository;
 import com.objectcomputing.checkins.services.feedback.FeedbackRepository;
 import com.objectcomputing.checkins.services.feedback_template.template_question.TemplateQuestionRepository;
 import com.objectcomputing.checkins.services.frozen_template.FrozenTemplateRepository;
-import com.objectcomputing.checkins.services.feedback_template.template_question.TemplateQuestionRepository;
 import com.objectcomputing.checkins.services.feedback_request.FeedbackRequestRepository;
 import com.objectcomputing.checkins.services.feedback_request_questions.FeedbackRequestQuestionRepository;
 import com.objectcomputing.checkins.services.feedback_template.FeedbackTemplateRepository;
