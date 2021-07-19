54072b2f41e5849d1b79bbb8bb9f80975c50478e
Jefferson Brown <jeffedwardbrown@gmail.com>
2021-07-19 14:53:40 -0500

    Saving progress

--- server/src/main/java/com/objectcomputing/checkins/services/request_notifications/RequestNotifications.java
+++ server/src/main/java/com/objectcomputing/checkins/services/request_notifications/RequestNotifications.java
@@ -1,14 +1,21 @@
 package com.objectcomputing.checkins.services.request_notifications;
 
+import com.objectcomputing.checkins.services.feedback_request.FeedbackRequest;
+
+import javax.inject.Singleton;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @PubSubListener
 public class RequestNotifications {
 
-    @Subscription("Example")
+
+    @Singleton
+    @Subscription("Example Topic")
     public void Check() {
         LocalDate today = LocalDate.now();
-
+        List<FeedbackRequest> TodaysRequest = feedbackReqServices.findByValues();
         System.out.println("Work would be done here...");
     }
 
