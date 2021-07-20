626fdfbebe2f1a88964dba2b4c726785838414c9
kimberlinc <cmkd7h@umsystem.edu>
2021-07-20 11:05:22 -0500

    Added link to notification content

--- web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.jsx
+++ web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.jsx
@@ -1,14 +1,16 @@
-import React, {useState} from "react";
+import React, { useContext, useCallback, useState } from "react";
+import queryString from 'query-string';
 import Typography from "@material-ui/core/Typography";
 import {makeStyles, withStyles} from '@material-ui/core/styles';
 import PropTypes from "prop-types";
 import {green} from '@material-ui/core/colors';
-
 import FeedbackSubmitQuestion from "../feedback_submit_question/FeedbackSubmitQuestion";
 import Button from "@material-ui/core/Button";
-
 import "./FeedbackSubmitForm.css";
-import {useHistory} from "react-router-dom";
+import {useLocation, useHistory} from "react-router-dom";
+import {AppContext} from "../../context/AppContext";
+import {selectCurrentUser} from "../../context/selectors";
+import {UPDATE_TOAST} from "../../context/actions";
 
 const useStyles = makeStyles({
   announcement: {
@@ -48,58 +50,92 @@ const ColorButton = withStyles({
 })(Button);
 
 const propTypes = {
-  requesteeName: PropTypes.string.isRequired,
+  requesteeName: PropTypes.string.isRequired
 }
+
+
+
+//const hasID = useCallback(() => {
+//
+//}, [idQuery])
+
 
 const FeedbackSubmitForm = (props) => {
   const classes = useStyles();
+  const location = useLocation();
+  const {state, dispatch} = useContext(AppContext);
+  const query = queryString.parse(location?.search);
+  const idQuery = query.id?.toString();
   const history = useHistory();
-  const handleClick = () => history.push('/feedback/submit/confirmation');
+  const currentUser = selectCurrentUser(state);
 
-  const [isReviewing, setIsReviewing] = useState(false);
 
-  return (
-    <div className="submit-form">
-      <Typography className={classes.announcement} variant="h3">Submitting Feedback on <b>{props.requesteeName}</b></Typography>
-      {sampleQuestions.map((sampleQuestion) => (
-        <FeedbackSubmitQuestion
-          key={sampleQuestion.id}
-          question={sampleQuestion.question}
-          questionNumber={sampleQuestion.id}
-          editable={!isReviewing}
-        />
-      ))}
-      <div className="submit-action-buttons">
-        {isReviewing ?
-          <React.Fragment>
-            <ColorButton
-              className={classes.button}
-              onClick={() => setIsReviewing(false)}
-              variant="contained"
-              color="primary">
-              Edit
-            </ColorButton>
-            <Button
-              className={classes.button}
-              onClick={handleClick}
-              variant="contained"
-              color="primary">
-              Submit
-            </Button>
-          </React.Fragment> :
-          <ColorButton
-            className={classes.button}
-            onClick={() => setIsReviewing(true)}
-            variant="contained"
-            color="primary">
-            Review
-          </ColorButton>
-        }
-      </div>
-    </div>
-  );
-};
+  const isIdValid = () => {
+    if (idQuery !== null && idQuery !== undefined) {
+      if (currentUser.id !== idQuery) {
+        history.push("/checkins");
+        dispatch({
+          type: UPDATE_TOAST,
+          payload: {
+            severity: "error",
+            toast: "You do not have Permission to Access this Request",
+          },
+        });
+      }
 
-FeedbackSubmitForm.propTypes = propTypes;
+    }
 
+  }
+
+  const FeedbackSubmitForm = (props) => {
+    const classes = useStyles();
+    const history = useHistory();
+    const handleClick = () => history.push('/feedback/submit/confirmation');
+
+    const [isReviewing, setIsReviewing] = useState(false);
+
+    return (
+        <div className="submit-form">
+          <Typography className={classes.announcement} variant="h3">Submitting Feedback on <b>{props.requesteeName}</b></Typography>
+          {sampleQuestions.map((sampleQuestion) => (
+              <FeedbackSubmitQuestion
+                  key={sampleQuestion.id}
+                  question={sampleQuestion.question}
+                  questionNumber={sampleQuestion.id}
+                  editable={!isReviewing}
+              />
+          ))}
+          <div className="submit-action-buttons">
+            {isReviewing ?
+                <React.Fragment>
+                  <ColorButton
+                      className={classes.button}
+                      onClick={() => setIsReviewing(false)}
+                      variant="contained"
+                      color="primary">
+                    Edit
+                  </ColorButton>
+                  <Button
+                      className={classes.button}
+                      onClick={handleClick}
+                      variant="contained"
+                      color="primary">
+                    Submit
+                  </Button>
+                </React.Fragment> :
+                <ColorButton
+                    className={classes.button}
+                    onClick={() => setIsReviewing(true)}
+                    variant="contained"
+                    color="primary">
+                  Review
+                </ColorButton>
+            }
+          </div>
+        </div>
+    );
+  };
+
+  FeedbackSubmitForm.propTypes = propTypes;
+}
 export default FeedbackSubmitForm;
