9217474203024cfece48a10eb01e5dc155675474
Jefferson Brown <jeffedwardbrown@gmail.com>
2021-07-23 15:52:14 -0500

    Finished 1292

--- web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.jsx
+++ web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.jsx
@@ -7,6 +7,7 @@ import FeedbackSubmitQuestion from "../feedback_submit_question/FeedbackSubmitQu
 import Button from "@material-ui/core/Button";
 import "./FeedbackSubmitForm.css";
 import {useHistory} from "react-router-dom";
+import {Alert, AlertTitle} from "@material-ui/lab";
 
 const useStyles = makeStyles({
   announcement: {
@@ -15,6 +16,11 @@ const useStyles = makeStyles({
       fontSize: "22px"
     }
   },
+
+  warning: {
+    marginTop: "20px"
+  },
+
   button: {
     margin: "3em 1em 1em 1em"
   }
@@ -49,7 +55,6 @@ const propTypes = {
   requesteeName: PropTypes.string.isRequired
 }
 
-const FeedbackSubmitForm = (props) => {
   const FeedbackSubmitForm = (props) => {
     const classes = useStyles();
     const history = useHistory();
@@ -60,6 +65,13 @@ const FeedbackSubmitForm = (props) => {
     return (
         <div className="submit-form">
           <Typography className={classes.announcement} variant="h3">Submitting Feedback on <b>{props.requesteeName}</b></Typography>
+          {isReviewing ?
+            <Alert className={classes.warning} severity="warning">
+              <AlertTitle>Notice!</AlertTitle>
+              Feedback is not anonymous, and can be seen by more than just the feedback Requester.
+              <strong> Be mindful of your answers, for you never know who will see it!</strong>
+            </Alert> : null
+          }
           {sampleQuestions.map((sampleQuestion) => (
               <FeedbackSubmitQuestion
                   key={sampleQuestion.id}
@@ -100,5 +112,4 @@ const FeedbackSubmitForm = (props) => {
   };
 
   FeedbackSubmitForm.propTypes = propTypes;
-}
 export default FeedbackSubmitForm;
