7cc0a0f3808af6c758be8824ab99198f98689a2f
pieperm <michaelepieper@gmail.com>
2021-07-26 14:58:50 -0500

    Adjust tests

--- server/src/test/java/com/objectcomputing/checkins/services/feedback_answer/FeedbackAnswerControllerTest.java
+++ server/src/test/java/com/objectcomputing/checkins/services/feedback_answer/FeedbackAnswerControllerTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
+import java.lang.reflect.Member;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,15 +28,19 @@ public class FeedbackAnswerControllerTest extends TestContainersSuite implements
     @Client("/services/feedback/answers")
     HttpClient client;
 
-    public FeedbackAnswer saveSampleAnswer(MemberProfile sender, MemberProfile recipient) {
+    public FeedbackAnswer createSampleAnswer(MemberProfile sender, MemberProfile recipient) {
         createDefaultRole(RoleType.PDL, sender);
         MemberProfile requestee = createADefaultMemberProfileForPdl(sender);
         MemberProfile templateCreator = createADefaultSupervisor();
-        FeedbackRequest feedbackRequest = createFeedbackRequest(sender, requestee, recipient);
         FeedbackTemplate template = createFeedbackTemplate(templateCreator.getId());
         getFeedbackTemplateRepository().save(template);
+        FeedbackRequest feedbackRequest = createFeedbackRequest(sender, requestee, recipient, template.getId());
         TemplateQuestion question = saveTemplateQuestion(template, 1);
-        return createFeedbackAnswer(question.getId(), feedbackRequest.getId());
+        return createSampleFeedbackAnswer(question.getId(), feedbackRequest.getId());
+    }
+
+    public FeedbackAnswer saveSampleAnswer(MemberProfile sender, MemberProfile recipient) {
+        return getFeedbackAnswerRepository().save(createSampleAnswer(sender, recipient));
     }
 
     FeedbackAnswerCreateDTO createDTO(FeedbackAnswer feedbackAnswer) {
@@ -73,7 +78,7 @@ public class FeedbackAnswerControllerTest extends TestContainersSuite implements
 
         MemberProfile sender = createASecondDefaultMemberProfile();
         MemberProfile recipient = createADefaultRecipient();
-        FeedbackAnswerCreateDTO dto = createDTO(saveSampleAnswer(sender, recipient));
+        FeedbackAnswerCreateDTO dto = createDTO(createSampleAnswer(sender, recipient));
 
         final HttpRequest<?> request = HttpRequest.POST("", dto)
                 .basicAuth(admin.getWorkEmail(), RoleType.Constants.ADMIN_ROLE);
@@ -87,9 +92,9 @@ public class FeedbackAnswerControllerTest extends TestContainersSuite implements
     void testPostAnswerByRecipient() {
         MemberProfile sender = createADefaultMemberProfile();
         MemberProfile recipient = createADefaultRecipient();
-
-        FeedbackAnswer feedbackAnswer = saveSampleAnswer(sender, recipient);
+        FeedbackAnswer feedbackAnswer = createSampleAnswer(sender, recipient);
         FeedbackAnswerCreateDTO dto = createDTO(feedbackAnswer);
+
         final HttpRequest<?> request = HttpRequest.POST("", dto)
                 .basicAuth(recipient.getWorkEmail(), RoleType.Constants.MEMBER_ROLE);
         final HttpResponse<FeedbackAnswerResponseDTO> response = client.toBlocking().exchange(request, FeedbackAnswerResponseDTO.class);
@@ -103,7 +108,7 @@ public class FeedbackAnswerControllerTest extends TestContainersSuite implements
     void testPostBySenderUnauthorized() {
         MemberProfile sender = createADefaultMemberProfile();
         MemberProfile recipient = createADefaultRecipient();
-        FeedbackAnswer feedbackAnswer = saveSampleAnswer(sender, recipient);
+        FeedbackAnswer feedbackAnswer = createSampleAnswer(sender, recipient);
         FeedbackAnswerCreateDTO dto = createDTO(feedbackAnswer);
         final HttpRequest<?> request = HttpRequest.POST("", dto)
                 .basicAuth(sender.getWorkEmail(), RoleType.Constants.PDL_ROLE);
@@ -112,7 +117,6 @@ public class FeedbackAnswerControllerTest extends TestContainersSuite implements
 
         assertUnauthorized(exception);
     }
-
 
     @Test
     void testUpdateByRecipient() {
