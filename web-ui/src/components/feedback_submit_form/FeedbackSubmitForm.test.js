0f4dd00090d52c99454e078e48fd1d50c2a544a2
Mahyar Moshiri <moshirim@OCI1868MBP16INCH2019.local>
2021-07-27 16:01:08 -0500

    Fixed snapshot

--- web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.test.js
+++ web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.test.js
@@ -2,5 +2,5 @@ import React from "react";
 import FeedbackSubmitForm from "./FeedbackSubmitForm";
 
 it("renders the feedback submit form", async () => {
-  shallowSnapshot(<FeedbackSubmitForm requesteeName="John Doe"/>);
+  shallowSnapshot(<FeedbackSubmitForm requesteeName="John Doe" requestId={'ab7b21d4-f88c-4494-9b0b-8541636025eb'}/>);
 });
