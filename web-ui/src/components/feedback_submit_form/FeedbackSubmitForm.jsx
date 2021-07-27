9e0a0041d91a522ac913ecb89ae77366e9cd8766
kimberlinc <cmkd7h@umsystem.edu>
2021-07-27 14:57:19 -0500

    Added URL validation for requestees

--- web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.jsx
+++ web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.jsx
@@ -1,5 +1,4 @@
-import React, {useContext, useState} from "react";
-import queryString from 'query-string';
+import React, {useState} from "react";
 import Typography from "@material-ui/core/Typography";
 import {makeStyles, withStyles} from '@material-ui/core/styles';
 import PropTypes from "prop-types";
@@ -8,10 +7,7 @@ import FeedbackSubmitQuestion from "../feedback_submit_question/FeedbackSubmitQu
 import Button from "@material-ui/core/Button";
 import "./FeedbackSubmitForm.css";
 import {Alert, AlertTitle} from "@material-ui/lab";
-import {useHistory, useLocation} from "react-router-dom";
-import {AppContext} from "../../context/AppContext";
-import {selectCurrentUser} from "../../context/selectors";
-import {UPDATE_TOAST} from "../../context/actions";
+import {useHistory} from "react-router-dom";
 
 const useStyles = makeStyles({
   announcement: {
@@ -64,30 +60,8 @@ const FeedbackSubmitForm = (props) => {
   const classes = useStyles();
   const handleClick = () => history.push(`/feedback/submit/confirmation/?request=${props.requestId}`);
   const [isReviewing, setIsReviewing] = useState(false);
-  const location = useLocation();
-  const {state, dispatch} = useContext(AppContext);
-  const query = queryString.parse(location?.search);
-  const idQuery = query.id?.toString();
   const history = useHistory();
-  const currentUser = selectCurrentUser(state);
 
-
-  const isIdValid = () => {
-    if (idQuery !== null && idQuery !== undefined) {
-      if (currentUser.id !== idQuery) {
-        history.push("/checkins");
-        dispatch({
-          type: UPDATE_TOAST,
-          payload: {
-            severity: "error",
-            toast: "You do not have Permission to Access this Request",
-          },
-        });
-      }
-
-    }
-
-  }
 
     return (
         <div className="submit-form">
