26a53892fba8a5f2d68ff49eb8a0fadc71ed91d1
Mahyar Moshiri <66429937+moshirim@users.noreply.github.com>
2021-07-28 11:28:46 -0500

    Delete FeedbackSubmitForm.test.js

--- web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.test.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import FeedbackSubmitForm from "./FeedbackSubmitForm";
-
-it("renders the feedback submit form", async () => {
-  shallowSnapshot(<FeedbackSubmitForm requesteeName="John Doe" requestId={'ab7b21d4-f88c-4494-9b0b-8541636025eb'}/>);
-});
