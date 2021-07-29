ea429e5d548cb3212c86efdfe499e3b75f7049e5
Jesse Hanner <hannerj@objectcomputing.com>
2021-07-29 16:12:03 -0500

    moved error boundary to App.js

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 import { createBrowserHistory } from "history";
 
 import Routes from "./components/routes/Routes";
@@ -12,28 +13,37 @@ import DateFnsUtils from "@date-io/date-fns";
 
 import "./App.css";
 
-
 const customHistory = createBrowserHistory();
 
 function App() {
+  const ErrorFallback = ({ error }) => {
+    return (
+      <div role="alert">
+        <p>Something went wrong :/</p>
+        <pre style={{ color: "red" }}>{error.message}</pre>
+      </div>
+    );
+  };
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Router history={customHistory}>
         <AppContextProvider>
-          <div>
-            <Menu />
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
-              className="App"
-            >
-              <Routes />
+          <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <div>
+              <Menu />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+                className="App"
+              >
+                <Routes />
+              </div>
+              <SnackBarWithContext />
             </div>
-            <SnackBarWithContext />
-          </div>
+          </ErrorBoundary>
         </AppContextProvider>
       </Router>
     </MuiPickersUtilsProvider>
