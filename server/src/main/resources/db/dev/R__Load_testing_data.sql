c9191132c29a59cd226abda0ed8bee476b5c8e00
pieperm <michaelepieper@gmail.com>
2021-07-29 15:32:22 -0500

    Populate view page with request and template info, update snapshots, update Storybook

--- server/src/main/resources/db/dev/R__Load_testing_data.sql
+++ server/src/main/resources/db/dev/R__Load_testing_data.sql
@@ -12,11 +12,9 @@ delete from questions;
 delete from role;
 delete from team_member;
 delete from team;
-delete from frozen_template_questions;
-delete from frozen_templates;
 delete from template_questions;
-delete from feedback_templates;
 delete from feedback_requests;
+delete from feedback_templates;
 delete from member_profile;
 
 INSERT INTO member_profile
@@ -437,3 +435,8 @@ INSERT INTO feedback_requests
 (id, creator_id, requestee_id, recipient_id, template_id, send_date, due_date, submit_date, status)
 VALUES
 ('ab7b21d4-f88c-4494-9b0b-8541636025eb', '7a6a2d4e-e435-4ec9-94d8-f1ed7c779498', 'b2d35288-7f1e-4549-aa2b-68396b162490', '67dc3a3b-5bfa-4759-997a-fb6bac98dcf3', '97b0a312-e5dd-46f4-a600-d8be2ad925bb', '2020-07-07', null, null, 'pending');
+
+INSERT INTO feedback_requests
+(id, creator_id, requestee_id, recipient_id, template_id, send_date, due_date, submit_date, status)
+VALUES
+('c15961e4-6e9b-42cd-8140-ece9efe2445c', '7a6a2d4e-e435-4ec9-94d8-f1ed7c779498', 'b2d35288-7f1e-4549-aa2b-68396b162490', '2c1b77e2-e2fc-46d1-92f2-beabbd28ee3d', '97b0a312-e5dd-46f4-a600-d8be2ad925bb', '2020-07-07', null, '2020-07-08', 'submitted');
