64d86ddf0187f6cab242e26b4e7f10c7d9cca6db
Jesse Hanner <hannerj@objectcomputing.com>
2021-07-29 16:12:03 -0500

    moved error boundary to App.js

--- web-ui/src/components/routes/Routes.jsx
+++ web-ui/src/components/routes/Routes.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import { Switch, Route } from "react-router-dom";
 
 import AdminPage from "../../pages/AdminPage";
@@ -22,88 +21,77 @@ import TeamSkillReportPage from "../../pages/TeamSkillReportPage";
 import BirthdayAnniversaryReportPage from "../../pages/BirthdayAnniversaryReportPage";
 import { selectIsAdmin } from "../../context/selectors";
 
-function ErrorFallback({ error }) {
-  return (
-    <div role="alert">
-      <p>Something went wrong :/</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
-    </div>
-  );
-}
-
 export default function Routes() {
   const { state } = useContext(AppContext);
 
   const isAdmin = selectIsAdmin(state);
 
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Switch>
-        <Route path="/teams">
-          <Header title="Teams">
-            <GroupIcon fontSize="large" />
-          </Header>
-          <TeamsPage />
-        </Route>
-        <Route path="/guilds">
-          <Header title="Guilds">
-            <GroupIcon fontSize="large" />
-          </Header>
-          <GuildsPage />
-        </Route>
-        <Route path="/home">
-          <Header />
-          <HomePage />
-        </Route>
-        <Route path="/people">
-          <Header title="People" />
-          <PeoplePage />
-        </Route>
-        <Route path="/checkins/:memberId?/:checkinId?">
-          <Header title="Check-ins" />
-          <CheckinsPage />
-        </Route>
-        <Route path="/profile/:memberId?">
-          <Header title="Member Profile" />
-          <MemberProfilePage />
-        </Route>
+    <Switch>
+      <Route path="/teams">
+        <Header title="Teams">
+          <GroupIcon fontSize="large" />
+        </Header>
+        <TeamsPage />
+      </Route>
+      <Route path="/guilds">
+        <Header title="Guilds">
+          <GroupIcon fontSize="large" />
+        </Header>
+        <GuildsPage />
+      </Route>
+      <Route path="/home">
+        <Header />
+        <HomePage />
+      </Route>
+      <Route path="/people">
+        <Header title="People" />
+        <PeoplePage />
+      </Route>
+      <Route path="/checkins/:memberId?/:checkinId?">
+        <Header title="Check-ins" />
+        <CheckinsPage />
+      </Route>
+      <Route path="/profile/:memberId?">
+        <Header title="Member Profile" />
+        <MemberProfilePage />
+      </Route>
 
-        {isAdmin && (
-          <Switch>
-            <Route path="/edit-skills">
-              <Header title="Skills" />
-              <EditSkillsPage />
-            </Route>
-            <Route path="/checkins-reports">
-              <Header title="Check-in Report" />
-              <CheckinsReportPage />
-            </Route>
-            <Route path="/skills-reports">
-              <Header title="Skill Reports" />
-              <SkillReportPage />
-            </Route>
-            <Route path="/team-skills-reports">
-              <Header title="Team Skill Reports" />
-              <TeamSkillReportPage />
-            </Route>
-            <Route path="/birthday-anniversary-reports">
-              <Header title="Birthday & Anniversary Reports" />
-              <BirthdayAnniversaryReportPage />
-            </Route>
-            <Route path="/admin">
-              <Header title="Admin">
-                <GroupIcon fontSize="large" />
-              </Header>
-              <AdminPage />
-            </Route>
-          </Switch>
-        )}
+      {isAdmin && (
+        <Switch>
+          <Route path="/edit-skills">
+            <Header title="Skills" />
+            <EditSkillsPage />
+          </Route>
+          <Route path="/checkins-reports">
+            <Header title="Check-in Report" />
+            <CheckinsReportPage />
+          </Route>
+          <Route path="/skills-reports">
+            <Header title="Skill Reports" />
+            <SkillReportPage />
+          </Route>
+          <Route path="/team-skills-reports">
+            <Header title="Team Skill Reports" />
+            <TeamSkillReportPage />
+          </Route>
+          <Route path="/birthday-anniversary-reports">
+            <Header title="Birthday & Anniversary Reports" />
+            <BirthdayAnniversaryReportPage />
+          </Route>
+          <Route path="/admin">
+            <Header title="Admin">
+              <GroupIcon fontSize="large" />
+            </Header>
+            <AdminPage />
+          </Route>
+        </Switch>
+      )}
 
-        <Route path="/">
-          <Header />
-          <ProfilePage />
-        </Route>
-      </Switch>
-    </ErrorBoundary>
+      <Route path="/">
+        <Header />
+        <ProfilePage />
+      </Route>
+    </Switch>
   );
 }
