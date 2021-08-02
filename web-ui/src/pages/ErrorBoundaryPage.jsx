54bcca46e15936099ad94ae90e0f319477335a00
Jesse Hanner <hannerj@objectcomputing.com>
2021-08-02 13:57:52 -0500

    issue creating working, just need token and to add it to secrets

--- web-ui/src/pages/ErrorBoundaryPage.jsx
+++ web-ui/src/pages/ErrorBoundaryPage.jsx
@@ -1,7 +1,5 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 
-import { AppContext } from "../context/AppContext";
-import { selectCsrfToken } from "../context/selectors";
 import { Octokit } from "@octokit/core";
 import { UPDATE_TOAST } from "../context/actions";
 
@@ -10,48 +8,57 @@ import { Button, Modal, TextField } from "@material-ui/core";
 import "./ErrorBoundaryPage.css";
 
 const ErrorFallback = ({ error }) => {
-  const { state } = useContext(AppContext);
-
   const [open, setOpen] = useState(false);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const [owner, setOwner] = useState("");
+
   const close = () => {
     setOpen(false);
   };
 
   const octokit = new Octokit({
-    auth: "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",
+    // need to generate personal access token with no expiration date or perhaps register as OAuth. This token expires in 1 day
+    auth: "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",
   });
 
   const createGitIssue = async () => {
-    await octokit.request("POST /repos/oci-labs/check-ins/issues", {
-      title: "New issue",
+    let res = await octokit.request("POST /repos/oci-labs/check-ins/issues", {
+      title: title,
       body: body,
     });
+    if (res.status === 201) {
+      window.snackDispatch({
+        type: UPDATE_TOAST,
+        payload: {
+          severity: "success",
+          toast: `New issue ${title} created! Gratzie &#128512`,
+        },
+      });
+    }
   };
 
   return (
     <div className="error-boundary" role="alert">
       <div className="message-section">
-        <h1 className="message">Something went wrong &#128533;</h1>
+        <h1 className="message">
+          Something went wrong
+          <span aria-label="sad face" role="img">
+            &#128533;
+          </span>
+        </h1>
         <h1 className="error">Error: {error.message || error}</h1>
         <h1 className="new-issue-message">
           Would you mind creating a new issue and explaining what happened?
         </h1>
         <Button
-          className="create-new-issue"
+          className="create-new-issue-button"
           color="primary"
           onClick={() => setOpen(true)}
         >
           Create New Issue
         </Button>
       </div>
-      <Modal
-        open={open}
-        onClose={close}
-        aria-labelledby="edit-guild-modal-title"
-      >
+      <Modal open={open} onClose={close}>
         <div className="create-new-issue-modal">
           <TextField
             required
@@ -71,32 +78,13 @@ const ErrorFallback = ({ error }) => {
             value={body}
             onChange={(e) => setBody(e.target.value)}
           />
-          <TextField
-            required
-            id="new-issue-owner"
-            label="Owner"
-            className="fullWidth"
-            placeholder="What's your GitHub name?"
-            value={owner}
-            onChange={(e) => setOwner(e.target.value)}
-          />
-          <div className="create-new-issue-actions halfWidth">
+          <div className="create-new-issue-modal-actions halfWidth">
             <Button onClick={close} color="secondary">
               Cancel
             </Button>
             <Button
               onClick={() => {
-                let res = createGitIssue();
-                console.log({ res });
-                if (!res.error && res.status === 201) {
-                  window.snackDispatch({
-                    type: UPDATE_TOAST,
-                    payload: {
-                      severity: "success",
-                      toast: `New issue ${title} created! Gratzie &#128512`,
-                    },
-                  });
-                }
+                createGitIssue();
                 close();
               }}
               color="primary"
