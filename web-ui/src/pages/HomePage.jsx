90274a3f3235e422d381201964e3f699b7617699
Jesse Hanner <hannerj@objectcomputing.com>
2021-08-03 14:26:04 -0500

    added h1 to homepage to avoid confusion

--- web-ui/src/pages/HomePage.jsx
+++ web-ui/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function HomePage() {
   return (
     <div>
-      
+      <h1>New HomePage Coming Soon!</h1>
     </div>
-  )
+  );
 }
