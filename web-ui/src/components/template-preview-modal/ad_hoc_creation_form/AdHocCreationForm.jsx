a778aa7bc0cccbf70abf9da60923ef0d22e206c6
Jefferson Brown <jeffedwardbrown@gmail.com>
2021-08-05 12:42:07 -0500

    Starting 1362

--- web-ui/src/components/template-preview-modal/ad_hoc_creation_form/AdHocCreationForm.jsx
+++ web-ui/src/components/template-preview-modal/ad_hoc_creation_form/AdHocCreationForm.jsx
@@ -1,22 +1,37 @@
 import React, {useEffect, useState} from "react";
-import {TextField} from "@material-ui/core";
+import {FormControlLabel, TextField, Tooltip} from "@material-ui/core";
+import Checkbox from '@material-ui/core/Checkbox';
+import {withStyles} from '@material-ui/core/styles'
 import PropTypes from "prop-types";
-
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 const propTypes = {
   onFormChange: PropTypes.func
 }
+
+const BlueCheckbox = withStyles({
+  root: {
+    color: '#0000ff',
+    '&$checked': {
+      color: '#0000ff',
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
 
 const AdHocCreationForm = (props) => {
 
   const [title, setTitle] = useState("Ad Hoc");
   const [description, setDescription] = useState("");
   const [question, setQuestion] = useState("");
+  const [isPublic, setIsPublic] = useState(false);
 
   useEffect(() => {
     props.onFormChange({
       title: title,
       description: description,
-      question: question
+      question: question,
     }); // eslint-disable-next-line
   }, [title, description, question]);
 
@@ -51,6 +66,20 @@ const AdHocCreationForm = (props) => {
         onChange={(event) => {
           setQuestion(event.target.value);
         }}/>
+      <FormControlLabel
+        control={
+          <BlueCheckbox
+            icon={<CheckBoxOutlineBlankIcon fontSize="small" />}
+            checkedIcon={<CheckBoxIcon fontSize="small" />}
+            name="checkedI"
+            onClick={setIsPublic(!isPublic)}
+          />
+        }
+        label="Make template public?"
+      />
+      <Tooltip title="Check this box if you want the template to be accessed by the public" placement="left" arrow>
+        <HelpOutlineIcon style={{color: "gray"}}/>
+      </Tooltip>
     </React.Fragment>
   );
 }
