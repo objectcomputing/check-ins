34d31aface5bd229b61b5ca3e4636cc8289c0874
HootDunk <hootmanj@objectcomputing.com>
2021-08-06 11:54:50 -0500

    refactor to use reactive client

--- server/src/main/java/com/objectcomputing/checkins/services/github/GithubController.java
+++ server/src/main/java/com/objectcomputing/checkins/services/github/GithubController.java
@@ -1,7 +1,5 @@
 package com.objectcomputing.checkins.services.github;
 
-import com.objectcomputing.checkins.services.guild.GuildCreateDTO;
-import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
@@ -11,7 +9,7 @@ import io.micronaut.http.annotation.Produces;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.micronaut.core.async.annotation.SingleResult;
+import org.reactivestreams.Publisher;
 
 import javax.validation.Valid;
 
@@ -31,17 +29,14 @@ public class GithubController {
     /**
      * Create and save a new github issue
      *
-     * @param request, {@link GithubRequestDTO}
+     * @param request, {@link IssueCreateDTO}
      * @return {@link HttpResponse < GithubRequestDTO >}
      */
 
-    @Post()
-    @SingleResult
-    HttpResponse sendIssueToGithub(@Body @Valid GithubRequestDTO request) {
-        githubClient.sendIssue(request);
-        return HttpResponse.ok();
+    @Post
+    Publisher<IssueResponseDTO> sendIssue(@Body @Valid IssueCreateDTO request) {
+        return githubClient.sendIssue(request);
     }
-
 
 
 }
