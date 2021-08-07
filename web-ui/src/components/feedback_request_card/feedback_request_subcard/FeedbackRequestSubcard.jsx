9634737d30cd0e536e0f115474b67807aafd9dd4
pieperm <michaelepieper@gmail.com>
2021-08-07 04:08:08 -0500

    Destructure dates from props, remove props from Submitted component

--- web-ui/src/components/feedback_request_card/feedback_request_subcard/FeedbackRequestSubcard.jsx
+++ web-ui/src/components/feedback_request_card/feedback_request_subcard/FeedbackRequestSubcard.jsx
@@ -18,16 +18,13 @@ import {makeStyles} from "@material-ui/core/styles";
 const useStyles = makeStyles({
   redTypography: {
     color: "#FF0000"
-    },
-
+  },
   yellowTypography: {
       color: "#EE8C00"
-    },
-
+  },
   greenTypography: {
       color: "#006400"
-    },
-
+  },
   darkGrayTypography: {
     color: "#333333"
   }
@@ -39,18 +36,19 @@ const propTypes = {
   request: PropTypes.object.isRequired,
 }
 
-const FeedbackRequestSubcard = (props) => {
+const FeedbackRequestSubcard = ({ request }) => {
   const {state} = useContext(AppContext);
   const csrf = selectCsrfToken(state);
   const classes = useStyles();
-  const recipient = selectProfile(state, props.request?.recipientId);
-  const submitDate = props.request?.submitDate ? dateFns.format(new Date(props.request.submitDate.join("-")), "LLLL dd, yyyy") : null;
-  const dueDate = props.request?.dueDate ? dateFns.format(new Date(props.request.dueDate.join("-")), "LLLL dd, yyyy"): null;
-  const sendDate = dateFns.format(new Date(props.request.sendDate.join("-")), "LLLL dd, yyyy");
+  let { submitDate, dueDate, sendDate } = request;
+  const recipient = selectProfile(state, request?.recipientId);
+  submitDate = submitDate ? dateFns.format(new Date(submitDate.join("-")), "LLLL dd, yyyy") : null;
+  dueDate = dueDate ? dateFns.format(new Date(dueDate.join("-")), "LLLL dd, yyyy"): null;
+  sendDate = dateFns.format(new Date(sendDate.join("-")), "LLLL dd, yyyy");
 
-  const handleReminderNotification = async() => {
+  const handleReminderNotification = async () => {
     if (csrf) {
-      let res = await sendReminderNotification(props.request.id, [recipient.email], csrf);
+      let res = await sendReminderNotification(request.id, [recipient.email], csrf);
       let reminderResponse =
         res &&
         res.payload &&
@@ -80,23 +78,19 @@ const FeedbackRequestSubcard = (props) => {
     handleReminderNotification();
   }
 
-  const Submitted = (props) => {
-      if (props.dueDate) {
-        let today = new Date();
-        let due = new Date(props.dueDate);
-        if ((!props.submitDate) && today > due) {
-          return (
-            <Typography className={classes.redTypography}>Overdue</Typography>
-          )
-        }
+  const Submitted = () => {
+    if (request.dueDate) {
+      let today = new Date();
+      let due = new Date(request.dueDate);
+      if (!request.submitDate && today > due) {
+        return <Typography className={classes.redTypography}>Overdue</Typography>;
       }
-      if (props.submitDate) {
-        return <Typography className={classes.greenTypography}>Submitted {submitDate}</Typography>
-      } else
-        return <Typography className={classes.yellowTypography}>Not Submitted</Typography>
     }
-
-    Submitted.propTypes = {submitDate: PropTypes.arrayOf(PropTypes.string)};
+    if (request.submitDate) {
+      return <Typography className={classes.greenTypography}>Submitted {submitDate}</Typography>;
+    } else
+      return <Typography className={classes.yellowTypography}>Not Submitted</Typography>;
+  }
 
   return (
     <React.Fragment>
@@ -117,13 +111,13 @@ const FeedbackRequestSubcard = (props) => {
             </Grid>
             <Grid item xs={3}>
               <Typography className={classes.darkGrayTypography} variant= "body1">Sent on {sendDate}</Typography>
-              <Typography variant="body2">{props.request?.dueDate ? `Due on ${dueDate}` : "No due date"}</Typography>
+              <Typography variant="body2">{request?.dueDate ? `Due on ${dueDate}` : "No due date"}</Typography>
             </Grid>
             <Grid item xs={3}>
-              <Submitted dueDate={props.request?.dueDate} submitDate={props.request?.submitDate}/>
+              <Submitted/>
             </Grid>
             <Grid item xs={2} className="align-end">
-              {props.request && !props.request.submitDate &&
+              {request && !request.submitDate &&
                 <Tooltip title={"Send Reminder"} aria-label={"Send Reminder"}>
                   <IconButton
                     onClick={handleReminderClick}
@@ -133,7 +127,7 @@ const FeedbackRequestSubcard = (props) => {
                   </IconButton>
                 </Tooltip>
               }
-              {props.request.submitDate ? <Link to="" className="response-link"> View response </Link> : null}
+              {request.submitDate ? <Link to="" className="response-link">View response</Link> : null}
             </Grid>
           </Grid>
         </Grid>
