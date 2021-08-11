6a134fa6b536aeeed7537dfb5358f23a6f4b57eb
pieperm <michaelepieper@gmail.com>
2021-08-11 12:28:45 -0500

    Create search and filter fields

--- web-ui/src/components/view_feedback_responses/ViewFeedbackResponses.jsx
+++ web-ui/src/components/view_feedback_responses/ViewFeedbackResponses.jsx
@@ -1,5 +1,5 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Typography} from '@material-ui/core';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
+import {TextField, Typography} from '@material-ui/core';
 import "./ViewFeedbackResponses.css";
 import {makeStyles} from '@material-ui/core/styles';
 import FeedbackResponseCard from "./feedback_response_card/FeedbackResponseCard";
@@ -9,22 +9,46 @@ import {useLocation} from "react-router-dom";
 import {AppContext} from "../../context/AppContext";
 import {selectCsrfToken} from "../../context/selectors";
 import {UPDATE_TOAST} from "../../context/actions";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import {Group as GroupIcon, Search as SearchIcon} from "@material-ui/icons";
 
-const useStylesCardContent = makeStyles({
+const useStyles = makeStyles({
   root: {
     '&:last-child': {
       paddingBottom: '16px',
     }
+  },
+  notFoundMessage: {
+    color: "gray",
+    marginTop: "3em",
+    textAlign: "center"
   }
 }, {name: "MuiCardContent"});
 
 const ViewFeedbackResponses = () => {
-  useStylesCardContent();
+  const classes = useStyles();
   const { state } = useContext(AppContext);
   const csrf = selectCsrfToken(state);
   const location = useLocation();
   const [query, setQuery] = useState({});
   const [questionsAndAnswers, setQuestionsAndAnswers] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredResponders, setFilteredResponders] = useState([]);
+
+  const getFilteredResponses = useCallback(() => {
+    if (!questionsAndAnswers) {
+      return null
+    } else if (questionsAndAnswers.length === 0) {
+      return <Typography className={classes.notFoundMessage} variant="h5">No responses found</Typography>
+    }
+
+    let responsesToDisplay = [];
+
+    if (responsesToDisplay.length === 0) {
+      return <Typography className={classes.notFoundMessage} variant="h5">No matching responses</Typography>
+    }
+
+  }, [questionsAndAnswers]);
 
   useEffect(() => {
     setQuery(queryString.parse(location?.search));
@@ -42,6 +66,7 @@ const ViewFeedbackResponses = () => {
 
     retrieveQuestionsAndAnswers(query.request, csrf).then((res) => {
       if (res) {
+        console.log(res);
         setQuestionsAndAnswers(res);
       } else {
         window.snackDispatch({
@@ -62,6 +87,24 @@ const ViewFeedbackResponses = () => {
         style={{textAlign: "center", marginBottom: "1em"}}>
         View Feedback for <b>Joe Johnson</b>
       </Typography>
+      <div className="responses-filter-container">
+        <TextField
+          style={{marginRight: "3em"}}
+          label="Search responses..."
+          placeholder="Enter a keyword or phrase"
+          value={searchText}
+          onChange={(event) => setSearchText(event.target.value)}
+          InputProps={{
+            endAdornment: <InputAdornment style={{color: "gray"}} position="end"><SearchIcon/></InputAdornment>
+          }}
+        />
+        <TextField
+          label="Filter responders"
+          InputProps={{
+            endAdornment: <InputAdornment style={{color: "gray"}} position="end"><GroupIcon/></InputAdornment>
+          }}
+        />
+      </div>
 
       {questionsAndAnswers.map((question) => {
         return (
