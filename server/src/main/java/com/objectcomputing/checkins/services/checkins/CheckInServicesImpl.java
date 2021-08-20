e1e6222333bc541ce01372a99773c63ba843e8ca
HootDunk <hootmanj@objectcomputing.com>
2021-08-20 13:39:42 -0500

    updated logic to accomodate changes to the Role table and RoleService

--- server/src/main/java/com/objectcomputing/checkins/services/checkins/CheckInServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/checkins/CheckInServicesImpl.java
@@ -51,12 +51,14 @@ public class CheckInServicesImpl implements CheckInServices {
             throw new NotFoundException(String.format("Checkin %s not found", checkinId));
         });
 
-        Set<Role> memberRoles = roleServices.findByFields(RoleType.ADMIN, memberTryingToGainAccess.getId());
-        boolean isAdmin = !memberRoles.isEmpty();
+        boolean isAdmin = false;
+        if (roleServices.findByRole(RoleType.ADMIN).isPresent()){
+            isAdmin = roleServices.findUserRoles(memberTryingToGainAccess.getId())
+                    .contains(roleServices.findByRole(RoleType.ADMIN).get());
+            LOG.debug("Member is Admin: {}", isAdmin);
+        }
 
-        LOG.debug("Member is Admin: {}", isAdmin);
-
-        if (isAdmin) {
+        if(isAdmin){
             grantAccess = true;
         } else {
             MemberProfile teamMemberOnCheckin = memberRepo.findById(checkinRecord.getTeamMemberId()).orElseThrow(() -> {
