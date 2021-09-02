23dadb09cef16a45017541ac86c8bed7cb869aab
Jesse Hanner <hannerj@objectcomputing.com>
2021-09-02 12:22:40 -0500

    added space and - after Billable Hours:

--- web-ui/src/components/contribution_hours/ProgressBar.jsx
+++ web-ui/src/components/contribution_hours/ProgressBar.jsx
@@ -59,9 +59,9 @@ const LinearBuffer = ({
         color="textSecondary"
         style={{ display: "block" }}
       >
-        Billable Hours: {billableHours}
-        Contribution Hours: {contributionHours} - Target Hours: {targetHours} -
-        PTO Hours: {ptoHours}
+        Billable Hours: {billableHours} - Contribution Hours:{" "}
+        {contributionHours} - Target Hours: {targetHours} - PTO Hours:{" "}
+        {ptoHours}
       </Typography>
     </div>
   );
