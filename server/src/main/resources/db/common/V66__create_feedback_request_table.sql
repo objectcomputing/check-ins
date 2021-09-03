01a383265316bc35f9ebb3ac8ba842f93408ae32
Michael Kimberlin <mkimberlin@gmail.com>
2021-09-03 15:23:13 -0500

    Combined trouble migrations to avoid numbering issue

--- server/src/main/resources/db/common/V66__create_feedback_request_table.sql
+++ server/src/main/resources/db/common/V66__create_feedback_request_table.sql
@@ -4,8 +4,10 @@ CREATE TABLE feedback_requests (
   id varchar PRIMARY KEY,
   creatorId varchar REFERENCES member_profile(id),
   requesteeId varchar REFERENCES member_profile(id),
+  recipientId varchar REFERENCES member_profile(id),
   templateId varchar,
   sendDate date,
   dueDate date,
+  submitDate date,
   status varchar
 );
