2b62c25d498e3fefa28d50dae80f0c63f9df4a82
Pramukh Bagur <bagurp@objectcomputing.com>
2021-09-22 12:09:40 -0500

    Fix: Fix the security hotspot flagged by SonarQube on githubClient

--- server/src/main/java/com/objectcomputing/checkins/services/github/GithubClient.java
+++ server/src/main/java/com/objectcomputing/checkins/services/github/GithubClient.java
@@ -1,16 +1,23 @@
 package com.objectcomputing.checkins.services.github;
 
+import io.micronaut.context.annotation.ConfigurationProperties;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
 import org.reactivestreams.Publisher;
 
 import javax.inject.Singleton;
+import javax.validation.constraints.NotNull;
 
-import static io.micronaut.http.HttpHeaders.*;
+import static io.micronaut.http.HttpHeaders.ACCEPT;
+import static io.micronaut.http.HttpHeaders.AUTHORIZATION;
+import static io.micronaut.http.HttpHeaders.USER_AGENT;
 
+@ConfigurationProperties("github-credentials")
 @Singleton
 public class GithubClient {
 
+    @NotNull
+    public String github_token;
     private String baseURL = "https://api.github.com/";
     private final HttpClient httpClient;
 
@@ -22,7 +29,7 @@ public class GithubClient {
         HttpRequest<?> req = HttpRequest.POST(baseURL + "repos/oci-labs/check-ins/issues", requestDTO)
                 .header(USER_AGENT, "Micronaut HTTP Client")
                 .header(ACCEPT, "application/vnd.github.v3+json, application/json")
-                .header(AUTHORIZATION, "token " + System.getenv("GIT_HUB_TOKEN"));
+                .header(AUTHORIZATION, "token " + github_token);
 
         return httpClient.retrieve(req, IssueResponseDTO.class);
     }
