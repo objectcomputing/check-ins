3c8425e001324ba4467361f86b3d35641b189fbb
Pramukh Bagur <bagurp@objectcomputing.com>
2021-09-28 11:36:46 -0500

    Refactor: update code to resolve a potential bug

--- server/src/main/java/com/objectcomputing/checkins/services/github/GithubClient.java
+++ server/src/main/java/com/objectcomputing/checkins/services/github/GithubClient.java
@@ -1,35 +1,36 @@
 package com.objectcomputing.checkins.services.github;
 
-import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Property;
+import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
 import org.reactivestreams.Publisher;
 
 import javax.inject.Singleton;
-import javax.validation.constraints.NotNull;
 
 import static io.micronaut.http.HttpHeaders.ACCEPT;
 import static io.micronaut.http.HttpHeaders.AUTHORIZATION;
 import static io.micronaut.http.HttpHeaders.USER_AGENT;
 
-@ConfigurationProperties("github-credentials")
 @Singleton
+@Requires(property = GithubClient.GITHUB_CREDENTIALS_TOKEN)
 public class GithubClient {
 
-    @NotNull
-    public String github_token;
     private String baseURL = "https://api.github.com/";
     private final HttpClient httpClient;
+    public static final String GITHUB_CREDENTIALS_TOKEN = "github-credentials.github_token";
+    private final String githubToken;
 
-    public GithubClient(HttpClient httpClient) {
+    public GithubClient(HttpClient httpClient, @Property(name = GITHUB_CREDENTIALS_TOKEN) String githubToken) {
         this.httpClient = httpClient;
+        this.githubToken = githubToken;
     }
 
     Publisher<IssueResponseDTO> sendIssue(IssueCreateDTO requestDTO) {
         HttpRequest<?> req = HttpRequest.POST(baseURL + "repos/oci-labs/check-ins/issues", requestDTO)
                 .header(USER_AGENT, "Micronaut HTTP Client")
                 .header(ACCEPT, "application/vnd.github.v3+json, application/json")
-                .header(AUTHORIZATION, "token " + github_token);
+                .header(AUTHORIZATION, "token " + githubToken);
 
         return httpClient.retrieve(req, IssueResponseDTO.class);
     }
