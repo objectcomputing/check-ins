c57493786c8403cf1f6e08711a21068be979d019
Pramukh Bagur <bagurp@objectcomputing.com>
2021-10-13 01:36:15 -0500

    Feat: delete redundant enum Permissions

--- server/src/main/java/com/objectcomputing/checkins/security/permissions/Permissions.java
+++
@@ -1,6 +0,0 @@
-package com.objectcomputing.checkins.security.permissions;
-
-public enum Permissions {
-    CAN_CREATE_ORGANIZATION_MEMBERS,
-    CAN_DELETE_ORGANIZATION_MEMBERS
-}
--- server/src/main/java/com/objectcomputing/checkins/services/permissions/Permissions.java
+++
@@ -1,9 +0,0 @@
-package com.objectcomputing.checkins.services.permissions;
-
-public enum Permissions {
-    CAN_VIEW_FEEDBACK,
-    CAN_CREATE_FEEDBACK,
-    CAN_DELETE_FEEDBACK,
-    CAN_DELETE_ORGANIZATION_MEMBERS,
-    CAN_CREATE_ORGANIZATION_MEMBERS;
-}
