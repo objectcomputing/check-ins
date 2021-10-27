2cbe9d2e07e1b9a11ed1bafd4fdbff17cc7d57e8
moshirim <mahyarmoshiri.mm@gmaqil.com>
2021-10-27 09:39:43 -0500

    Feat: Wrote the first cypress test that tests the functionality of the login page.

--- web-ui/cypress/integration/login.spec.js
+++ web-ui/cypress/integration/login.spec.js
@@ -1,9 +1,8 @@
 
-describe("Tests for login page", () => {
+describe("Tests that login page exists and functions properly", () => {
 
-  // TODO: setup http://localhost:8080 as base url in configuration
   it("ensure components are displayed", () => {
-    cy.visit("https://checkins.objectcomputing.com");
+    cy.visit("/oauth/login/google");
     cy.get("form");
     cy.get('input[name="email"]')                     // Ensure input for email exists
       .type("sharmag@objectcomputing.com")
@@ -11,82 +10,11 @@ describe("Tests for login page", () => {
     cy.get('input[name="role"]')                      // Ensure input for role exists
       .type("ADMIN")
       .should("have.value", "ADMIN")
-    cy.get('input[name="Submit"]')                    // Ensure submit button exists
-      .type("Submit")
+    cy.get('input[value="Submit"]').click()           // Ensure submit button exists and take you to checkins page.
+    cy.url().should("include", "/")
+    cy.request("/").then((response) => {
+      expect(response.status).to.eq(200)
+    });
   });
 
-  // it("ensure rest call was initiated on form submit", () => {
-  //   cy.visit("http://localhost:8080/oauth/login/google");
-  //   cy.get("form");
-  //   cy.get('input[name="email"]').type("sharmag@objectcomputing.com");
-  //   cy.get('input[name="role"]').type("ADMIN");
-  //   cy.get('input[name="Submit"]').click();
-  //   // assert that a post call was intercepted
-  // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // before( () => {
-  //   const email = "sharmag@objectcomputing.com";
-  //   const role = "SUPER";
-  //   cy.loginByCsrf(email, role).then((res) => {
-  //     expect(res.status).to.eq(200);
-  //   });
-  // });
-  //
-  // it("assert true", () => {
-  //   expect(true).to.equal(true);
-  // });
-  //
-  // it("navigates to the homepage", () => {
-  //   cy.visit("localhost:8080");
-  // });
-
-  // it("navigates to the homepage", () => {
-  //   cy.visit("localhost:8080");
-  //   cy.get("a[href*='/checkins']").last().click();
-  //   cy.url().should("include", "/checkins");
-  //
-  //   cy.contains("Request Feedback").first().click();
-  //   cy.url().should("include", "/feedback/request");
-  //   cy.url().should("include", "for=");
-  //   cy.contains("Feedback Request for");
-  //
-  //   cy.contains("Survey 1").click();
-  //   cy.url().should("include", "template=");
-  //   cy.contains("Next").click();
-  //
-  //   cy.url().should("include", "step=2");
-  //   cy.get(".member-card").click({multiple: true});
-  //   cy.url().should("match", /from=*&*/);
-  //   cy.contains("Next").click();
-  //
-  //   cy.url().should("include", "step=3");
-  //
-  //   cy.contains("Send Date:").click();
-  //   cy.contains("OK").click();
-  //   cy.url().should("include", "send=");
-  //   cy.contains("Due Date:").click();
-  //   cy.contains("OK").click();
-  //   cy.url().should("include", "due=");
-  //
-  //   cy.contains("Submit").click();
-  //   cy.contains("Feedback scheduled for today");
-  //   cy.url().should("include", "/feedback/request/confirmation");
-  // });
 });
