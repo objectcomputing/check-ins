b73eaac099583d391ea786d55b111f023fa828bc
moshirim <mahyarmoshiri.mm@gmaqil.com>
2021-10-28 15:33:51 -0500

    feat: Added dummy test to ensure cypress is configured properly in github workflow.

---
+++ web-ui/cypress/integration/dummyTest.spec.js
@@ -0,0 +1,7 @@
+
+// This test is purely written to ensure that Cypress is configured properly in github workflow.
+describe('Dummy Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
