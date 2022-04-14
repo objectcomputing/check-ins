374edc322bb25d767456bf0df5a06c1c752aad7f
pieperm <michaelepieper@gmail.com>
2022-04-14 16:58:26 -0500

    Add save functionality to role permission service

--- server/src/main/java/com/objectcomputing/checkins/services/role/role_permissions/RolePermissionServices.java
+++ server/src/main/java/com/objectcomputing/checkins/services/role/role_permissions/RolePermissionServices.java
@@ -3,5 +3,9 @@ package com.objectcomputing.checkins.services.role.role_permissions;
 import java.util.List;
 
 public interface RolePermissionServices {
+
+    RolePermission save(RolePermission rolePermission);
+
     List<RolePermissionResponseDTO> findAll();
+
 }
--- server/src/main/java/com/objectcomputing/checkins/services/role/role_permissions/RolePermissionServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/role/role_permissions/RolePermissionServicesImpl.java
@@ -1,14 +1,19 @@
 package com.objectcomputing.checkins.services.role.role_permissions;
 
+import com.objectcomputing.checkins.exceptions.BadArgException;
 import com.objectcomputing.checkins.services.permissions.Permission;
+import com.objectcomputing.checkins.services.permissions.PermissionRepository;
 import com.objectcomputing.checkins.services.permissions.PermissionServices;
 import com.objectcomputing.checkins.services.role.Role;
+import com.objectcomputing.checkins.services.role.RoleRepository;
 import com.objectcomputing.checkins.services.role.RoleServices;
 
 import javax.inject.Singleton;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Singleton
 public class RolePermissionServicesImpl implements RolePermissionServices {
@@ -16,13 +21,42 @@ public class RolePermissionServicesImpl implements RolePermissionServices {
     private final RolePermissionRepository rolePermissionRepository;
     private final RoleServices roleServices;
     private final PermissionServices permissionServices;
+    private final RoleRepository roleRepository;
+    private final PermissionRepository permissionRepository;
 
     public RolePermissionServicesImpl(RolePermissionRepository rolePermissionRepository,
                                       RoleServices roleServices,
-                                      PermissionServices permissionServices) {
+                                      PermissionServices permissionServices,
+                                      RoleRepository roleRepository,
+                                      PermissionRepository permissionRepository) {
         this.rolePermissionRepository = rolePermissionRepository;
         this.roleServices = roleServices;
         this.permissionServices = permissionServices;
+        this.roleRepository = roleRepository;
+        this.permissionRepository = permissionRepository;
+    }
+
+    @Override
+    public RolePermission save(@NotNull RolePermission rolePermission) {
+        final UUID roleId = rolePermission.getRoleId();
+        final UUID permissionId = rolePermission.getPermissionId();
+
+        // Ensure the role exists
+        roleRepository.findById(roleId).orElseThrow(() -> {
+            throw new BadArgException(String.format("Role %s not found", roleId));
+        });
+
+        // Ensure the permission exists
+        permissionRepository.findById(permissionId).orElseThrow(() -> {
+            throw new BadArgException(String.format("Permission %s not found", permissionId));
+        });
+
+        // Ensure this role has not already been granted this permission
+        rolePermissionRepository.findByRoleIdAndPermissionId(roleId, permissionId).ifPresent(rp -> {
+            throw new BadArgException(String.format("Role %s already has permission %s", roleId, permissionId));
+        });
+
+        return rolePermissionRepository.save(rolePermission);
     }
 
     public List<RolePermissionResponseDTO> findAll() {
