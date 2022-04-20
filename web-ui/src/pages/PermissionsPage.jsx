245a86ed0268cc0a12a0c286cbb2e37623e10a14
pieperm <michaelepieper@gmail.com>
2022-04-20 16:18:19 -0500

    Change table to use mock roles, connect event handlers to checkboxes, make API calls for role permissions

--- web-ui/src/pages/PermissionsPage.jsx
+++ web-ui/src/pages/PermissionsPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
 import {
   Checkbox,
   InputAdornment,
@@ -11,9 +11,16 @@ import {
   TextField
 } from "@mui/material";
 import {Search} from "@mui/icons-material";
+import {
+  getAllRolePermissions,
+  addRolePermission,
+  removeRolePermission
+} from "../api/permissions";
 
 import "./PermissionsPage.css";
 import {styled} from "@mui/material/styles";
+import {AppContext} from "../context/AppContext";
+import {UPDATE_TOAST} from "../context/actions";
 
 const StyledTableCell = styled(TableCell)(({theme}) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -47,29 +54,40 @@ const mockPermissions = [
   {id: 1, permission: "Add Team Members"},
   {id: 2, permission: "Delete Team Members"},
   {id: 3, permission: "Review Check-ins"},
-  {id: 4, permission: "Upload Files"},
-  {id: 5, permission: "Add Team Members"},
-  {id: 6, permission: "Delete Team Members"},
-  {id: 7, permission: "Review Check-ins"},
-  {id: 8, permission: "Upload Files"},
-  {id: 9, permission: "Add Team Members"},
-  {id: 10, permission: "Delete Team Members"},
-  {id: 11, permission: "Review Check-ins"},
-  {id: 12, permission: "Upload Files"},
-  {id: 13, permission: "Add Team Members"},
-  {id: 14, permission: "Delete Team Members"},
-  {id: 15, permission: "Review Check-ins"},
-  {id: 16, permission: "Upload Files"}
+  {id: 4, permission: "Upload Files"}
 ];
 
+const mockRoles = [
+  {id: 1, role: "Member"},
+  {id: 2, role: "PDL"},
+  {id: 3, role: "Admin"}
+]
+
 const PermissionsPage = () => {
+  const { state } = useContext(AppContext);
+  const { csrf } = state;
 
   const [searchText, setSearchText] = useState("");
+  const [allRolePermissions, setAllRolePermissions] = useState([]);
   const [filteredPermissions, setFilteredPermissions] = useState([]);
 
   useEffect(() => {
     setFilteredPermissions(mockPermissions);
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    const getRolePermissions = async () => {
+      const res = await getAllRolePermissions(csrf);
+      return res && res.payload && res.payload.data ? res.payload.data : [];
+    }
+
+    if (csrf) {
+      getRolePermissions().then((rolePermissions) => {
+        setAllRolePermissions(rolePermissions);
+      });
+    }
+
+  }, [csrf]);
 
   useEffect(() => {
     let searchedPermissions = mockPermissions;
@@ -80,6 +98,31 @@ const PermissionsPage = () => {
     }
     setFilteredPermissions(searchedPermissions);
   }, [searchText]);
+
+  const roleHasPermission = useCallback((role, permission) => {
+    return allRolePermissions.find((entry) => entry.roleId === role.id && entry.permissionId === permission.id);
+  }, [allRolePermissions]);
+
+  const handleCheckboxChange = useCallback(async (checked, role, permission) => {
+    let res;
+    let toastMessage;
+    if (checked) {
+      res = await addRolePermission(role.id, permission.id, csrf);
+      toastMessage = `Role ${role.role.toUpperCase()} has been given permission to ${permission.permission.toLowerCase()}`
+    } else {
+      res = await removeRolePermission(role.id, permission.id, csrf);
+      toastMessage = `Role ${role.role.toUpperCase()} no longer has permission to ${permission.permission.toLowerCase()}`
+    }
+    if (res.payload && !res.error) {
+      window.snackDispatch({
+        type: UPDATE_TOAST,
+        payload: {
+          severity: "success",
+          toast: toastMessage
+        }
+      });
+    }
+  }, [csrf]);
 
   return (
     <div className="permissions-content">
@@ -97,18 +140,27 @@ const PermissionsPage = () => {
           <TableHead>
             <TableRow>
               <StyledTableCell style={{width: "50%"}}>Permission</StyledTableCell>
-              <StyledTableCell align="center">Member</StyledTableCell>
-              <StyledTableCell align="center">PDL</StyledTableCell>
-              <StyledTableCell align="center">Admin</StyledTableCell>
+              {mockRoles.map(role =>
+                <StyledTableCell
+                  key={role.id}
+                  align="center">
+                  {role.role}
+                </StyledTableCell>
+              )}
             </TableRow>
           </TableHead>
           <TableBody>
             {filteredPermissions.map(permission =>
               <StyledTableRow key={permission.id}>
                 <StyledTableCell>{permission.permission}</StyledTableCell>
-                <StyledTableCell align="center"><Checkbox/></StyledTableCell>
-                <StyledTableCell align="center"><Checkbox/></StyledTableCell>
-                <StyledTableCell align="center"><Checkbox/></StyledTableCell>
+                {mockRoles.map((role) => (
+                  <StyledTableCell key={`${role.id}-${permission.id}`} align="center">
+                    <Checkbox
+                      checked={roleHasPermission(role, permission)}
+                      onChange={(event) => handleCheckboxChange(event.target.value, role, permission)}
+                    />
+                  </StyledTableCell>
+                ))}
               </StyledTableRow>
             )}
           </TableBody>
