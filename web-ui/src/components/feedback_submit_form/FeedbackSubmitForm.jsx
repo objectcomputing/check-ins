efce247baffe90d1ccb30d40785c6a97c105c7d5
pieperm <michaelepieper@gmail.com>
2022-04-26 16:37:05 -0500

    Use FeedbackSubmitQuestion component for rendering each question and answer, adjust the way API calls are made, adjust state updates

--- web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.jsx
+++ web-ui/src/components/feedback_submit_form/FeedbackSubmitForm.jsx
@@ -4,10 +4,6 @@ import Typography from "@mui/material/Typography";
 import PropTypes from "prop-types";
 import { green } from "@mui/material/colors";
 import Button from "@mui/material/Button";
-import Slider from "@mui/material/Slider";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import "./FeedbackSubmitForm.css";
 import { Alert, AlertTitle } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
@@ -19,15 +15,15 @@ import { UPDATE_TOAST } from "../../context/actions";
 import {
   updateAllAnswers,
   updateSingleAnswer,
-  updateFeedbackRequest,
+  updateFeedbackRequest, saveSingleAnswer,
 } from "../../api/feedback";
 import {
 getQuestionAndAnswer
 } from "../../api/feedbackanswer"
-import TextField from "@mui/material/TextField";
 import { debounce } from "lodash/function";
 import DateFnsUtils from "@date-io/date-fns";
 import SkeletonLoader from "../skeleton_loader/SkeletonLoader";
+import FeedbackSubmitQuestion from "../feedback_submit_question/FeedbackSubmitQuestion";
 
 const dateUtils = new DateFnsUtils();
 const PREFIX = "FeedbackSubmitForm";
@@ -38,34 +34,6 @@ const classes = {
   button: `${PREFIX}-button`,
   coloredButton: `${PREFIX}-coloredButton`
 };
-
-const agreeMarks = [
-  {
-    value: 0,
-    label: "Strongly Disagree",
-    text: "Strongly Disagree"
-  },
-  {
-    value: 1,
-    label: "Disagree",
-    text: "Disagree"
-  },
-  {
-    value: 2,
-    label: "Neither Agree nor Disagree",
-    text: "Neither Agree nor Disagree"
-  },
-  {
-    value: 3,
-    label: "Agree",
-    text: "Agree"
-  },
-  {
-    value: 4,
-    label: "Strongly Agree",
-    text: "Strongly Agree"
-  }
-];
 
 const Root = styled('div')({
   [`& .${classes.announcement}`]: {
@@ -114,33 +82,40 @@ const propTypes = {
   request: PropTypes.any.isRequired,
 }
 
-const getSliderValue = (marks, text) => {
-  const value = marks?.find((mark) => mark?.text === text)?.value
-  return value;
-}
-
-const realUpdateAnswer = async (answer, csrf) => {
-    if (csrf) {
-      const res = updateSingleAnswer(answer, csrf);
-      return res;
-    }
-};
-
-const updateFeedbackAnswer = debounce(realUpdateAnswer, 1000);
-
 const FeedbackSubmitForm = ({ requesteeName, requestId, request }) => {
   const { state, dispatch } = useContext(AppContext);
   const csrf = selectCsrfToken(state);
   const [isLoading, setIsLoading] = useState(false);
   const [isReviewing, setIsReviewing] = useState(false);
   const history = useHistory();
-  const [questionAnswerPairs, setQuestionAnswerPairs] = useState([])
-  const [templateTitle, setTemplateTitle] = useState(null)
+  const [questionAnswerPairs, setQuestionAnswerPairs] = useState([]);
+  const [templateTitle, setTemplateTitle] = useState(null);
 
-  const updateAnswer = useCallback(
-    (index) => updateFeedbackAnswer(questionAnswerPairs[index]?.answer, csrf),
-    [questionAnswerPairs, csrf]
-  );
+  const updateFeedbackAnswer = async (index, answer) => {
+    let res;
+
+    // Save or update answer on server
+    if (answer && csrf) {
+      if (answer.id) {
+        res = await updateSingleAnswer(answer, csrf);
+      } else {
+        res = await saveSingleAnswer(answer, csrf);
+      }
+    }
+
+    // Update local state with new answer
+    if (res && res.payload && res.payload.data && !res.error) {
+      let updatedQuestionAnswerPairs = [...questionAnswerPairs];
+      updatedQuestionAnswerPairs[index].answer = {...res.payload.data};
+      setQuestionAnswerPairs(updatedQuestionAnswerPairs);
+    }
+  }
+
+  const updateFeedbackAnswerWithDebounce = debounce(updateFeedbackAnswer, 1000);
+
+  const handleAnswerChange = useCallback((index, newAnswer) => {
+    updateFeedbackAnswerWithDebounce(index, newAnswer);
+  }, [updateFeedbackAnswerWithDebounce]);
 
   async function updateRequestSubmit() {
     request.status = "submitted"
@@ -150,13 +125,14 @@ const FeedbackSubmitForm = ({ requesteeName, requestId, request }) => {
   }
 
   async function updateAllAnswersSubmit(){
-    let answers= [];
+    let answers = [];
     for (let i = 0; i < questionAnswerPairs.length; ++i) {
       answers.push(questionAnswerPairs[i].answer || {})
     }
     const res = await updateAllAnswers(answers, csrf)
     return res;
   }
+
   const onSubmitHandler =() => {
     updateAllAnswersSubmit().then((res) => {
       for (let i = 0; i < res.length; ++i ) {
@@ -192,76 +168,6 @@ const FeedbackSubmitForm = ({ requesteeName, requestId, request }) => {
     });
   }
 
-  const onSliderChange = (event, index, marks, value) => {
-    let questionAnswerCopy = [...questionAnswerPairs];
-    questionAnswerCopy[index].answer = questionAnswerCopy[index].answer || {};
-    questionAnswerCopy[index].answer.answer = marks?.find((mark) => mark.value === value)?.text;
-    setQuestionAnswerPairs(questionAnswerCopy);
-    updateAnswer(index);
-  }
-
-  const onRadioChange = (event, index, value) => {
-    let questionAnswerCopy = [...questionAnswerPairs];
-    questionAnswerCopy[index].answer = questionAnswerCopy[index].answer || {};
-    questionAnswerCopy[index].answer.answer = value;
-    setQuestionAnswerPairs(questionAnswerCopy);
-    updateAnswer(index);
-  }
-
-  const onTextChange = (event, index) => {
-    let questionAnswerCopy = [...questionAnswerPairs];
-    questionAnswerCopy[index].answer = questionAnswerCopy[index].answer || {};
-    questionAnswerCopy[index].answer.answer = event.target.value;
-    setQuestionAnswerPairs(questionAnswerCopy);
-    updateAnswer(index);
-  }
-
-  const getQuestionHeader = (index, isReview) => isReview && index === 1
-    && (<h2>How often has this team member displayed each of the following in the past year...</h2>);
-
-  const getInput = (questionAnswerPair, isReviewing, index) => {
-    let inputField;
-    switch(questionAnswerPair.question.inputType) {
-      case "SLIDER":
-        inputField =
-          (<Slider
-            disabled={isReviewing}
-            min={0}
-            max={4}
-            value={getSliderValue(agreeMarks, questionAnswerPair?.answer?.answer)}
-            step={1}
-            marks={agreeMarks}
-            onChange={(e, value) => onSliderChange(e, index, agreeMarks, value)}
-          />);
-        break;
-      case "RADIO":
-        inputField =
-          (<RadioGroup row value={questionAnswerPair?.answer?.answer} onChange={(event, value) => onRadioChange(event, index, value)}>
-            <FormControlLabel disabled={isReviewing} value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel disabled={isReviewing} value="No" control={<Radio />} label="No" />
-            <FormControlLabel disabled={isReviewing} value="I don't know." control={<Radio />} label="I don't know" />
-          </RadioGroup>);
-        break;
-      default:
-        inputField =
-          (<TextField
-            multiline
-            rows={5}
-            maxRows={10}
-            className="fullWidth"
-            variant="outlined"
-            InputProps={{
-              readOnly: isReviewing,
-            }}
-            onChange={(e) => onTextChange(e, index)}
-            defaultValue={questionAnswerPair?.answer?.answer}
-          />);
-        break;
-    }
-
-    return inputField;
-  }
-
   useEffect(() => {
     async function getAllQuestionsAndAnswers(requestId, cookie) {
       if (!requestId) {
@@ -291,8 +197,6 @@ const FeedbackSubmitForm = ({ requesteeName, requestId, request }) => {
     }
   }, [requestId, csrf, dispatch]);
 
-  const isReview = templateTitle === "Annual Review";
-
   return isLoading ? <SkeletonLoader type="feedback_requests" /> : (
     <Root className="submit-form">
       <Typography className={classes.announcement} variant="h3">Submitting Feedback on <b>{requesteeName}</b></Typography>
@@ -308,13 +212,22 @@ const FeedbackSubmitForm = ({ requesteeName, requestId, request }) => {
         </Alert> : null
       }
       {questionAnswerPairs.map((questionAnswerPair, index) => (
-        <div className="feedback-submit-question" key={questionAnswerPair.question.id}>
-        <p>{questionAnswerPair.type}</p>
-          {getQuestionHeader(index, isReview)}
-          <Typography variant="body1"><b>Q{questionAnswerPair.question.questionNumber}:</b> {questionAnswerPair.question.question}</Typography>
-          {getInput(questionAnswerPair, isReviewing, index, isReview)}
-        </div>
-
+        <FeedbackSubmitQuestion
+          key={questionAnswerPair.question.id}
+          question={questionAnswerPair.question.question}
+          questionNumber={questionAnswerPair.question.questionNumber}
+          inputType={questionAnswerPair.question.inputType}
+          readOnly={isReviewing}
+          answer={questionAnswerPair.answer?.answer}
+          handleAnswerChange={(newAnswer) => {
+            handleAnswerChange(index, {
+              answer: newAnswer,
+              id: questionAnswerPair.answer?.id,
+              questionId: questionAnswerPair.question.id,
+              requestId: questionAnswerPair.request.id
+            });
+          }}
+        />
       ))}
       <div className="submit-action-buttons">
         {isReviewing ?
