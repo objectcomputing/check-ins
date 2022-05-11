2d2facad264d80edf3fb362880f568e091b94f6c
pieperm <michaelepieper@gmail.com>
2022-05-11 14:50:02 -0500

    Add route to menu

--- web-ui/src/components/menu/Menu.jsx
+++ web-ui/src/components/menu/Menu.jsx
@@ -94,6 +94,7 @@ const adminLinks = [
   // ["/admin/permissions", "Permissions"],
   ["/admin/roles", "Roles"],
   ["/admin/users", "Users"],
+  ["/admin/email", "Email"],
 ];
 
 const directoryLinks = [
