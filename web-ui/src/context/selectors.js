67153ffae33202f045159ac568744f113eb9e106
pieperm <michaelepieper@gmail.com>
2022-06-08 16:14:47 -0500

    Fix bug where terminated members were displayed as blank options when selecting PDL

--- web-ui/src/context/selectors.js
+++ web-ui/src/context/selectors.js
@@ -126,10 +126,17 @@ export const selectPdlRoles = createSelector(selectRoles, (roles) =>
   roles?.filter((role) => role.role?.includes("PDL"))
 );
 
+export const selectCurrentUserRoles = createSelector(
+  selectUserRoles,
+  selectCurrentMemberIds,
+  (userRoles, memberIds) =>
+    userRoles?.filter((userRole) => memberIds.includes(userRole.memberRoleId.memberId))
+);
+
 export const selectMappedPdls = createSelector(
   selectProfileMap,
   selectPdlRoles,
-  selectUserRoles,
+  selectCurrentUserRoles,
   (memberProfileMap, roles, userRoles) =>
     userRoles?.filter((userRole) => roles.find((role) => role.id === userRole?.memberRoleId?.roleId ) !== undefined)?.map((userRole) =>
       userRole?.memberRoleId?.memberId in memberProfileMap ? memberProfileMap[userRole?.memberRoleId?.memberId] : {}
