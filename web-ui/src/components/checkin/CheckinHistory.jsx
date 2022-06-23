50fa8e87428876698715637bfcea68bc72f348d8
pieperm <michaelepieper@gmail.com>
2022-06-23 16:31:27 -0500

    User must specify date before creating a check-in, default date is 30 days in future

--- web-ui/src/components/checkin/CheckinHistory.jsx
+++ web-ui/src/components/checkin/CheckinHistory.jsx
@@ -1,55 +1,70 @@
-import React, { useContext } from "react";
+import React, {useContext, useState} from "react";
 import { useHistory, useParams } from "react-router-dom";
-import {
-  UPDATE_CHECKIN,
-} from "../../context/actions";
 import { AppContext } from "../../context/AppContext";
-import { updateCheckin } from "../../api/checkins";
-import { selectCsrfToken, selectCheckinsForMember, selectProfile } from "../../context/selectors";
+import {
+  selectCheckinsForMember,
+  selectCsrfToken,
+  selectCurrentUser,
+  selectIsAdmin, selectIsPDL,
+  selectProfile
+} from "../../context/selectors";
 import IconButton from "@mui/material/IconButton";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import {MobileDateTimePicker} from "@mui/lab";
+import {StaticDateTimePicker} from "@mui/lab";
 import TextField from "@mui/material/TextField";
 
 import "./Checkin.css";
+import {
+  Alert,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle, Tooltip
+} from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import {updateCheckin} from "../../api/checkins";
+import {UPDATE_CHECKIN, UPDATE_TOAST} from "../../context/actions";
+import {createNewCheckinWithDate} from "../../context/thunks";
+
+const PREFIX = "CheckinsPage";
+const classes = {
+  addButton: `${PREFIX}-addButton`
+};
 
 const CheckinsHistory = () => {
   const { state, dispatch } = useContext(AppContext);
   const { checkinId, memberId } = useParams();
   const history = useHistory();
   const csrf = selectCsrfToken(state);
-  const selectedProfile = selectProfile(state, memberId);
+  const [checkinDatetime, setCheckinDatetime] = useState(null);
+  const [defaultDatetimeChanged, setDefaultDatetimeChanged] = useState(false);
+  const [datetimeDialogOpen, setDatetimeDialogOpen] = useState(false);
+  const [tooltipIsOpen, setTooltipIsOpen] = useState(false);
+
+  const memberProfile = selectCurrentUser(state);
+  const currentUserId = memberProfile?.id;
 
   const checkins = selectCheckinsForMember(state, memberId);
   const index = checkins.findIndex(checkin => checkin.id === checkinId);
 
-  const getCheckinDate = () => {
-    if (checkins && checkins[index]?.checkInDate) {
-      const [year, month, day, hour, minute] = checkins[index].checkInDate;
-      return new Date(year, month - 1, day, hour, minute, 0);
-    }
-    // return new date unless you are running a Jest test
-    return process.env.JEST_WORKER_ID ? new Date(2020, 9, 21) : new Date();
-  };
+  const selectedProfile = selectProfile(state, memberId);
+  const memberCheckins = selectCheckinsForMember(
+    state,
+    selectedProfile ? selectedProfile.id : currentUserId
+  );
+  const hasOpenCheckins = memberCheckins.some((checkin) => !checkin.completed);
 
-  const leftArrowClass = "arrow " + (index > 0 ? "enabled" : "disabled");
-  const rightArrowClass =
-    "arrow " + (index < checkins.length - 1 ? "enabled" : "disabled");
+  const isAdmin = selectIsAdmin(state);
+  const isPdl = selectIsPDL(state);
 
-  const previousCheckin = () => {
-    if (index > 0) {
-      history.push(`/checkins/${memberId}/${checkins[index - 1].id}`);
-    }
-  };
+  const createCheckIn = async (date) => {
+    const newId = await createNewCheckinWithDate(selectedProfile, date, dispatch, csrf);
+    if (newId) history.push(`/checkins/${memberId}/${newId}`);
+  }
 
-  const nextCheckin = () => {
-    if (index < checkins.length - 1) {
-      history.push(`/checkins/${memberId}/${checkins[index + 1].id}`);
-    }
-  };
-
-  const pickDate = async (date) => {
+  const updateCheckInDate = async (date) => {
     if (csrf) {
       const year = date.getFullYear();
       const month = date.getMonth() + 1;
@@ -69,43 +84,172 @@ const CheckinsHistory = () => {
     }
   };
 
+  const handleSave = () => {
+    setDatetimeDialogOpen(false);
+    if (checkins) {
+      if (checkins[index]) {
+        updateCheckInDate(checkinDatetime);
+      } else {
+        console.log("creating new...");
+        createCheckIn(checkinDatetime);
+      }
+    }
+  }
+
+  const handleCreateClick = async () => {
+    if (!selectedProfile.pdlId) {
+      window.snackDispatch({
+        type: UPDATE_TOAST,
+        payload: {
+          severity: "error",
+          toast: "You must have an assigned PDL in order to create a Check In",
+        },
+      });
+    } else {
+      // Default date is 30 days in the future
+      const futureDate = new Date();
+      futureDate.setDate(futureDate.getDate() + 30);
+      setCheckinDatetime(futureDate);
+      setDatetimeDialogOpen(true);
+    }
+  };
+
+  const getCheckinDate = () => {
+    if (checkins && checkins[index]?.checkInDate) {
+      const [year, month, day, hour, minute] = checkins[index].checkInDate;
+      return new Date(year, month - 1, day, hour, minute, 0);
+    }
+    // return new date unless you are running a Jest test
+    return process.env.JEST_WORKER_ID ? new Date(2020, 9, 21) : new Date();
+  };
+
+  const getFormattedCheckinDate = () => {
+    let checkinDate = new Date();
+    if (checkins && checkins[index]?.checkInDate) {
+      const [year, month, day, hour, minute] = checkins[index].checkInDate;
+      checkinDate = new Date(year, month - 1, day, hour, minute, 0);
+    } else {
+      return "No check-in selected";
+    }
+    const dateString = checkinDate.toDateString();
+    const timeString = checkinDate.toLocaleTimeString([], {hour: "numeric", minute: "2-digit"});
+    return `${dateString}, ${timeString}`;
+  }
+
+  const leftArrowClass = "arrow " + (index > 0 ? "enabled" : "disabled");
+  const rightArrowClass =
+    "arrow " + (index < checkins.length - 1 ? "enabled" : "disabled");
+
+  const previousCheckin = () => {
+    if (index > 0) {
+      history.push(`/checkins/${memberId}/${checkins[index - 1].id}`);
+    }
+  };
+
+  const nextCheckin = () => {
+    if (index < checkins.length - 1) {
+      history.push(`/checkins/${memberId}/${checkins[index + 1].id}`);
+    }
+  };
+
   return (
-      getCheckinDate() && (
-          <div className="date-picker">
-            <IconButton
-              disabled={index <= 0}
-              aria-label="Previous Check-in`"
-              onClick={previousCheckin}
-              size="large">
-              <ArrowBackIcon
-                className={leftArrowClass}
-                style={{ fontSize: "1.2em" }}
-              />
-            </IconButton>
-            <MobileDateTimePicker
-              renderInput={props => <TextField style={{width: '18em'}} {...props}/>}
+    <>
+      {getCheckinDate() && (
+      <div className="date-picker">
+        <IconButton
+          disabled={index <= 0}
+          aria-label="Previous Check-in`"
+          onClick={previousCheckin}
+          size="large">
+          <ArrowBackIcon
+            className={leftArrowClass}
+            style={{ fontSize: "1.2em" }}
+          />
+        </IconButton>
+        <Dialog className="check-in-datetime-dialog" open={datetimeDialogOpen}>
+          <DialogTitle>Check-In Date</DialogTitle>
+          <DialogContent>
+            {(checkins && checkins[index]?.checkInDate ? false : !defaultDatetimeChanged) &&
+              <Alert severity="info" style={{width: "300px"}}>By default, this check-in is set 30 days in the future</Alert>
+            }
+            <StaticDateTimePicker
+              onChange={(datetime) => {
+                setCheckinDatetime(datetime);
+                setDefaultDatetimeChanged(true);
+              }}
+              value={checkinDatetime}
+              ampm
+              hideTabs
               format="MMMM dd, yyyy @hh:mm aaaa"
-              value={getCheckinDate()}
-              onChange={pickDate}
-              label="Check-In Date"
-              showTodayButton
-              disabled={
-                !checkins?.length ||
-                checkins[index]?.completed
-              }
+              minDate={new Date()}
+              disablePast
+              displayStaticWrapperAs="mobile"
+              label=" "
+              renderInput={(props) => (
+                <TextField fullWidth {...props} />
+              )}
             />
-            <IconButton
-              disabled={index >= checkins.length - 1}
-              aria-label="Next Check-in`"
-              onClick={nextCheckin}
-              size="large">
-              <ArrowForwardIcon
-                className={rightArrowClass}
-                style={{ fontSize: "1.2em" }}
-              />
-            </IconButton>
-      </div>
-    )
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => {
+              setCheckinDatetime(null);
+              setDefaultDatetimeChanged(false);
+              setDatetimeDialogOpen(false);
+            }} style={{color: "gray"}}>Cancel</Button>
+            <Button onClick={() => handleSave()} color="primary">Save</Button>
+          </DialogActions>
+        </Dialog>
+        <TextField
+          placeholder="testing"
+          value={getFormattedCheckinDate()}
+          label="Check-In Date"
+          onClick={() => {
+            if (checkins?.length && !checkins[index]?.completed) {
+              setDatetimeDialogOpen(true);
+            }
+          }}
+          style={{width: "18em"}}
+          disabled={!checkins?.length || checkins[index]?.completed}
+          focused={false}
+        />
+        <IconButton
+          disabled={index >= checkins.length - 1}
+          aria-label="Next Check-in`"
+          onClick={nextCheckin}
+          size="large">
+          <ArrowForwardIcon
+            className={rightArrowClass}
+            style={{ fontSize: "1.2em" }}
+          />
+        </IconButton>
+      </div>)}
+      <Tooltip
+        open={tooltipIsOpen && hasOpenCheckins}
+        onOpen={() => setTooltipIsOpen(true)}
+        onClose={() => setTooltipIsOpen(false)}
+        enterTouchDelay={0}
+        placement="top-start"
+        title={
+          "This is disabled because there is already an open Check-In"
+        }
+      >
+        <div
+          aria-describedby="checkin-tooltip-wrapper"
+          className="create-checkin-tooltip-wrapper"
+        >
+          {(isAdmin || isPdl || currentUserId === memberId) && (
+            <Button
+              disabled={hasOpenCheckins}
+              className={classes.addButton}
+              startIcon={<CheckCircleIcon />}
+              onClick={handleCreateClick}
+            >
+              Create Check-In
+            </Button>
+          )}
+        </div>
+      </Tooltip>
+    </>
   );
 };
 
