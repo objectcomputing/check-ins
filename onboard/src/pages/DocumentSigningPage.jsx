e438a68bf80c2e93fd1345f6bb577945684867e2
danie1r <r.seunghyeondaniel@wustl.edu>
2022-07-01 14:41:34 -0500

    implemented signrequest backend/frontend

--- onboard/src/pages/DocumentSigningPage.jsx
+++ onboard/src/pages/DocumentSigningPage.jsx
@@ -1,26 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import "./DocumentSigningPage.css";
+import getSigner from "../api/signer.js";
 
 const DocumentSigningPage = () => {
+  const [document, setDocument] = useState(null);
+
+  useEffect(async () => {
+    let res = await getSigner();
+    let document;
+    if (res && res.payload) {
+      document = res.payload.data && !res.error ? res.payload.data : undefined;
+      if (document) {
+        setDocument(document);
+      }
+    }
+  }, [getSigner]);
+
   return (
     <div>
       <center>
         <h1>Internal Document Signing</h1>
+        {document.status === "si" || document.status === "sd" ? "Signed Baby!" : "Not Signed. :("}
       </center>
-      <Link to="/equipment">
-        <Button variant="contained" size="large" id="workEquipment">
-          Work Equipment
-        </Button>
-      </Link>
-      <Link to="/congratulations">
-        <Button variant="contained" size="large" id="finish">
-          Finish
-        </Button>
-      </Link>
     </div>
   );
 };
 
+// const [signer, setSigner] = useState([]);
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     // const xhr = new XMLHttpRequest();
+//     // xhr.open('GET', 'http://localhost:8080/signer');
+//     // xhr.setRequestHeader('Content-Type', 'application/json');
+
+//     // xhr.send();
+
+//     const result = await fetch('http://localhost:8080/signer',{
+//       'Access-Control-Allow-Credentials': true
+//     });
+//     const jsonResult = await result.json();
+//     console.log(JSON.stringify(jsonResult));
+
+//     setSigner(result);
+//   }
+
+//   fetchData();
+// }, [])
 export default DocumentSigningPage;
---
+++ server/src/main/java/com/objectcomputing/checkins/services/onboard/signrequest/SignRequestController.java
@@ -0,0 +1,56 @@
+package com.oci.danie1r;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import jakarta.inject.Inject;
+
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import io.micronaut.security.authentication.Authentication;
+
+import javax.print.attribute.standard.Media;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.*;
+
+@Secured(SecurityRule.IS_ANONYMOUS)
+@Controller
+public class SignRequestController {
+    @Inject
+    @Client("https://ocitest.signrequest.com/api/v1/")
+    private HttpClient httpClient;
+    private Signer signer;
+    private Map<String,String> signerInfo = new HashMap<>();
+
+    @Get("/signer")
+    public String getData(){
+
+        try{
+            String retrieve = httpClient.toBlocking()
+                    .retrieve(HttpRequest.GET("/documents/152408ef-4b71-40a4-9416-92011a443450/")
+                            .header("Authorization","Token 8e3b636de707468e40abd0d8e212c2507e5d2197"));
+//            int openSignerIndex = retrieve.indexOf("signers");
+//            int closeSignerIndex = retrieve.indexOf("after_document");
+//            String substring = retrieve.substring(openSignerIndex+11,closeSignerIndex-2);
+//            String[] stringArr = substring.split(",");
+//            for (String item : stringArr) {
+//                String[] stringPair = item.split(":");
+//                signerInfo.put(stringPair[0],stringPair[1]);
+//            }
+//
+//            signer = new Signer(signerInfo.get("\"email\""), signerInfo.get("\"signed\""), signerInfo.get("\"viewed\""));
+//            return signer.toString();
+            return retrieve;
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        return null;
+    }
+}
