5541fb12745472e02d6efb9ae6cf5bc85ff07be0
pieperm <michaelepieper@gmail.com>
2022-07-05 12:09:04 -0500

    Create API for documents and role-documents

---
+++ web-ui/src/api/document.js
@@ -0,0 +1,57 @@
+import { resolve } from "./api";
+
+const documentUrl = "/services/documents";
+const roleDocumentsUrl = "/services/documents/role-documents";
+
+export const createDocument = async (document, cookie) => {
+  return resolve({
+    method: "post",
+    url: documentUrl,
+    responseType: "json",
+    data: document,
+    headers: { "X-CSRF-Header": cookie }
+  });
+};
+
+export const getDocumentsByRole = async (roleId, cookie) => {
+  return resolve({
+    url: roleDocumentsUrl,
+    responseType: "json",
+    params: {
+      roleId: roleId
+    },
+    headers: { "X-CSRF-Header": cookie }
+  });
+};
+
+export const updateDocument = async (document, cookie) => {
+  return resolve({
+    method: "put",
+    url: documentUrl,
+    responseType: "json",
+    data: document,
+    headers: { "X-CSRF-Header": cookie }
+  });
+};
+
+export const addRoleAccessToDocument = async (roleId, documentId, cookie) => {
+  return resolve({
+    method: "post",
+    url: `${roleDocumentsUrl}/${roleId}/${documentId}`,
+    data: {
+      roleId: roleId,
+      documentId: documentId
+    },
+    responseType: "json",
+    headers: { "X-CSRF-Header": cookie }
+  });
+};
+
+export const removeRoleAccessToDocument = async (roleId, documentId, cookie) => {
+  return resolve({
+    method: "delete",
+    url: `${roleDocumentsUrl}/${roleId}/${documentId}`,
+    responseType: "json",
+    headers: { "X-CSRF-Header": cookie }
+  });
+};
