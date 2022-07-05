6c6a6762f176e78aa3baceddd0855829c21110d5
Michelle Balducci <michelle.balducci@gmail.com>
2022-07-05 13:20:50 -0500

    services class

--- server/src/main/java/com/objectcomputing/checkins/services/onboardeeprofile/OnboardingProfileServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/onboardeeprofile/OnboardingProfileServicesImpl.java
@@ -1,11 +1,15 @@
 package com.objectcomputing.checkins.services.onboardeeprofile;
 
+import com.objectcomputing.checkins.exceptions.AlreadyExistsException;
 import com.objectcomputing.checkins.exceptions.NotFoundException;
 import io.micronaut.core.annotation.Nullable;
 import jakarta.inject.Singleton;
+import static com.objectcomputing.checkins.util.Util.nullSafeUUIDToString;
+import com.objectcomputing.checkins.services.team.member.TeamMemberServices;
 
 import javax.validation.constraints.NotNull;
 import java.util.*;
+
 
 @Singleton
 public class OnboardingProfileServicesImpl implements OnboardingProfileServices  {
@@ -15,7 +19,7 @@ public class OnboardingProfileServicesImpl implements OnboardingProfileServices 
         this.onboardingProfileRepository = onboardingProfileRepository;
     }
 
-    @Override
+//    @Override
     public Onboarding_profile getByID(@NotNull UUID id) {
         Optional<Onboarding_profile> onboardingProfile = onboardingProfileRepository.findById(id);
         if (onboardingProfile.isEmpty()) {
@@ -24,7 +28,7 @@ public class OnboardingProfileServicesImpl implements OnboardingProfileServices 
         return onboardingProfile.get();
     }
 
-    @Override
+//    @Override
     public Set<Onboarding_profile> findByValues (
             @Nullable UUID id,
             @Nullable String firstName,
@@ -41,6 +45,31 @@ public class OnboardingProfileServicesImpl implements OnboardingProfileServices 
 
         return onboarding_profiles;
     }
+
+    @Override
+    public Onboarding_profile saveProfile(Onboarding_profile onboarding_profile) {
+        Onboarding_profile employeeSocialSecurityNumber = onboardingProfileRepository.findBySocial(onboarding_profile.socialSecurityNumber()).orElse(null);
+
+        if (employeeSocialSecurityNumber != null && employeeSocialSecurityNumber.getId() != null && !Objects.equals(onboarding_profile.getId(), employeeSocialSecurityNumber.getId())) {
+            throw new AlreadyExistsException(String.format("Employee SSN already exists in database",
+                    onboarding_profile.getSocialSecurityNumber()));
+        }
+
+        if (onboarding_profile.getId() == null) {
+            return onboardingProfileRepository.save(onboarding_profile);
+        }
+
+        return onboardingProfileRepository.update(onboarding_profile);
+    }
+
+    @Override
+    public Boolean deleteProfile(@NotNull UUID id) {
+        memberProfileRepository.deleteById(id);
+        return true;
+    }
+
+
+
 
 
 }
