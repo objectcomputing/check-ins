9849ef905e9be7d828d6bdf46b517c2ccd111471
danie1r <r.seunghyeondaniel@wustl.edu>
2022-07-06 15:58:39 -0500

    commiting change

--- server/src/main/java/com/objectcomputing/checkins/services/onboard/signrequest/SignRequestController.java
+++ server/src/main/java/com/objectcomputing/checkins/services/onboard/signrequest/SignRequestController.java
@@ -8,7 +8,7 @@ import io.micronaut.http.annotation.*;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import jakarta.inject.Inject;
-
+import io.micronaut.context.annotation.Property;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.security.authentication.Authentication;
@@ -21,10 +21,14 @@ import java.util.*;
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller
 public class SignRequestController {
+
     @Inject
     @Client("https://ocitest.signrequest.com/api/v1/")
     private HttpClient httpClient;
     private Map<String,String> signerInfo = new HashMap<>();
+
+    @Property(name = "signrequest-credentials.signrequest_token")
+    private String SIGNREQUEST_TOKEN;
 
     @Get("/signer")
     public String getData(){
@@ -32,18 +36,7 @@ public class SignRequestController {
         try{
             String retrieve = httpClient.toBlocking()
                     .retrieve(HttpRequest.GET("/documents/152408ef-4b71-40a4-9416-92011a443450/")
-                            .header("Authorization","Token 8e3b636de707468e40abd0d8e212c2507e5d2197"));
-//            int openSignerIndex = retrieve.indexOf("signers");
-//            int closeSignerIndex = retrieve.indexOf("after_document");
-//            String substring = retrieve.substring(openSignerIndex+11,closeSignerIndex-2);
-//            String[] stringArr = substring.split(",");
-//            for (String item : stringArr) {
-//                String[] stringPair = item.split(":");
-//                signerInfo.put(stringPair[0],stringPair[1]);
-//            }
-//
-//            signer = new Signer(signerInfo.get("\"email\""), signerInfo.get("\"signed\""), signerInfo.get("\"viewed\""));
-//            return signer.toString();
+                            .header("Authorization", SIGNREQUEST_TOKEN));
             return retrieve;
         }
         catch (Exception e){
