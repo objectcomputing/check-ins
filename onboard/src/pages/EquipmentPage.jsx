130f7c8a917164f0dc8407c3c26cce0787c42b8d
Matthew Backes <backesm@objectcomputing.com>
2022-07-12 11:30:28 -0500

    Included-additional-testing-files

--- onboard/src/pages/EquipmentPage.jsx
+++ onboard/src/pages/EquipmentPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Button, Box, Input } from "@mui/material";
+import { Button, Box, OutlinedInput, ToggleButton, Grid } from "@mui/material";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import "./EquipmentPage.css";
 
 const ariaLabel = { "aria-label": "description" };
@@ -14,11 +15,11 @@ const equipmentList = [
 ];
 
 const EquipmentPage = () => {
-  const [isMacActive, setIsMacActive] = useState(false);
+  const [currentOS, setCurrentOS] = useState("windows");
   const [currentAccessory, setCurrentAccessory] = useState(equipmentList);
 
-  const handleClick = () => {
-    setIsMacActive((current) => !current);
+  const handleClick = (event, newOS) => {
+    setCurrentOS(newOS);
   };
 
   const handleAccessoryClick = (index) => {
@@ -29,87 +30,101 @@ const EquipmentPage = () => {
 
   return (
     <div>
-      <center>
-        <h1>Please select your computer Prefrence</h1>
-        <ButtonGroup
-          variant="contained"
-          sx={{
-            fullWidth: "true",
-          }}
-        >
-          <Button
-            sx={{
-              "&:hover": {
-                color: "white",
-                backgroundColor: "lightblue",
-              },
-              color: !isMacActive ? "white" : "#1666b6",
-              backgroundColor: !isMacActive ? "#1666b6" : "white",
-            }}
-            onClick={handleClick}
-          >
-            Windows
-          </Button>
-          <Button
-            sx={{
-              "&:hover": {
-                color: "white",
-                backgroundColor: "lightblue",
-              },
-              color: isMacActive ? "white" : "#1666b6",
-              backgroundColor: isMacActive ? "#1666b6" : "white",
-            }}
-            onClick={handleClick}
-          >
-            Mac OS
-          </Button>
-        </ButtonGroup>
-      </center>
+      <h2 style={{ marginTop: '3rem' }}>Please select your computer preference:</h2>
+      <ToggleButtonGroup
+        size="medium"
+        value={currentOS}
+        exclusive
+        onChange={handleClick}
+        sx={{
+          gap: 15,
+          "& .Mui-selected": {
+            backgroundColor: "#1666b6 !important", color: "white !important",
+            fontWeight: "bold"
+          }, "& .MuiToggleButtonGroup-grouped:not(:first-of-type)": {
+            marginLeft: '0px !important',
+            borderLeft: '1px solid rgba(25, 118, 210, 0.5) !important'
+          }
+        }}
+      >
+        <ToggleButton value="windows" sx={{
+          transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+          border: '1px solid rgba(25, 118, 210, 0.5)',
+          color: 'rgb(25, 118, 210)', borderRadius: '4px !important', boxShadow: '0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)'
+        }}>Windows</ToggleButton>
+        <ToggleButton value="mac" sx={{
+          transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+          border: '1px solid rgba(25, 118, 210, 0.5)',
+          color: 'rgb(25, 118, 210)', borderRadius: '4px !important', boxShadow: '0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)'
+        }}>Mac</ToggleButton>
+        <ToggleButton value="linux" sx={{
+          transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+          border: '1px solid rgba(25, 118, 210, 0.5)',
+          color: 'rgb(25, 118, 210)', borderRadius: '4px !important', boxShadow: '0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)'
+        }}>Linux</ToggleButton>
+      </ToggleButtonGroup>
 
-      <center>
-        <h1>Please select your computer accessories</h1>
-        <ButtonGroup
-          sx={{
-            gap: 1,
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 1fr)",
-          }}
-        >
-          {equipmentList &&
-            equipmentList.map((item, i) => {
-              return (
-                <Button
-                  variant="contained"
-                  key={item.id}
-                  onClick={() => handleAccessoryClick(i)}
-                  sx={{
-                    "&:hover": {
-                      color: "white",
-                      backgroundColor: "lightblue",
-                    },
-                    backgroundColor: item.isClicked ? "#1666b6" : "white",
-                    color: !item.isClicked ? "#1666b6" : "white",
-                  }}
-                >
-                  {item.name}
-                </Button>
-              );
-            })}
-        </ButtonGroup>
-      </center>
+      <h2 style={{ marginTop: '3rem' }}>Please select your computer accessories:</h2>
+      <Grid container justifyContent="center" alignitems="center" direction="row" item xs={12}>
+        <Grid item xs={8}>
+          <ButtonGroup
+            sx={{
+              gap: 6,
+              "& .Mui-selected": {
+                backgroundColor: "#1666b6 !important", color: "white !important",
+                fontWeight: "bold"
+              }, "& .MuiButtonGroup-grouped:not(:last-of-type)": {
+                marginLeft: '0px !important',
+                border: '1px solid rgba(25, 118, 210, 0.5) !important'
+              }, ".MuiButtonGroup-grouped:hover": {
+                textDecoration: 'none',
+                backgroundColor: 'rgba(0, 0, 0, 0.04)'
+              }
+            }}
+          >
+            {currentAccessory &&
+              currentAccessory.map((item, i) => {
+                return (
+                  <Button
+                    className={item.isClicked ? "Mui-selected" : ""}
+                    variant="contained"
+                    key={item.id}
+                    onClick={() => handleAccessoryClick(i)}
+                    sx={{
+                      transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+                      border: '1px solid rgba(25, 118, 210, 0.5)',
+                      color: 'rgb(25, 118, 210)', borderRadius: '4px !important', backgroundColor: 'transparent'
+                    }}
+                  >
+                    {item.name}
+                  </Button>
+                );
+              })}
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+
 
       <Box
         component="form"
         sx={{
-          "& > :not(style)": { mt: 2, width: "50vh" },
+          marginTop: '3rem'
         }}
         noValidate
         autoComplete="off"
       >
-        <Input
-          placeholder="Please list any other equipments you would like..."
-          inputProps={ariaLabel}
-        />
+        <h2>Please list any other equipment you would need:</h2>
+        <Grid container justifyContent="center" alignItems="center" direction="row" item xs={12}>
+          <Grid item xs={8}>
+            <OutlinedInput
+              fullWidth
+              multiline
+              maxRows={8}
+              minRows={3}
+              inputProps={ariaLabel}
+            />
+          </Grid>
+        </Grid>
       </Box>
     </div>
   );
