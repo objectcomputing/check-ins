59d398c0f785b2b42371d42aaae2c7787cd2c8d7
Brandon Li <lib@oci2016macbookpro162021.local>
2022-07-14 14:47:17 -0500

    Fixed iFrameURL type

--- onboard/src/pages/TestEmbeddedSigningPage.jsx
+++ onboard/src/pages/TestEmbeddedSigningPage.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./TestEmbeddedSigningPage.css";
 
-const EmbedRequest = () => {
+const EmbedRequest = ({ iframeUrl }) => {
   return (
     <div className="wrapContent">
       <center>
         <h1>Please Sign the Form Below</h1>
       </center>
       <div id="content">
-        <iframe
-          className="iFrameWrapper"
-          // Use Optional Rendering for this iFrame
-          src=""
-          title="Test Embedded Sign Request Form"
-        ></iframe>
+        {iframeUrl && (
+          <iframe
+            className="iFrameWrapper"
+            src={iframeUrl}
+            title="Test Embedded Sign Request Form"
+          ></iframe>
+        )}
       </div>
     </div>
   );
