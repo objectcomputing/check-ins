36dcf110c58c7e62616220f0927a68251fb031a1
BenVuong <ben.vuong2000@gmail.com>
2022-07-18 10:00:34 -0500

    initial commit

---
+++ server/src/main/java/com/objectcomputing/checkins/services/checkins/onboard/onboardeeEmploymentEligibility.java
@@ -0,0 +1,26 @@
+package com.objectcomputing.checkins.services.checkins.onboard;
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.data.annotation.AutoPopulated;
+import io.micronaut.data.annotation.TypeDef;
+import io.micronaut.data.jdbc.annotation.ColumnTransformer;
+import io.micronaut.data.model.DataType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+
+@Entity
+@Introspected
+@Table(name = "onboard_employment_eligibility")
+
+public class onboardeeEmploymentEligibility {
+    
+}
