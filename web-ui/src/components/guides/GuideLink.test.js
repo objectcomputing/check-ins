34c90ede03408f02cf18744e7c1fb839a711fc54
pieperm <michaelepieper@gmail.com>
2022-07-19 09:51:14 -0500

    Update snapshot test

--- web-ui/src/components/guides/GuideLink.test.js
+++ web-ui/src/components/guides/GuideLink.test.js
@@ -3,5 +3,13 @@ import GuideLink from "./GuideLink";
 import { MemoryRouter } from 'react-router-dom';
 
 it("renders correctly", () => {
-  snapshot(<MemoryRouter><GuideLink name="myFileName" /></MemoryRouter>);
+  snapshot(<MemoryRouter>
+    <GuideLink
+      document={{
+        name: "myFileName",
+        url: "/pdfs/myFileName.pdf"
+      }}
+      draggable={false}
+    />
+  </MemoryRouter>);
 });
