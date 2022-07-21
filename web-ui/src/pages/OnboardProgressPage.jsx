4febf3310d5c7ab008e1a7c773811025d1f4508e
Matthew Backes <backesm@objectcomputing.com>
2022-07-21 14:53:51 -0500

    Add modal that opens.

--- web-ui/src/pages/OnboardProgressPage.jsx
+++ web-ui/src/pages/OnboardProgressPage.jsx
@@ -6,9 +6,30 @@ import { Box } from "@mui/system";
 import SearchIcon from "@mui/icons-material/Search";
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
+import { Button, Modal, Typography } from "@mui/material";
+import { useState } from "react";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  backgroundColor: '#fff',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 export default function OnboardProgressPage() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
     { field: "name", headerName: "Name", width: 100 },
@@ -51,11 +72,26 @@ export default function OnboardProgressPage() {
               </InputAdornment>
             ),
           }}
-          variant="standard"/>
+          variant="standard" />
 
-        <Button variant="contained" sx={{ ml: "64%" }}>
+        <Button onClick={handleOpen} variant="contained" sx={{ ml: "64%" }}>
           Add Onboardee
         </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+
+        </Modal>
         <DataGrid
           rows={rows}
           columns={columns}
