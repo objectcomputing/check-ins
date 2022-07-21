ee6b5bda0a0c7376ecf12b189c81f6e092a686b5
Brandon Li <lib@oci2016macbookpro162021.local>
2022-07-21 16:46:55 -0500

    Start setting up SignRequest DTO

--- onboard-server/src/main/java/com/objectcomputing/checkins/services/onboard/signrequest/SignRequestCreateDTO.java
+++ onboard-server/src/main/java/com/objectcomputing/checkins/services/onboard/signrequest/SignRequestCreateDTO.java
@@ -1,4 +1,19 @@
 package com.objectcomputing.checkins.services.onboard.signrequest;
 
+import io.micronaut.core.annotation.Introspected;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.NotBlank;
+
+@Introspected
 public class SignRequestCreateDTO {
+
+    @NotBlank
+    @Schema(title = "The title of the new SignRequest request", required = true)
+    private String title;
+
+    @NotBlank
+    @Schema(title = "The description of the request", required = true)
+    private String body;
+
 }
