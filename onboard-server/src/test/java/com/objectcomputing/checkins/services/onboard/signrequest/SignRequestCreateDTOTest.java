49c44e2cdf6d63e2c9c0a009ac17198b788699ba
Brandon Li <lib@oci2016macbookpro162021.local>
2022-07-25 16:31:07 -0500

    Create testConstraintViolation() test

--- onboard-server/src/test/java/com/objectcomputing/checkins/services/onboard/signrequest/SignRequestCreateDTOTest.java
+++ onboard-server/src/test/java/com/objectcomputing/checkins/services/onboard/signrequest/SignRequestCreateDTOTest.java
@@ -5,6 +5,11 @@ import io.micronaut.validation.validator.Validator;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 @MicronautTest
@@ -16,6 +21,7 @@ public class SignRequestCreateDTOTest {
     @Test
     void testDTOInstantiation() {
         SignRequestCreateDTO dto = new SignRequestCreateDTO();
+
         assertNull(dto.getEmail());
         assertNull(dto.getFile());
         assertNull(dto.getName());
@@ -26,5 +32,19 @@ public class SignRequestCreateDTOTest {
         assertNull(dto.getDeleteDays());
         assertNull(dto.getSigners());
     }
+
+    @Test
+    void testConstraintViolation() {
+        SignRequestCreateDTO dto = new SignRequestCreateDTO();
+
+        Set<ConstraintViolation<SignRequestCreateDTO>> violations = validator.validate(dto);
+        // Think this should be 9, not 4
+        assertEquals(violations.size(), 4);
+        for (ConstraintViolation<SignRequestCreateDTO> violation: violations) {
+            assertEquals(violation.getMessage(), "must not be blank");
+        }
+    }
+
+
 
 }
