58ee7f9f9d8e0d057affad79dd09501e33df18e4
BenVuong <ben.vuong2000@gmail.com>
2022-07-25 11:09:48 -0500

    added an accordion with sections to the modal

--- web-ui/src/pages/OnboardProgressDetailPage.jsx
+++ web-ui/src/pages/OnboardProgressDetailPage.jsx
@@ -3,15 +3,37 @@ import { useLocation } from "react-router-dom";
 import { Box } from "@mui/system";
 import { Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import "./../components/modal/Modal"
+import "./../components/modal/Modal";
 import "./OnboardProgressDetailPage.css";
 import Modal from "./../components/modal/Modal";
+import Accordion from "../components/accordion/Accordion";
+import { isArrayPresent } from "../utils/helperFunction";
 import { useState } from "react";
 
 export default function OnboardProgressDetailPage() {
   const location = useLocation();
   const { name, email, hireType } = location.state;
   const [show, setShow] = useState(false);
+  const accordionArr = [
+    {
+      title: "Personal Information",
+    },
+    {
+      title: "Employment Eligbility",
+    },
+    {
+      title: "Employment Desired and Availability",
+    },
+    {
+      title: "Education",
+    },
+    {
+      title: "Employment History",
+    },
+    {
+      title: "Referral Type and Signature",
+    },
+  ];
   const columns = [
     { field: "id", headerName: "#", width: 50 },
     { field: "documentName", headerName: "Document Name", width: 300 },
@@ -40,12 +62,33 @@ export default function OnboardProgressDetailPage() {
   return (
     <div className="detail-onboard">
       <Box sx={{ height: 400, width: "30%", mt: "5%", ml: "5%" }}>
-        <Button onClick={()=>setShow(true)} variant="contained" sx={{ fontSize: "1vw" }}>
+        <Button
+          onClick={() => setShow(true)}
+          variant="contained"
+          sx={{ fontSize: "1vw" }}
+        >
           Personal Information
         </Button>
-        <Modal title="Personal Information" onClose={() => setShow(false)} show={show}>
-        <p>This is modal body</p>
-      </Modal>
+        <Modal
+          title="Personal Information"
+          onClose={() => setShow(false)}
+          show={show}
+        >
+          <div>
+            {isArrayPresent(accordionArr) &&
+              accordionArr.map((arr, i) => {
+                return (
+                  <Accordion
+                    key={i}
+                    title={arr.title}
+                    open={i === 0 ? true : false}
+                    index={i}
+                    content={arr.content}
+                  />
+                );
+              })}
+          </div>
+        </Modal>
         <h1>Name: {name}</h1>
         <h1>Email: {email} </h1>
         <h1>Hire Type: {hireType}</h1>
@@ -64,7 +107,6 @@ export default function OnboardProgressDetailPage() {
         <Button variant="contained" href="/onboard/progress">
           Back
         </Button>
-        
       </Box>
     </div>
   );
