6aaa219de3c5147f0f24146bb6a9795229936016
BenVuong <ben.vuong2000@gmail.com>
2022-07-25 10:44:17 -0500

    added a basic modal when the personal information button is clicked on the onboard progress detail page

--- web-ui/src/pages/OnboardProgressDetailPage.jsx
+++ web-ui/src/pages/OnboardProgressDetailPage.jsx
@@ -3,11 +3,15 @@ import { useLocation } from "react-router-dom";
 import { Box } from "@mui/system";
 import { Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import "./../components/modal/Modal"
 import "./OnboardProgressDetailPage.css";
+import Modal from "./../components/modal/Modal";
+import { useState } from "react";
 
 export default function OnboardProgressDetailPage() {
   const location = useLocation();
   const { name, email, hireType } = location.state;
+  const [show, setShow] = useState(false);
   const columns = [
     { field: "id", headerName: "#", width: 50 },
     { field: "documentName", headerName: "Document Name", width: 300 },
@@ -36,9 +40,12 @@ export default function OnboardProgressDetailPage() {
   return (
     <div className="detail-onboard">
       <Box sx={{ height: 400, width: "30%", mt: "5%", ml: "5%" }}>
-        <Button variant="contained" sx={{ fontSize: "1vw" }}>
+        <Button onClick={()=>setShow(true)} variant="contained" sx={{ fontSize: "1vw" }}>
           Personal Information
         </Button>
+        <Modal title="My Modal" onClose={() => setShow(false)} show={show}>
+        <p>This is modal body</p>
+      </Modal>
         <h1>Name: {name}</h1>
         <h1>Email: {email} </h1>
         <h1>Hire Type: {hireType}</h1>
