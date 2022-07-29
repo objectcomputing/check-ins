9992842e08eb0e85658beafd80c99700af6b25fb
Michelle Balducci <michelle.balducci@gmail.com>
2022-07-29 15:23:31 -0500

    new modal for "onboardee added" with submit button

--- web-ui/src/pages/OnboardProgressPage.jsx
+++ web-ui/src/pages/OnboardProgressPage.jsx
@@ -3,22 +3,48 @@ import { DataGrid } from "@mui/x-data-grid";
 import { Link, useHistory } from "react-router-dom";
 import "./OnboardProgressPage.css";
 import { Box } from "@mui/system";
-import FileUploadIcon from "@mui/icons-material/FileUpload"
+import FileUploadIcon from "@mui/icons-material/FileUpload";
 import SearchIcon from "@mui/icons-material/Search";
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
-import { Button, Modal, Typography, Grid, Divider, IconButton, Autocomplete } from "@mui/material";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import ThumbsUp from "../assets/thumb.png";
+
+import {
+  Button,
+  Modal,
+  Typography,
+  Grid,
+  Divider,
+  IconButton,
+  Autocomplete,
+} from "@mui/material";
 import { useState } from "react";
 import ProgressIndicator from "../components/onboard_progress/ProgressIndicator";
 
 const modalBoxStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '75%',
-  backgroundColor: '#fff',
-  border: '2px solid #000',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "75%",
+  backgroundColor: "#fff",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  m: 2,
+};
+
+const modalBoxStyleMini = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "25%",
+  backgroundColor: "#fff",
+  border: "2px solid #000",
   boxShadow: 24,
   pt: 2,
   px: 4,
@@ -28,36 +54,45 @@ const modalBoxStyle = {
 
 export default function OnboardProgressPage() {
   const [open, setOpen] = useState(false);
-  const [empFile, setEmpFile] = useState(' ');
-  const [offer, setOfferFile] = useState(' ');
+  const [empFile, setEmpFile] = useState(" ");
+  const [offer, setOfferFile] = useState(" ");
+  const [addOnboardeeModal, setAddOnboardeeModal] = useState(false);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
     setOpen(false);
-    setEmpFile(' ');
-    setOfferFile(' ');
+    setEmpFile(" ");
+    setOfferFile(" ");
+  };
+  const handleSubmitClose = () => {
+    setOpen(false);
+    setAddOnboardeeModal(true);
+  };
+  const handleMsgModalClose = () => {
+    setAddOnboardeeModal(false);
   };
   const handleEmployeeAgreement = (e) => {
-    setEmpFile(e.target.value.replace(/^.*[\\/]/, ''));
+    setEmpFile(e.target.value.replace(/^.*[\\/]/, ""));
   };
   const handleOfferLetter = (e) => {
-    setOfferFile(e.target.value.replace(/^.*[\\/]/, ''));
+    setOfferFile(e.target.value.replace(/^.*[\\/]/, ""));
   };
 
-  const posOptions = ['dummy1', 'dummy2', 'dummy3'];
-  const hireOptions = ['dummy4', 'dummy5', 'dummy6'];
-  const pdlOptions = ['dummy7', 'dummy8', 'dummy9'];
+  const posOptions = ["dummy1", "dummy2", "dummy3"];
+  const hireOptions = ["dummy4", "dummy5", "dummy6"];
+  const pdlOptions = ["dummy7", "dummy8", "dummy9"];
 
   const history = useHistory();
   const handleRowClick = (name, email, hireType, userID) => {
     history.push({
-      pathname: `/onboard/progress/${userID}`, state: {
+      pathname: `/onboard/progress/${userID}`,
+      state: {
         name: name,
         email: email,
         hireType: hireType,
-      }
+      },
     });
-  }
+  };
 
   const columns = [
     { field: "id", headerName: "ID", width: 50 },
@@ -120,9 +155,32 @@ export default function OnboardProgressPage() {
     },
   ];
 
-  const rowsNotif = [{ id: 1, notificationMsg: "daniel finished something.", userID: 1, name: "Daniel Ryu", email: "ryud@objectcomputing.com", hireType: "Intern" },
-  { id: 2, notificationMsg: "Brandon did something.", userID: 2, name: "Brandon", email: "lib@objectcomputing.com", hireType: "Intern" },
-  { id: 3, notificationMsg: "Daniel did something else.", userID: 1, name: "Daniel Ryu", email: "ryud@objectcomputing.com", hireType: "Intern" }];
+  const rowsNotif = [
+    {
+      id: 1,
+      notificationMsg: "daniel finished something.",
+      userID: 1,
+      name: "Daniel Ryu",
+      email: "ryud@objectcomputing.com",
+      hireType: "Intern",
+    },
+    {
+      id: 2,
+      notificationMsg: "Brandon did something.",
+      userID: 2,
+      name: "Brandon",
+      email: "lib@objectcomputing.com",
+      hireType: "Intern",
+    },
+    {
+      id: 3,
+      notificationMsg: "Daniel did something else.",
+      userID: 1,
+      name: "Daniel Ryu",
+      email: "ryud@objectcomputing.com",
+      hireType: "Intern",
+    },
+  ];
 
   return (
     <div className="onboard-page">
@@ -140,14 +198,20 @@ export default function OnboardProgressPage() {
           variant="standard"
         />
 
-        <Button onClick={handleOpen} variant="contained" sx={{ ml: "64%" }}>
+        <Button
+          onClick={handleOpen}
+          variant="contained"
+          sx={{ ml: "64%" }}
+          startIcon={<PersonAddIcon />}
+        >
           Add Onboardee
         </Button>
         <Modal
           open={open}
           onClose={handleClose}
           aria-labelledby="title"
-          aria-describedby="description">
+          aria-describedby="description"
+        >
           <Box sx={modalBoxStyle}>
             <Typography align="center" id="title" variant="h3" component="h2">
               Add Onboardee:
@@ -161,7 +225,10 @@ export default function OnboardProgressPage() {
                   disablePortal
                   options={posOptions}
                   sx={{ width: "75%" }}
-                  renderInput={(option) => <TextField variant="outlined" {...option} />} />
+                  renderInput={(option) => (
+                    <TextField variant="outlined" {...option} />
+                  )}
+                />
               </Grid>
               <Grid item xs={6}>
                 <Typography id="description" sx={{ mt: 2 }}>
@@ -171,7 +238,10 @@ export default function OnboardProgressPage() {
                   disablePortal
                   options={hireOptions}
                   sx={{ width: "75%" }}
-                  renderInput={(option) => <TextField variant="outlined" {...option} />} />
+                  renderInput={(option) => (
+                    <TextField variant="outlined" {...option} />
+                  )}
+                />
               </Grid>
             </Grid>
             <Grid container space={2}>
@@ -179,13 +249,21 @@ export default function OnboardProgressPage() {
                 <Typography id="description" sx={{ mt: 2 }}>
                   First Name:
                 </Typography>
-                <TextField sx={{ width: "75%" }} id="firstName" variant="outlined" />
+                <TextField
+                  sx={{ width: "75%" }}
+                  id="firstName"
+                  variant="outlined"
+                />
               </Grid>
               <Grid item xs={6}>
                 <Typography id="description" sx={{ mt: 2 }}>
                   Last Name:
                 </Typography>
-                <TextField sx={{ width: "75%" }} id="lastName" variant="outlined" />
+                <TextField
+                  sx={{ width: "75%" }}
+                  id="lastName"
+                  variant="outlined"
+                />
               </Grid>
             </Grid>
             <Grid container space={3}>
@@ -193,7 +271,11 @@ export default function OnboardProgressPage() {
                 <Typography id="description" sx={{ mt: 2 }}>
                   Email:
                 </Typography>
-                <TextField sx={{ width: "75%" }} id="email" variant="outlined" />
+                <TextField
+                  sx={{ width: "75%" }}
+                  id="email"
+                  variant="outlined"
+                />
               </Grid>
               <Grid item xs={6}>
                 <Typography id="description" sx={{ mt: 2 }}>
@@ -203,40 +285,117 @@ export default function OnboardProgressPage() {
                   disablePortal
                   options={pdlOptions}
                   sx={{ width: "75%" }}
-                  renderInput={(option) => <TextField variant="outlined" {...option} />} />
+                  renderInput={(option) => (
+                    <TextField variant="outlined" {...option} />
+                  )}
+                />
               </Grid>
             </Grid>
             <Divider sx={{ m: 3 }} variant="middle" />
-            <Grid container flexDirection="column" alignItems="center" rowSpacing={3}>
-              <Grid item xs={'auto'}>
-                <Typography align="center" id="description" sx={{ mt: 0, display: "inline-flex" }}>
+            <Grid
+              container
+              flexDirection="column"
+              alignItems="center"
+              rowSpacing={3}
+            >
+              <Grid item xs={"auto"}>
+                <Typography
+                  align="center"
+                  id="description"
+                  sx={{ mt: 0, display: "inline-flex" }}
+                >
                   Offer Letter:
                 </Typography>
                 <IconButton component="label">
-                  <input hidden accept=".pdf" type="file" id="offerLetter" onChange={handleOfferLetter} />
+                  <input
+                    hidden
+                    accept=".pdf"
+                    type="file"
+                    id="offerLetter"
+                    onChange={handleOfferLetter}
+                  />
                   <FileUploadIcon />
                 </IconButton>
-                <Typography sx={{ display: "inline-flex", fontStyle: 'italic', fontSize: 12, marginLeft: 5 }}>{offer}</Typography>
+                <Typography
+                  sx={{
+                    display: "inline-flex",
+                    fontStyle: "italic",
+                    fontSize: 12,
+                    marginLeft: 5,
+                  }}
+                >
+                  {offer}
+                </Typography>
               </Grid>
-              <Grid item id="description" xs={'auto'}>
-                <Typography align="center" id="description" sx={{ mt: 2, display: "inline-flex" }}>
+              <Grid item id="description" xs={"auto"}>
+                <Typography
+                  align="center"
+                  id="description"
+                  sx={{ mt: 2, display: "inline-flex" }}
+                >
                   Employment Agreement:
                 </Typography>
                 <IconButton component="label">
-                  <input hidden accept=".pdf" type="file" id="empAgreement" onChange={handleEmployeeAgreement} />
+                  <input
+                    hidden
+                    accept=".pdf"
+                    type="file"
+                    id="empAgreement"
+                    onChange={handleEmployeeAgreement}
+                  />
                   <FileUploadIcon />
                 </IconButton>
-                <Typography sx={{ display: "inline-flex", fontStyle: 'italic', fontSize: 12, marginLeft: 5 }}>{empFile}</Typography>
+                <Typography
+                  sx={{
+                    display: "inline-flex",
+                    fontStyle: "italic",
+                    fontSize: 12,
+                    marginLeft: 5,
+                  }}
+                >
+                  {empFile}
+                </Typography>
               </Grid>
             </Grid>
             <Grid container>
-              <Grid item xs={6} style={{ display: "flex", justifyContent: "flex-start" }}>
-                <Button variant="contained" onClick={handleClose} >Cancel</Button>
+              <Grid
+                item
+                xs={6}
+                style={{ display: "flex", justifyContent: "flex-start" }}
+              >
+                <Button variant="contained" onClick={handleClose}>
+                  Cancel
+                </Button>
               </Grid>
-              <Grid item xs={6} style={{ display: "flex", justifyContent: "flex-end" }}>
-                <Button variant="contained">Submit</Button>
+              <Grid
+                item
+                xs={6}
+                style={{ display: "flex", justifyContent: "flex-end" }}
+              >
+                <Button variant="contained" onClick={handleSubmitClose}>
+                  Submit
+                </Button>
               </Grid>
             </Grid>
+          </Box>
+        </Modal>
+        <Modal
+          open={addOnboardeeModal}
+          onClose={handleClose}
+          aria-labelledby="title"
+          aria-describedby="description"
+        >
+          <Box sx={modalBoxStyleMini}>
+            <div style={{textAlign:"center",marginLeft:"auto", marginRight:"auto", marginTop:"auto", marginBottom:"auto"}}>
+              <Typography variant="p" component="h3">
+                Onboardee added!
+              </Typography>
+            </div>
+            <div >
+              <Button variant="contained" onClick={handleMsgModalClose} style={{display:"flex", justifyContent:"centered", gap:"10px"}}>
+                Okay
+              </Button>
+            </div>
           </Box>
         </Modal>
         <DataGrid
@@ -257,7 +416,12 @@ export default function OnboardProgressPage() {
           rowsPerPageOptions={[10]}
           onRowClick={(params, event) => {
             if (!event.ignore) {
-              handleRowClick(params.row.name, params.row.email, params.row.hireType, params.row.userID);
+              handleRowClick(
+                params.row.name,
+                params.row.email,
+                params.row.hireType,
+                params.row.userID
+              );
             }
           }}
         />
