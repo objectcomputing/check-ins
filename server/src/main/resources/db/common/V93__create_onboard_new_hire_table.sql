e073104a156c623c4f5a52e317287797cf67660a
S78901 <39834839+S78901@users.noreply.github.com>
2022-08-09 09:39:58 -0500

    Add 'new_hire_credentials' table

--- server/src/main/resources/db/common/V93__create_onboard_new_hire_table.sql
+++ server/src/main/resources/db/common/V93__create_onboard_new_hire_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE new_hire_account(
-            new_hire_account_id varchar PRIMARY KEY,
-            state varchar,
-            email_address varchar(255),
-            created_instant timestamp,
-            changed_instant timestamp
+    new_hire_account_id varchar PRIMARY KEY,
+    state varchar,
+    email_address varchar(255),
+    created_instant timestamp,
+    changed_instant timestamp
 );
---
+++ server/src/main/resources/db/common/V94__create_onboard_new_hire_credentials_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE new_hire_credentials(
+    new_hire_credentials_id varchar PRIMARY KEY,
+    salt varchar,
+    verifier varchar
+);
