56c83fde16a0be9969bcb15fe219f0da138d2de4
li-brandon <li.brandon@outlook.com>
2022-08-11 16:19:12 -0500

    Complete EmploymentPreferencesFixture

--- server/src/test/java/com/objectcomputing/checkins/services/fixture/EmploymentPreferencesFixture.java
+++ server/src/test/java/com/objectcomputing/checkins/services/fixture/EmploymentPreferencesFixture.java
@@ -8,7 +8,12 @@ public interface EmploymentPreferencesFixture extends RepositoryFixture {
     LocalDate date = LocalDate.of(2020, 1, 8);
 
     default EmploymentDesiredAvailability createADefaultEmploymentPreferences() {
-        return getEmploymentDesiredA().save(new EmploymentDesiredAvailability("Boss", date, "100",
+        return getEmploymentDesiredAvailabilityRepository().save(new EmploymentDesiredAvailability("Boss", date, "100",
                 true, true, true, true, date));
+    }
+
+    default EmploymentDesiredAvailability createSecondDefaultEmploymentPreferences() {
+        return getEmploymentDesiredAvailabilityRepository().save(new EmploymentDesiredAvailability("Worker", date, "10",
+                false, false, false, false, date));
     }
 }
