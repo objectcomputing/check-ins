4245c324914fe7d098c3a64b70beec718531f7b0
Michelle Balducci <michelle.balducci@gmail.com>
2022-08-15 11:55:25 -0500

    another api change

--- web-ui/src/pages/OnboardProgressPage.jsx
+++ web-ui/src/pages/OnboardProgressPage.jsx
@@ -5,11 +5,11 @@ import "./OnboardProgressPage.css";
 import { Box } from "@mui/system";
 import { AppContext } from "../context/AppContext";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import AddOnboardeeModal from "../components/modal/AddOnboardeeModal";
+import AddOnboardModal from "../components/modal/AddOnboardeeModal";
 import { Button } from "@mui/material";
 import { useState, useContext } from "react";
 import { UPDATE_ONBOARDEE_MEMBER_PROFILES } from "../context/actions";
-import { createOnboardee } from "../api/onboardeeMember";
+import { createOnboardee, initializeOnboardee } from "../api/onboardeeMember";
 
 export default function OnboardProgressPage(onboardee) {
   const [open, setOpen] = useState(false);
@@ -19,14 +19,13 @@ export default function OnboardProgressPage(onboardee) {
   const handleOpen = () => setOpen(true);
 
   const history = useHistory();
-  const handleRowClick = (name, email, hireType, userID, title) => {
+  const handleRowClick = (name, email, hireType, userID) => {
     history.push({
       pathname: `/onboard/progress/${userID}`,
       state: {
         name: name,
         email: email,
         hireType: hireType,
-        title: title,
       },
     });
   };
@@ -44,7 +43,6 @@ export default function OnboardProgressPage(onboardee) {
       },
       width: 220,
     },
-    { field: "title", headerName: "Title", width: 150 },
     { field: "hireType", headerName: "Hire Type", width: 150 },
     {
       field: "completed",
@@ -63,8 +61,6 @@ export default function OnboardProgressPage(onboardee) {
                 name: cellValues.row.name,
                 email: cellValues.row.email,
                 hireType: cellValues.row.hireType,
-                title: cellValues.row.title,
-                completed: cellValues.row.completed
               },
             }}
           >
@@ -81,19 +77,17 @@ export default function OnboardProgressPage(onboardee) {
       id: 1,
       name: "Daniel Ryu",
       email: "d97shryu@gmail.com",
-      title: "Intern",
-      completed: "Not Completed",
+      hireType: "Intern",
+      completed: "No",
       dateAdded: "Jul 15, 2022",
-      hireType: "Hourly",
     },
     {
       id: 2,
       name: "Brandon Li",
       email: "li.brandon@outlook.com",
-      title: "Intern",
-      completed: "Not Completed",
+      hireType: "Intern",
+      completed: "No",
       dateAdded: "Jul 15, 2022",
-      hireType: "Hourly",
     },
   ];
 
@@ -113,8 +107,7 @@ export default function OnboardProgressPage(onboardee) {
       userID: 1,
       name: "Daniel Ryu",
       email: "ryud@objectcomputing.com",
-      title: "Intern",
-      hireType: "Hourly",
+      hireType: "Intern",
     },
     {
       id: 2,
@@ -122,8 +115,7 @@ export default function OnboardProgressPage(onboardee) {
       userID: 2,
       name: "Brandon",
       email: "lib@objectcomputing.com",
-      title: "Intern",
-      hireType: "Hourly",
+      hireType: "Intern",
     },
     {
       id: 3,
@@ -131,10 +123,35 @@ export default function OnboardProgressPage(onboardee) {
       userID: 1,
       name: "Daniel Ryu",
       email: "ryud@objectcomputing.com",
-      title: "Intern",
-      hireType: "Hourly",
+      hireType: "Intern",
     },
   ];
+
+  async function submitInfo(onboardee) {
+    if (
+      onboardee.firstName &&
+      onboardee.lastName &&
+      onboardee.position &&
+      onboardee.email &&
+      onboardee.hireType &&
+      onboardee.pdl &&
+      csrf
+    ) {
+
+      let res1 = await initializeOnboardee(onboardee.email, csrf);
+      console.log(res1);
+
+      // let res2 = await createOnboardee(onboardee, csrf);
+      // let data2 =
+      //   res2.payload && res2.payload.data && !res2.error ? res2.payload.data : null;
+      // if (data2) {
+      //   dispatch({
+      //     type: UPDATE_ONBOARDEE_MEMBER_PROFILES,
+      //     payload: [...onboardeeProfiles, data2],
+      //   });
+      // }
+    }
+  }
 
   return (
     <div className="onboard-page">
@@ -147,33 +164,11 @@ export default function OnboardProgressPage(onboardee) {
         >
           Add Onboardee
         </Button>
-        <AddOnboardeeModal
+        <AddOnboardModal
           onboardee={{}}
           open={open}
           onClose={handleAddModalClose}
-          onSave={async (onboardee) => {
-            if (
-              onboardee.firstName &&
-              onboardee.lastName &&
-              onboardee.position &&
-              onboardee.email &&
-              onboardee.hireType &&
-              onboardee.pdl &&
-              csrf
-            ) {
-              let res = await createOnboardee(onboardee, csrf);
-              let data =
-                res.payload && res.payload.data && !res.error
-                  ? res.payload.data
-                  : null;
-              if (data) {
-                dispatch({
-                  type: UPDATE_ONBOARDEE_MEMBER_PROFILES,
-                  payload: [...onboardeeProfiles, data],
-                });
-              }
-            }
-          }}
+          onSave={submitInfo}
         />
         <DataGrid
           rows={rows}
@@ -205,8 +200,7 @@ export default function OnboardProgressPage(onboardee) {
                 params.row.name,
                 params.row.email,
                 params.row.hireType,
-                params.row.userID,
-                params.row.title
+                params.row.userID
               );
             }
           }}
