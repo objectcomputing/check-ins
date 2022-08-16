c5428ba23f1bbae45a38eca3ce17b4e498a92833
danie1r <r.seunghyeondaniel@wustl.edu>
2022-08-16 14:08:28 -0500

    deleted onboard related DTO from onboard-server

--- onboard-server/src/main/java/com/objectcomputing/checkins/services/onboard/exceptions/BadArgException.java
+++
@@ -1,7 +0,0 @@
-package com.objectcomputing.checkins.services.onboard.exceptions;
-
-public class BadArgException extends RuntimeException {
-    public BadArgException(String message) {
-        super(message);
-    }
-}
--- onboard-server/src/main/java/com/objectcomputing/checkins/services/onboard/exceptions/PermissionException.java
+++
@@ -1,7 +0,0 @@
-package com.objectcomputing.checkins.services.onboard.exceptions;
-
-public class PermissionException extends RuntimeException {
-    public PermissionException(String message) {
-        super(message);
-    }
-}
