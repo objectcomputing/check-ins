790d0660ba01060927ed0304ab32c643559eb5de
li-brandon <li.brandon@outlook.com>
2022-08-17 15:35:34 -0500

    Create V106 Table

---
+++ server/src/main/resources/db/common/V105__employment_desired_availability.sql
@@ -0,0 +1,8 @@
+CREATE TABLE referral_type(
+        id varchar PRIMARY KEY,
+        discoveredOpportunity varchar,
+        referredBy varchar,
+        referrerEmail varchar,
+        referrerJobSite varchar,
+        referralTypeOther varchar,
+);
