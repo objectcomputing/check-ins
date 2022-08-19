55727c042fecad4981a1737bc685f0d7c6206168
Matthew Backes <backesm@objectcomputing.com>
2022-08-19 13:59:01 -0500

    Undid unneeded changes

--- onboard-server/src/main/java/com/objectcomputing/checkins/services/onboard/onboardeeprofile/OnboardingProfileRepository.java
+++ onboard-server/src/main/java/com/objectcomputing/checkins/services/onboard/onboardeeprofile/OnboardingProfileRepository.java
@@ -34,7 +34,7 @@ public interface OnboardingProfileRepository extends ReactorCrudRepository<Onboa
 
         Flux<OnboardingProfile> findAll();
 
-        @Query(value = "SELECT id, " +
+        @Query(value = "SELECT onboard_profile_id, " +
                         "PGP_SYM_DECRYPT(cast(mp.firstName as bytea),'${aes.key}') as firstName, " +
                         "PGP_SYM_DECRYPT(cast(mp.middleName as bytea),'${aes.key}') as middleName," +
                         "PGP_SYM_DECRYPT(cast(mp.lastName as bytea),'${aes.key}') as lastName," +
