f9fdd2d2f6f22dec407b6217675cb5f954d399a8
pieperm <michaelepieper@gmail.com>
2022-08-25 10:10:21 -0500

    Create kudos service

---
+++ server/src/main/java/com/objectcomputing/checkins/services/kudos/KudosServices.java
@@ -0,0 +1,19 @@
+package com.objectcomputing.checkins.services.kudos;
+
+import io.micronaut.core.annotation.Nullable;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface KudosServices {
+
+    Kudos save(Kudos kudos);
+
+    Kudos update(Kudos kudos);
+
+    Kudos getById(UUID id);
+
+    boolean delete(UUID id);
+
+    List<Kudos> findByValues(@Nullable UUID senderId, @Nullable UUID recipientId, @Nullable Boolean includePending);
+}
