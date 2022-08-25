28792b357f801108b3010fff0f43f09ac5d355ba
pieperm <michaelepieper@gmail.com>
2022-08-25 10:08:52 -0500

    Create migration for kudos table

---
+++ server/src/main/resources/db/common/V108__create_kudos_table.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS kudos;
+CREATE TABLE kudos(
+    id varchar PRIMARY KEY,
+    message varchar,
+    senderid varchar REFERENCES member_profile(id),
+    recipientid varchar REFERENCES member_profile(id),
+    datecreated date,
+    dateapproved date
+);
