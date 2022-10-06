79d4e76955752260d9f4d19df310802effae4ed5
zsbrown97 <zackbrownbandofficial@gmail.com>
2022-10-06 13:06:39 -0500

    Installed tinyMCE and yanked out Quill

--- web-ui/src/components/markdown-note/MarkdownNote.jsx
+++ web-ui/src/components/markdown-note/MarkdownNote.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useRef} from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { Editor } from '@tinymce/tinymce-react';
 const modules = {
   toolbar: [
     [{ header: [1, 2, false] }],
@@ -29,13 +30,36 @@ const formats = [
   "link",
 ];
 
+
+
 export default function MarkdownNote(props) {
+  const editorRef = useRef(null);
+  function saveTextHandler() {
+    const text = editorRef.current.getContent();
+    console.log(text)
+    return text;
+  };
   return (
-    <ReactQuill
-      {...props}
-      theme="snow"
-      modules={modules}
-      formats={formats}
-    ></ReactQuill>
+      <>
+      <Editor
+          apiKey='246ojmsp6c7qtnr9aoivktvi3mi5t7ywuf0vevn6wllfcn9e'
+          onInit={(evt, editor) => editorRef.current = editor}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+              'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+              'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+            ],
+            toolbar: 'undo redo | blocks | ' +
+                'bold italic forecolor | alignleft aligncenter ' +
+                'alignright alignjustify | bullist numlist outdent indent | ' +
+                'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+      />
+        <button onClick={saveTextHandler}>Log content</button>
+      </>
   );
 }
