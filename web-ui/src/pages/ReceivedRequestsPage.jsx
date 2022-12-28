bdd6a72016a05cb8865547717264a98f35dc44f3
Jesse Hanner <hannerj@objectcomputing.com>
2022-12-28 10:36:22 -0600

    fixed gradle build issue

--- web-ui/src/pages/ReceivedRequestsPage.jsx
+++ web-ui/src/pages/ReceivedRequestsPage.jsx
@@ -35,8 +35,7 @@ const Root = styled("div")({
   [`& .${classes.pageTitle}`]: {
     paddingRight: "0.4em",
     minWidth: "330px",
-    ["@media screen and (max-width: 600px)"]: {
-      // eslint-disable-line no-useless-computed-key
+    ["@media screen and (max-width: 600px)"]: { // eslint-disable-line no-useless-computed-key
       fontSize: "30px",
       width: "100%",
       padding: 0,
@@ -49,8 +48,7 @@ const Root = styled("div")({
   },
   [`& .${classes.searchField}`]: {
     width: "100%",
-    ["@media screen and (max-width: 840px)"]: {
-      // eslint-disable-line no-useless-computed-key
+    ["@media screen and (max-width: 840px)"]: { // eslint-disable-line no-useless-computed-key
       marginBottom: "1em",
     },
   },
