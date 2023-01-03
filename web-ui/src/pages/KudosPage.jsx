325c5425a2f3bb006cb36a5ae3bf8a116b55a113
zsbrown97 <zackbrownbandofficial@gmail.com>
2023-01-03 09:50:18 -0600

    no sent kudos message on kudos page

--- web-ui/src/pages/KudosPage.jsx
+++ web-ui/src/pages/KudosPage.jsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useContext, useEffect, useState} from "react";
 import {styled} from "@mui/material/styles";
-import {Button, Collapse, Divider, IconButton, Tab, Typography} from "@mui/material";
+import {Button, Tab, Typography} from "@mui/material";
 import {TabContext, TabList, TabPanel} from '@mui/lab';
 import {AppContext} from "../context/AppContext";
 import {selectCsrfToken, selectCurrentUser} from "../context/selectors";
@@ -164,8 +164,8 @@ const KudosPage = () => {
                     />
                   )}
               </div>
-              : <div className="no-pending-kudos-message">
-                  <Typography variant="body2">There are currently no pending kudos</Typography>
+              : <div className="no-received-kudos-message">
+                  <Typography variant="body2">There are currently no received kudos</Typography>
               </div>
             )
           }
@@ -174,11 +174,15 @@ const KudosPage = () => {
         {sentKudosLoading
             ? Array.from({length: 5}).map((_, index) => <SkeletonLoader key={index} type="kudos"/>)
             : (
+              sentKudos.length > 0 ?
               <div>
                 {sentKudos.map(k =>
                   <KudosCard key={k.id} kudos={k}/>
                 )}
-              </div>
+              </div> 
+              : <div className="no-sent-kudos-message">
+                  <Typography variant="body2">There are currently no sent kudos</Typography>
+                </div>
             )
           }
         </TabPanel>
