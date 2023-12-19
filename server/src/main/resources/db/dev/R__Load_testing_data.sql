80b7c3add0a383f67fdcde7e43b710a0cc13a2d7
Sam Bley <hello@sambley.me>
2023-12-19 14:56:40 -0600

    add create checkins elevated permissions for private notes and checkin documents

--- server/src/main/resources/db/dev/R__Load_testing_data.sql
+++ server/src/main/resources/db/dev/R__Load_testing_data.sql
@@ -650,6 +650,11 @@ insert into role_permissions
 values
     ('e8a4fff8-e984-4e59-be84-a713c9fa8d23', '6c144b56-3c1c-472f-8c3e-243e5a4149a1'); -- CAN_VIEW_CHECKINS_ELEVATED
 
+insert into role_permissions
+(roleid, permissionid)
+values
+    ('e8a4fff8-e984-4e59-be84-a713c9fa8d23', 'a5a71f7d-a3f5-4126-8aaf-0ad18a06615a'); -- CAN_CREATE_CHECKINS_ELEVATED
+
 
 -- PDL Permissions
 insert into role_permissions
@@ -696,6 +701,11 @@ insert into role_permissions
 (roleid, permissionid)
 values
     ('d03f5f0b-e29c-4cf4-9ea4-6baa09405c56', '6c144b56-3c1c-472f-8c3e-243e5a4149a1'); -- CAN_VIEW_CHECKINS_ELEVATED
+
+insert into role_permissions
+(roleid, permissionid)
+values
+    ('d03f5f0b-e29c-4cf4-9ea4-6baa09405c56', '6c144b56-3c1c-472f-8c3e-243e5a4149a1'); -- CAN_CREATE_CHECKINS_ELEVATED
 
 
 -- Member permissions
