3919d18db72a2db02f649976d55f313bd04f8439
S78901 <39834839+S78901@users.noreply.github.com>
2024-03-04 00:20:23 -0600

    [2064] Add Post action for role permissions

--- web-ui/src/api/rolepermissions.js
+++ web-ui/src/api/rolepermissions.js
@@ -1,11 +1,21 @@
 import { resolve } from "./api.js";
 
-const rolePermissionsListUrl = "/services/roles/role-permissions"
+const rolePermissionsListUrl = "/services/roles/role-permissions";
 
 export const getRolePermissionsList = async (cookie) => {
-    return resolve({
-      url: rolePermissionsListUrl,
-      responseType: "json",
-      headers: { "X-CSRF-Header": cookie },
-    });
-  };
+  return resolve({
+    url: rolePermissionsListUrl,
+    responseType: "json",
+    headers: { "X-CSRF-Header": cookie },
+  });
+};
+
+export const postRolePermissionsList = async (roleData, cookie) => {
+  return resolve({
+    method: "post",
+    url: rolePermissionsListUrl,
+    responseType: "json",
+    data: roleData,
+    headers: { "X-CSRF-Header": cookie },
+  });
+};
