6a2eb598114157265e2f11e9b3535954bf370a08
pieperm <michaelepieper@gmail.com>
2024-03-28 14:25:43 -0500

    Add selectable members to dialog, search by member name

--- web-ui/src/components/member_selector/MemberSelector.jsx
+++ web-ui/src/components/member_selector/MemberSelector.jsx
@@ -1,15 +1,15 @@
-import React, {useContext, useState} from "react";
+import React, {useCallback, useContext, useState} from "react";
 import PropTypes from "prop-types";
 import {
   AppBar,
   Avatar, Button,
   Card,
-  CardHeader, DialogContent,
+  CardHeader, Checkbox, DialogContent,
   IconButton,
   List,
   ListItem,
-  ListItemAvatar,
-  ListItemText, Toolbar,
+  ListItemAvatar, ListItemButton,
+  ListItemText, TextField, Toolbar,
   Tooltip, Typography
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
@@ -20,6 +20,10 @@ import {selectCurrentMembers} from "../../context/selectors";
 import Dialog from "@mui/material/Dialog";
 import Slide from "@mui/material/Slide";
 import CloseIcon from "@mui/icons-material/Close";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+
+import "./MemberSelector.css";
 
 const DialogTransition = React.forwardRef((props, ref) => (
   <Slide direction="up" ref={ref} {...props}/>
@@ -42,6 +46,22 @@ const MemberSelector = (onChange) => {
   const members = selectCurrentMembers(state);
   const [selectedMembers, setSelectedMembers] = useState([members[0], members[1]]);
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [nameQuery, setNameQuery] = useState("");
+
+  const getSelectableMembers = useCallback(() => {
+    // Only include members that are not already selected
+    let selectableMembers = members.filter(member =>
+      !selectedMembers.includes(member)
+    );
+
+    // Filter by member name
+    selectableMembers = selectableMembers.filter(member => {
+      const sanitizedQuery = nameQuery.trim().toLowerCase();
+      return member.name.toLowerCase().includes(sanitizedQuery);
+    });
+
+    return selectableMembers;
+  }, [members, selectedMembers, nameQuery]);
 
   return (
     <>
@@ -58,6 +78,7 @@ const MemberSelector = (onChange) => {
           {selectedMembers.length
             ? (selectedMembers.map(member =>
               <ListItem
+                key={member.id}
                 role="listitem"
                 secondaryAction={
                   <Tooltip title="Deselect member" arrow>
@@ -81,6 +102,7 @@ const MemberSelector = (onChange) => {
         </List>
       </Card>
       <Dialog
+        className="member-selector-dialog"
         open={dialogOpen}
         fullScreen
         onClose={() => setDialogOpen(false)}
@@ -93,12 +115,43 @@ const MemberSelector = (onChange) => {
             </IconButton>
             <Typography variant="h6" flexGrow={1}>Select Members</Typography>
             <Button color="inherit">
-              Save
+              Add
             </Button>
           </Toolbar>
         </AppBar>
-        <DialogContent>
-
+        <DialogContent className="member-selector-dialog-content">
+          <TextField
+            label="Name"
+            placeholder="Search by member name"
+            variant="outlined"
+            value={nameQuery}
+            onChange={(event) => setNameQuery(event.target.value)}
+            InputProps={{
+              endAdornment: <InputAdornment position="end" color="gray"><SearchIcon/></InputAdornment>
+            }}
+          />
+          <List dense role="list">
+            {getSelectableMembers().map(member => (
+              <ListItem
+                key={member.id}
+                role="listitem"
+                disablePadding
+                secondaryAction={
+                  <Checkbox disableRipple/>
+                }
+              >
+                <ListItemButton>
+                  <ListItemAvatar>
+                    <Avatar src={getAvatarURL(member.workEmail)}/>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={<Typography fontWeight="bold">{member.name}</Typography>}
+                    secondary={<Typography color="textSecondary" component="h6">{member.title}</Typography>}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
         </DialogContent>
       </Dialog>
     </>
