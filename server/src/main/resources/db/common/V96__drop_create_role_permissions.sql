3ef395ff1dbca185de9162d7665da3eb73c98c33
Zachary Klein <zak@silver-chalice.com>
2024-04-02 14:56:07 -0500

    - Migrate existing role_permission records to new table

--- server/src/main/resources/db/common/V96__drop_create_role_permissions.sql
+++ server/src/main/resources/db/common/V96__drop_create_role_permissions.sql
@@ -1,7 +1,23 @@
-drop table if exists role_permissions;
-CREATE TABLE role_permissions(
-                                 roleid varchar references role(id),
-                                 permission varchar,
-                                 primary key(roleid, permission)
+CREATE TABLE tmp_role_permissions
+(
+    roleid     varchar references role (id),
+    permission varchar,
+    primary key (roleid, permission)
 );
-drop table if exists permissions;
+
+
+
+INSERT INTO tmp_role_permissions (roleid, permission)
+SELECT rp.roleid, p.permission
+FROM role_permissions rp
+         JOIN
+     permissions p
+     ON
+         rp.permissionid = p.id;
+
+
+DROP TABLE role_permissions;
+DROP TABLE permissions;
+
+ALTER TABLE tmp_role_permissions
+    RENAME TO role_permissions;
