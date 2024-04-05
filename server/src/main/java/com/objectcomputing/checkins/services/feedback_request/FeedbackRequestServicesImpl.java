f961144c119b13f7e98ff4c7f8bc69cbde8aa8bb
estradaw <estradaw@objectcomputing>
2024-04-05 14:45:02 -0500

    issue#2173 tweak

--- server/src/main/java/com/objectcomputing/checkins/services/feedback_request/FeedbackRequestServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/feedback_request/FeedbackRequestServicesImpl.java
@@ -93,7 +93,7 @@ public class FeedbackRequestServicesImpl implements FeedbackRequestServices {
         if (feedbackRequest.getDueDate() != null && feedbackRequest.getSendDate().isAfter(feedbackRequest.getDueDate())){
             throw new BadArgException("Send date of feedback request must be before the due date.");
         }
-        String status = feedbackRequest.getSendDate().equals(LocalDate.now()) ? "sent" : "pending";
+        String status = feedbackRequest.getSendDate().isAfter(LocalDate.now()) ? "pending" : "sent";
         feedbackRequest.setStatus(status);
         FeedbackRequest storedRequest = feedbackReqRepository.save(feedbackRequest);
         if (feedbackRequest.getSendDate().equals(LocalDate.now())) {
