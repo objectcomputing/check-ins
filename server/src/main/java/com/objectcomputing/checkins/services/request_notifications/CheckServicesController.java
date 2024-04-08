93c887bd78c7a94668662a64f4f25f7d8a15c89b
borinquenkid <wbduque@mac.com>
2024-04-08 08:08:24 -0500

    Sending scheduled emails cleaned up (#2175)
    * issue#2173 Made code more robust.
    
    * issue#2173 Made code more robust.
    
    * issue#2173 remove logging
    
    * issue#2173 delegate email sending
    
    * issue#2173 PR comments
    
    * issue#2173 more PR comments
    
    * issue#2173 tweak
    
    ---------
    
    Co-authored-by: estradaw <estradaw@objectcomputing>

--- server/src/main/java/com/objectcomputing/checkins/services/request_notifications/CheckServicesController.java
+++ server/src/main/java/com/objectcomputing/checkins/services/request_notifications/CheckServicesController.java
@@ -1,10 +1,6 @@
 package com.objectcomputing.checkins.services.request_notifications;
 
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -16,66 +12,34 @@ import jakarta.inject.Named;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 
 @Controller("/services/feedback/daily-request-check")
 @PermitAll
 public class CheckServicesController {
+
+
     private final CheckServices checkServices;
     private final EventLoopGroup eventLoopGroup;
     private final ExecutorService ioExecutorService;
-    private final GoogleIdTokenVerifier verifier =
-            new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                    .setAudience(Collections.singletonList("https://checkins.objectcomputing.com/services/feedback/daily-request-check"))
-                    .build();
+    private final ServiceAccountVerifier serviceAccountVerifier;
 
     public CheckServicesController(CheckServices checkServices, EventLoopGroup eventLoopGroup,
-                              @Named(TaskExecutors.IO) ExecutorService ioExecutorService) {
+                                   @Named(TaskExecutors.IO) ExecutorService ioExecutorService, ServiceAccountVerifier serviceAccountVerifier) {
         this.checkServices = checkServices;
         this.eventLoopGroup = eventLoopGroup;
         this.ioExecutorService = ioExecutorService;
+        this.serviceAccountVerifier = serviceAccountVerifier;
     }
 
     @Get
-    public Mono<? extends HttpResponse<?>> GetTodaysRequests(@Header("Authorization") String authorizationHeader) {
-        System.out.println("!!!!!!-"+authorizationHeader);
+    public Mono<? extends HttpResponse<?>> sendScheduledEmails(@Header("Authorization") String authorizationHeader) {
         String authorization = authorizationHeader.split(" ")[1];
-        GoogleIdToken idToken = null;
-        try {
-            idToken = verifier.verify(authorization);
-            GoogleIdToken.Payload payload = idToken.getPayload();
-
-            // Print user identifier
-            String userId = payload.getSubject();
-            System.out.println("User ID: " + userId);
-
-            // Get profile information from payload
-            String email = payload.getEmail();
-            System.out.println("Email:"+ email);
-            String hostedDomain = payload.getHostedDomain();
-            System.out.println("Hosted Domain" + hostedDomain);
-            String prettyString = payload.toPrettyString();
-            System.out.println(prettyString);
-            boolean emailVerified = payload.getEmailVerified();
-            System.out.println("emailVerified:" + emailVerified);
-//            String name = (String) payload.get("name");
-//            String pictureUrl = (String) payload.get("picture");
-//            String locale = (String) payload.get("locale");
-//            String familyName = (String) payload.get("family_name");
-//            String givenName = (String) payload.get("given_name");
-        } catch (GeneralSecurityException | IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        GoogleIdToken.Payload payload = idToken.getPayload();
-        return Mono.fromCallable(checkServices::GetTodaysRequests)
+        serviceAccountVerifier.verify(authorization);
+        return Mono.fromCallable(checkServices::sendScheduledEmails)
                 .publishOn(Schedulers.fromExecutor(eventLoopGroup))
                 .map(success -> (HttpResponse<?>) HttpResponse.ok())
                 .subscribeOn(Schedulers.fromExecutor(ioExecutorService));
-
     }
 
 }
--- server/src/main/java/com/objectcomputing/checkins/services/request_notifications/CheckServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/request_notifications/CheckServicesImpl.java
@@ -1,52 +1,33 @@
 package com.objectcomputing.checkins.services.request_notifications;
 
-import com.objectcomputing.checkins.notifications.email.EmailSender;
-import com.objectcomputing.checkins.notifications.email.MailJetConfig;
 import com.objectcomputing.checkins.services.feedback_request.FeedbackRequest;
 import com.objectcomputing.checkins.services.feedback_request.FeedbackRequestRepository;
-import com.objectcomputing.checkins.services.memberprofile.MemberProfile;
-import com.objectcomputing.checkins.services.memberprofile.MemberProfileServices;
-import io.micronaut.context.annotation.Property;
-import jakarta.inject.Named;
+import com.objectcomputing.checkins.services.feedback_request.FeedbackRequestServicesImpl;
 import jakarta.inject.Singleton;
+
 import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
 
 @Singleton
 public class CheckServicesImpl implements CheckServices {
-    private final FeedbackRequestRepository feedbackReqRepository;
-    public static final String FEEDBACK_REQUEST_NOTIFICATION_SUBJECT = "check-ins.application.feedback.notifications.subject";
-    public static final String FEEDBACK_REQUEST_NOTIFICATION_CONTENT = "check-ins.application.feedback.notifications.content";
-    public static final String submitURL = "https://checkins.objectcomputing.com/feedback/submit?requestId=";
-    private EmailSender emailSender;
-    private String notificationSubject;
-    private String notificationContent;
-    private final MemberProfileServices memberProfileServices;
+    private final FeedbackRequestServicesImpl feedbackRequestServices;
+    private final FeedbackRequestRepository feedbackRequestRepository;
 
-    public CheckServicesImpl(FeedbackRequestRepository feedbackReqRepository,
-                             @Named(MailJetConfig.HTML_FORMAT) EmailSender emailSender,
-                             @Property(name = FEEDBACK_REQUEST_NOTIFICATION_SUBJECT) String notificationSubject,
-                             @Property(name = FEEDBACK_REQUEST_NOTIFICATION_CONTENT) String notificationContent,
-                             MemberProfileServices memberProfileServices) {
-        this.feedbackReqRepository = feedbackReqRepository;
-        this.emailSender = emailSender;
-        this.notificationContent = notificationContent;
-        this.notificationSubject = notificationSubject;
-        this.memberProfileServices = memberProfileServices;
+    public CheckServicesImpl(FeedbackRequestServicesImpl feedbackRequestServices,
+                             FeedbackRequestRepository feedbackRequestRepository) {
+        this.feedbackRequestServices = feedbackRequestServices;
+        this.feedbackRequestRepository = feedbackRequestRepository;
     }
 
     @Override
-    public boolean GetTodaysRequests() {
+    public boolean sendScheduledEmails() {
         LocalDate today = LocalDate.now();
-        List<FeedbackRequest> todaysRequests = new ArrayList<>();
-        todaysRequests.addAll(feedbackReqRepository.findByValues(null, null, null, today, null, null));
-        for (FeedbackRequest req: todaysRequests) {
-            MemberProfile from = memberProfileServices.getById(req.getCreatorId());
-            String fromName  = from.getFirstName() + " " + from.getLastName();
-            String newContent =  notificationContent + "<a href=\""+submitURL+req.getId()+"\">Check-Ins application</a>.";
-            emailSender.sendEmail(fromName, from.getWorkEmail(), notificationSubject, newContent);
+        List<FeedbackRequest> feedbackRequests = feedbackRequestRepository.findBySendDateBeforeAndStatusEqual(today, "pending");
+        for (FeedbackRequest req: feedbackRequests) {
+            feedbackRequestServices.sendNewRequestEmail(req);
+            req.setStatus("sent");
+            feedbackRequestRepository.update(req);
         }
-
         return true;
     }
 
