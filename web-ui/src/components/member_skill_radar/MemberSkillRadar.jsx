2ed865c5a02d3219c62809c49416a77519cdab71
pieperm <michaelepieper@gmail.com>
2024-04-18 14:26:17 -0500

    Pick radar colors from predefined list

--- web-ui/src/components/member_skill_radar/MemberSkillRadar.jsx
+++ web-ui/src/components/member_skill_radar/MemberSkillRadar.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 
 import {Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer, Tooltip} from "recharts";
+import {RADAR_COLORS} from "./RadarColors.jsx";
 
 const propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape({
@@ -12,10 +13,11 @@ const propTypes = {
 
 const MemberSkillRadar = ({ data, members }) => {
 
-  const [colors, setColors] = useState({});
+  const [colors, setColors] = useState({});  // Maps member id to object of colors for radar
+  const [colorOptions, setColorOptions] = useState(RADAR_COLORS);  // Collection of predefined colors to choose from
 
-  // Calculates a pair of colors for the fill and stroke
-  const calculateColors = () => {
+  // Generate a random color using HSL
+  const generateColor = () => {
     const hue = Math.random() * 360;
     return {
       fill: `hsl(${hue}, 100%, 50%)`,
@@ -23,14 +25,36 @@ const MemberSkillRadar = ({ data, members }) => {
     };
   }
 
+  // Pick a random color from the predefined colors
+  const pickColor = (options) => {
+    const colorKeys = Object.keys(options);
+    return colorKeys[colorKeys.length * Math.random() << 0];
+  }
+
   useEffect(() => {
     const updatedColors = {...colors};
+    const updatedColorOptions = {...colorOptions};
     members.forEach((member) => {
+      // Only get a new color if the member doesn't have one already
       if (!(member.name in colors)) {
-        updatedColors[member.name] = calculateColors();
+        // Use predefined colors until all those colors have been used
+        if (Object.keys(updatedColorOptions).length > 0) {
+          const colorName = pickColor(updatedColorOptions);
+          const selectedColor = updatedColorOptions[colorName];
+          updatedColors[member.name] = {
+            fill: selectedColor.light,
+            stroke: selectedColor.dark
+          };
+          delete updatedColorOptions[colorName];
+          console.log(`Used color: ${colorName} - ${Object.keys(updatedColorOptions).length} remaining`);
+        } else {
+          updatedColors[member.name] = generateColor();
+          console.log(updatedColors[member.name]);
+        }
       }
     });
     setColors(updatedColors);
+    setColorOptions(updatedColorOptions);
   }, [members]);
 
   return (
@@ -46,11 +70,11 @@ const MemberSkillRadar = ({ data, members }) => {
             name={member.name}
             dataKey={member.name}
             fill={colors[member.name] && colors[member.name].fill}
-            stroke={colors[member.name] && colors[member.name].stroke}
+            stroke={colors[member.name] && colors[member.name].fill}
             fillOpacity={0.6}
           />
         ))}
-        <Legend/>
+        <Legend onClick={(t) => console.log(t)}/>
       </RadarChart>
     </ResponsiveContainer>
   );
---
+++ web-ui/src/components/member_skill_radar/RadarColors.jsx
@@ -0,0 +1,78 @@
+export const RADAR_COLORS = {
+  "red": {
+    light: "#e74c3c",
+    dark: "#7b1a0f"
+  },
+  "rust": {
+    light: "#b54b43",
+    dark: "#6c2d28"
+  },
+  "persimmon": {
+    light: "#e17055",
+    dark: "#862e18"
+  },
+  "emerald": {
+    light: "#00b894",
+    dark: "#005242"
+  },
+  "teal": {
+    light: "#218f8f",
+    dark: "#135353"
+  },
+  "green": {
+    light: "#27ae60",
+    dark: "#155b32"
+  },
+  "viridian": {
+    light: "#468057",
+    dark: "#223f2b"
+  },
+  "blue": {
+    light: "#0984e3",
+    dark: "#054a7f"
+  },
+  "cerulean": {
+    light: "#3c40c6",
+    dark: "#1d1f63"
+  },
+  "twilight": {
+    light: "#49568c",
+    dark: "#2c3454"
+  },
+  "slate": {
+    light: "#717791",
+    dark: "#454959"
+  },
+  "purple": {
+    light: "#6c5ce7",
+    dark: "#241692"
+  },
+  "deep lavender": {
+    light: "#6a4e8a",
+    dark: "#3f2e52"
+  },
+  "deep lilac": {
+    light: "#8e44ad",
+    dark: "#512763"
+  },
+  "indigo": {
+    light: "#442482",
+    dark: "#291650"
+  },
+  "coral": {
+    light: "#ff7675",
+    dark: "#803b3b"
+  },
+  "magenta": {
+    light: "#b53471",
+    dark: "#671e40"
+  },
+  "plum": {
+    light: "#8a2f81",
+    dark: "#3d1539"
+  },
+  "pink": {
+    light: "#c96bc5",
+    dark: "#633461"
+  }
+};
