58310ae26ced6f292a6a1d851c94eab5b7314183
Zachary Klein <zak@silver-chalice.com>
2024-04-18 19:33:09 -0500

    - Resolve #2194 - Lookup permissions by role - Cache permissions per role - Invalidate permissions cache on change

--- server/src/main/java/com/objectcomputing/checkins/security/permissions/PermissionSecurityRule.java
+++ server/src/main/java/com/objectcomputing/checkins/security/permissions/PermissionSecurityRule.java
@@ -1,6 +1,8 @@
 package com.objectcomputing.checkins.security.permissions;
 
+import com.objectcomputing.checkins.services.permissions.Permission;
 import com.objectcomputing.checkins.services.permissions.RequiredPermission;
+import com.objectcomputing.checkins.services.role.role_permissions.RolePermissionServices;
 import io.micronaut.core.annotation.AnnotationValue;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpRequest;
@@ -10,18 +12,25 @@ import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.security.rules.SecurityRuleResult;
 import io.micronaut.web.router.MethodBasedRouteMatch;
 import io.micronaut.web.router.RouteMatch;
+import org.json.JSONArray;
 
 import jakarta.inject.Singleton;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Singleton
 public class PermissionSecurityRule implements SecurityRule {
 
     public static final Integer ORDER = SecuredAnnotationRule.ORDER - 100;
+
+    public final RolePermissionServices rolePermissionServices;
+
+    public PermissionSecurityRule(RolePermissionServices rolePermissionServices) {
+        this.rolePermissionServices = rolePermissionServices;
+    }
 
     public int getOrder() {
         return ORDER;
@@ -31,17 +40,25 @@ public class PermissionSecurityRule implements SecurityRule {
     public Publisher<SecurityRuleResult> check(HttpRequest<?> request, @Nullable RouteMatch<?> routeMatch, @Nullable Authentication authentication) {
 
         if (routeMatch instanceof MethodBasedRouteMatch) {
-            MethodBasedRouteMatch methodBasedRouteMatch = (MethodBasedRouteMatch) routeMatch;
+            MethodBasedRouteMatch methodBasedRouteMatch = (MethodBasedRouteMatch) routeMatch;    if (methodBasedRouteMatch.hasAnnotation(RequiredPermission.class)) {
 
-            if (methodBasedRouteMatch.hasAnnotation(RequiredPermission.class)) {
+
                 AnnotationValue<RequiredPermission> requiredPermissionAnnotation = methodBasedRouteMatch.getAnnotation(RequiredPermission.class);
                 Optional<String> optionalPermission = requiredPermissionAnnotation != null ? requiredPermissionAnnotation.stringValue("value") : Optional.empty();
 
                 Map<String, Object> claims = authentication != null ? authentication.getAttributes() : null;
 
                 if (optionalPermission.isPresent() && claims != null && claims.containsKey("permissions")) {
-                    final String requiredPermission = optionalPermission.get();
-                    final String userPermissions = claims.get("permissions").toString();
+
+                    final Permission requiredPermission = Permission.valueOf(optionalPermission.get());
+                    final Set<Permission> userPermissions = new HashSet<>();
+
+                    JSONArray jsonArray = new JSONArray(claims.get("roles").toString());
+                    final List<String> roles = jsonArray.toList().stream().map(Object::toString).collect(Collectors.toList());
+
+                    roles.forEach(role -> rolePermissionServices.findByRole(role)
+                        .forEach(rolePermission -> userPermissions.add(rolePermission.getPermission()))
+                    );
 
                    return Mono.just(userPermissions.contains(requiredPermission)
                             ? SecurityRuleResult.ALLOWED
