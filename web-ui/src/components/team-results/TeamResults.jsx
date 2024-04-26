dd21de9882604633eb605892d9fbc2c61739d47a
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-26 16:05:54 -0500

    Team Results page now gets settings from query parameters

--- web-ui/src/components/team-results/TeamResults.jsx
+++ web-ui/src/components/team-results/TeamResults.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { styled } from '@mui/material/styles';
 import TeamSummaryCard from './TeamSummaryCard';
 import { AppContext } from '../../context/AppContext';
@@ -38,7 +38,9 @@ const displayName = 'TeamResults';
 const TeamResults = () => {
   const { state } = useContext(AppContext);
   const loading = selectTeamsLoading(state);
+  const [open, setOpen] = useState(false);
   const [searchText, setSearchText] = useState('');
+  const [selectedTeamId, setSelectedTeamId] = useState('');
   const teams = selectNormalizedTeams(state, searchText);
 
   const teamCards = teams.map((team, index) => {
@@ -47,9 +49,37 @@ const TeamResults = () => {
         key={`team-summary-${team.id}`}
         index={index}
         team={team}
+        onTeamSelect={setSelectedTeamId}
+        selectedTeamId={selectedTeamId}
       />
     );
   });
+
+  useEffect(() => {
+    const url = new URL(location.href);
+
+    const addNew = url.searchParams.get('addNew');
+    setOpen(addNew === 'true');
+
+    const search = url.searchParams.get('search') || '';
+    setSearchText(search);
+
+    const selectedTeamId = url.searchParams.get('team') || '';
+    setSelectedTeamId(selectedTeamId);
+  }, []);
+
+  useEffect(() => {
+    const url = new URL(location.href);
+    let newUrl = url.origin + url.pathname;
+    const params = {};
+    if (open) params.addNew = true;
+    if (searchText) params.search = searchText;
+    if (selectedTeamId) params.team = selectedTeamId;
+    if (Object.keys(params).length) {
+      newUrl += '?' + new URLSearchParams(params).toString();
+    }
+    history.replaceState(params, '', newUrl);
+  }, [open, searchText, selectedTeamId]);
 
   return (
     <Root>
@@ -63,7 +93,7 @@ const TeamResults = () => {
             setSearchText(e.target.value);
           }}
         />
-        <TeamsActions />
+        <TeamsActions isOpen={open} onOpen={setOpen} />
       </div>
       <div className="teams">
         {loading
