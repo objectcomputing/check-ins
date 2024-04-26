9e53d7920678232c9d29e6fcbd4450ca3501dd23
S78901 <39834839+S78901@users.noreply.github.com>
2024-04-26 12:58:05 -0500

    [2220] Review Period Stepper Story

---
+++ web-ui/src/components/reviews/periods/ReviewPeriodStepper.stories.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReviewPeriodStepper from './ReviewPeriodStepper';
+
+export default {
+  component: ReviewPeriodStepper,
+  title: 'Check Ins/Review Period Stepper',
+  decorators: [
+    ReviewPeriodStepper => (
+      <div style={{ width: '400px', height: '400px' }}>
+        <ReviewPeriodStepper />
+      </div>
+    )
+  ]
+};
+
+const Template = args => {
+  return <ReviewPeriodStepper {...args} />;
+};
+
+const reviewPeriodStepperData = {
+  reviewPeriod:  {
+        id: "b0b8a6f3-2d15-4923-8552-2f729f58ea0f",
+        name: "Test",
+        open: true,
+        reviewTemplateId: "d1e94b60-47c4-4945-87d1-4dc88f088e57",
+        selfReviewTemplateId: "d1e94b60-47c4-4945-87d1-4dc88f088e57"
+    }
+};
+
+export const ReviewPeriodSteppers = Template.bind({});
+ReviewPeriodSteppers.args = {
+  ...reviewPeriodStepperData
+};
