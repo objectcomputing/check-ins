8c9ea91bc4045ea6a50f3641ee601c7b89e1fba2
S78901 <39834839+S78901@users.noreply.github.com>
2024-04-26 12:50:31 -0500

    [2220] Review Period Stepper component

---
+++ web-ui/src/components/reviews/periods/ReviewPeriodStepper.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+const steps = [
+  'Review Period Is Open',
+  'Review Period Is Closed'
+];
+
+export default function HorizontalLinearAlternativeLabelStepper({reviewPeriod}) {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stepper activeStep={reviewPeriod?.open === true ? 0 : 1} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}
