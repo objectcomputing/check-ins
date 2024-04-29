46af36a9f5df8206a6db40c6b20da1217209b909
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-29 14:43:47 -0500

    removed an unused import

--- web-ui/src/pages/PeoplePage.jsx
+++ web-ui/src/pages/PeoplePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { TextField, Grid } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
