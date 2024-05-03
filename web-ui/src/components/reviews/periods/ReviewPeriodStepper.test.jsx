e6a73166bf85a1d4e123569dfb57a000bc161fa5
S78901 <39834839+S78901@users.noreply.github.com>
2024-05-03 11:24:06 -0500

    [2220] Adjust parameters of test/story data

--- web-ui/src/components/reviews/periods/ReviewPeriodStepper.test.jsx
+++ web-ui/src/components/reviews/periods/ReviewPeriodStepper.test.jsx
@@ -6,6 +6,9 @@ const reviewPeriod = {
   id: "b0b8a6f3-2d15-4923-8552-2f729f58ea0f",
   name: "Test",
   reviewStatus: 'OPEN',
+  launchDate: "2024-05-03T15:53:56.673Z",
+  selfReviewCloseDate: "2024-05-03T15:53:56.673Z",
+  closeDate: "2024-05-03T15:53:56.673Z",
   reviewTemplateId: "d1e94b60-47c4-4945-87d1-4dc88f088e57",
   selfReviewTemplateId: "d1e94b60-47c4-4945-87d1-4dc88f088e57"
 }
