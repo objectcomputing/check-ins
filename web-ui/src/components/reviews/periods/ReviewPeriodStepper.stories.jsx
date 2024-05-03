40556e62f68896beb80abae6faed3226eabee2fe
S78901 <39834839+S78901@users.noreply.github.com>
2024-05-03 11:16:31 -0500

    [2220] Adjust story

--- web-ui/src/components/reviews/periods/ReviewPeriodStepper.stories.jsx
+++ web-ui/src/components/reviews/periods/ReviewPeriodStepper.stories.jsx
@@ -21,7 +21,7 @@ const reviewPeriodStepperData = {
   reviewPeriod:  {
         id: "b0b8a6f3-2d15-4923-8552-2f729f58ea0f",
         name: "Test",
-        open: true,
+        reviewStatus: "OPEN",
         reviewTemplateId: "d1e94b60-47c4-4945-87d1-4dc88f088e57",
         selfReviewTemplateId: "d1e94b60-47c4-4945-87d1-4dc88f088e57"
     }
