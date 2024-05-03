02969839b56f698847ae9fa4f9f11ae577c2e15c
S78901 <39834839+S78901@users.noreply.github.com>
2024-05-03 11:56:23 -0500

    [2220] Finalize Stepper component

--- web-ui/src/components/reviews/periods/ReviewPeriodStepper.jsx
+++ web-ui/src/components/reviews/periods/ReviewPeriodStepper.jsx
@@ -1,24 +1,29 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-
-const options = [
-  { label: 'Planning', option: 'PLANNING' },
-  { label: 'Awaiting Approval', option: 'AWAITING_APPROVAL' },
-  { label: 'Open', option: 'OPEN' },
-  { label: 'Closed', option: 'CLOSED' }
-];
+import optionsArr from './reviewStatus.json';
+import { toInteger } from 'lodash';
 
 export default function ReviewPeriodStepper({ reviewPeriod }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    let index = 0;
+    let revPeriod = reviewPeriod?.reviewStatus;
+    optionsArr.map((opt, i) => {
+      if (opt.option === revPeriod) {
+        index = i;
+      }
+    });
+    setActiveIndex(toInteger(index));
+  }, [reviewPeriod]);
+
   return (
     <Box sx={{ width: '100%' }}>
-      <Stepper
-        activeStep={reviewPeriod?.reviewStatus === option ? 0 : 1}
-        alternativeLabel
-      >
-        {options.map((label, option) => (
+      <Stepper activeStep={activeIndex} alternativeLabel>
+        {optionsArr.map(({ label, option }) => (
           <Step key={option}>
             <StepLabel>{label}</StepLabel>
           </Step>
