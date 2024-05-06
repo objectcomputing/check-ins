b6d5e161f8961414069130eb3c0a1e9bc4a950e2
Josh Habdas <habdasj@objectcomputing.com>
2024-05-06 15:38:08 -0400

    chore: introduce app-wide type definitions

---
+++ web-ui/src/app.d.js
@@ -0,0 +1,28 @@
+// Type definitions specified here will be availalbe app-wide
+
+/**
+ * @typedef {Object} Member
+ * @property {?string} bioText
+ * @property {?Array} birthday
+ * @property {string} empolyeeId
+ * @property {?string} excluded
+ * @property {string} firstName
+ * @property {!string} id
+ * @property {string} lastName
+ * @property {string} location
+ * @property {?string} middleName
+ * @property {string} name
+ * @property {?string} pdlId
+ * @property {Array} startDate
+ * @property {?string} suffix
+ * @property {?string} supervisorid
+ * @property {?Array} terminationDate
+ * @property {string} title
+ * @property {?string} voluntary
+ * @property {string} workEmail
+ */
+
+/**
+ * @typedef {Member} PDL
+ * @property {string} pdlId
+ */
