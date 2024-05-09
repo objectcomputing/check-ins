6126b1689db15bed48512fdfe6295c39f5b5638b
S78901 <39834839+S78901@users.noreply.github.com>
2024-05-09 13:06:48 -0500

    [2345] Add internal_score + external_score columns

--- server/src/main/resources/db/common/V102__alter_pulse_responses_table.sql
+++ server/src/main/resources/db/common/V102__alter_pulse_responses_table.sql
@@ -1,2 +1,4 @@
-ALTER TABLE pulse_response
-DROP COLUMN updatedDate;
+ALTER TABLE pulse_response DROP COLUMN updatedDate;
+
+ALTER TABLE pulse_response ADD COLUMN internal_score Integer;
+ALTER TABLE pulse_response ADD COLUMN external_score Integer;
