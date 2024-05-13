48bf2dee043eb915de29e30e4510e8ec3b4eb215
Jack Keller <jack@mediaextra.net>
2024-05-13 11:23:32 -0500

    adds blank settings page

---
+++ web-ui/src/pages/SettingsPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const displayName = "SettingsPage";
+
+const SettingsPage = () => {
+  return (
+    <div className="settings-page">
+      <blockquote>Settings Page Stuff to go here</blockquote>
+    </div>
+  );
+}
+
+SettingsPage.displayName = displayName;
+
+export default SettingsPage;
