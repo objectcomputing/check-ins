b2e07b191e4b3427499052ce4ebdc56431c9ffac
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-05-16 11:05:52 -0500

    progress on 2347

--- web-ui/src/pages/PulsePage.jsx
+++ web-ui/src/pages/PulsePage.jsx
@@ -6,8 +6,7 @@ import {
   SentimentVeryDissatisfied,
   SentimentVerySatisfied
 } from '@mui/icons-material';
-import { IconButton, Tooltip } from '@mui/material';
-// TODO: Should this use the SentimentIcon component?
+import { Button, IconButton, TextField, Tooltip } from '@mui/material';
 
 import './PulsePage.css';
 
@@ -30,33 +29,52 @@ const tooltips = [
 
 const Pulse = () => {
   const handleClick = () => {};
+  const [comment, setComment] = useState('');
   const [score, setScore] = useState(2);
 
   return (
     <div className="pulse">
-      {icons.map((sentiment, index) => (
-        <Tooltip key={`sentiment-${index}`} title={tooltips[index]} arrow>
-          <IconButton
-            aria-label="sentiment"
-            className={index === score ? 'selected' : ''}
-            onClick={() => setScore(index)}
-            sx={{ color: colors[index] }}
-          >
-            {sentiment}
-          </IconButton>
-        </Tooltip>
-      ))}
+      <div className="icon-row">
+        {icons.map((sentiment, index) => (
+          <Tooltip key={`sentiment-${index}`} title={tooltips[index]} arrow>
+            <IconButton
+              aria-label="sentiment"
+              className={index === score ? 'selected' : ''}
+              onClick={() => setScore(index)}
+              sx={{ color: colors[index] }}
+            >
+              {sentiment}
+            </IconButton>
+          </Tooltip>
+        ))}
+      </div>
+      <TextField
+        fullWidth
+        label="Comment"
+        onChange={e => {
+          setComment(e.target.value);
+        }}
+        placeholder="Comment"
+        value={comment}
+      />
     </div>
   );
 };
 
 const PulsePage = () => {
+  const submit = () => {
+    alert('Submitted');
+  };
+
   return (
     <div className="pulse-page">
-      <h2>Internal</h2>
+      <h2>Internal Feelings</h2>
       <Pulse key="pulse-internal" />
-      <h2>External</h2>
+      <h2>External Feelings</h2>
       <Pulse key="pulse-external" />
+      <Button onClick={submit} variant="contained">
+        Submit
+      </Button>
     </div>
   );
 };
