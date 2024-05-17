a8389822c5b7eb3f033d0b3184a688e83eb2fd51
Jack Keller <jack@mediaextra.net>
2024-05-17 17:26:51 -0500

    ordering

--- web-ui/src/components/settings/index.js
+++ web-ui/src/components/settings/index.js
@@ -1,2 +1,5 @@
-export { default as SettingsString } from './types/string.jsx';
+export { default as SettingsBoolean } from './types/boolean.jsx';
+export { default as SettingsColor } from './types/color.jsx';
+export { default as SettingsFile } from './types/file.jsx';
 export { default as SettingsNumber } from './types/number.jsx';
+export { default as SettingsString } from './types/string.jsx';
