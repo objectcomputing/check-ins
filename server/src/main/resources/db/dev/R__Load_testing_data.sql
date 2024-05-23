f630ee5eb742345ead074a3a5cf089621b4b9fbe
Tim Yates <tim.yates@gmail.com>
2024-05-23 13:47:26 +0100

    Remove duplicate permission from dev migration

--- server/src/main/resources/db/dev/R__Load_testing_data.sql
+++ server/src/main/resources/db/dev/R__Load_testing_data.sql
@@ -825,11 +825,6 @@ values
 insert into role_permissions
 (roleid, permission)
 values
-    ('e8a4fff8-e984-4e59-be84-a713c9fa8d23', 'CAN_ADMINISTER_SETTINGS');
-
-insert into role_permissions
-(roleid, permission)
-values
     ('e8a4fff8-e984-4e59-be84-a713c9fa8d23', 'CAN_VIEW_SETTINGS');
 
 insert into role_permissions
