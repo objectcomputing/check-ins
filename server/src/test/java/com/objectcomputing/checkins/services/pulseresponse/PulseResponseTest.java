40bb9ad939d486c5126b7fc5e3a0454952ccadf6
Christopher McKiernan <49450715+chrismckiernan@users.noreply.github.com>
2024-06-05 08:53:37 -0500

    Update server/src/test/java/com/objectcomputing/checkins/services/pulseresponse/PulseResponseTest.java
    Co-authored-by: Tim Yates <yatest@objectcomputing.com>

--- server/src/test/java/com/objectcomputing/checkins/services/pulseresponse/PulseResponseTest.java
+++ server/src/test/java/com/objectcomputing/checkins/services/pulseresponse/PulseResponseTest.java
@@ -21,8 +21,6 @@ import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 class PulseResponseTest extends TestContainersSuite {
