f94adb7e29afa222c279d5714a41c0a937ba18f5
Christopher McKiernan <mckiernanc@objectcomputing.com>
2024-06-05 08:14:40 -0500

    Email pdl/supervisor when a member marks any low score (<=2) in pulse survey

--- server/src/main/java/com/objectcomputing/checkins/services/pulseresponse/PulseResponseServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/pulseresponse/PulseResponseServicesImpl.java
@@ -1,40 +1,50 @@
 package com.objectcomputing.checkins.services.pulseresponse;
 
 import com.objectcomputing.checkins.exceptions.BadArgException;
+import com.objectcomputing.checkins.notifications.email.EmailSender;
+import com.objectcomputing.checkins.notifications.email.MailJetConfig;
+import com.objectcomputing.checkins.services.memberprofile.MemberProfile;
 import com.objectcomputing.checkins.services.memberprofile.MemberProfileRepository;
 import com.objectcomputing.checkins.services.memberprofile.MemberProfileServices;
 import com.objectcomputing.checkins.services.memberprofile.currentuser.CurrentUserServices;
 import com.objectcomputing.checkins.services.permissions.Permission;
 import com.objectcomputing.checkins.services.role.role_permissions.RolePermissionServices;
+import io.micronaut.core.annotation.NonNull;
+import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import jakarta.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Singleton
 public class PulseResponseServicesImpl implements PulseResponseService {
 
+    private static final Logger log = LoggerFactory.getLogger(PulseResponseServicesImpl.class);
     private final PulseResponseRepository pulseResponseRepo;
     private final MemberProfileServices memberProfileServices;
     private final MemberProfileRepository memberRepo;
     private final CurrentUserServices currentUserServices;
     private final RolePermissionServices rolePermissionServices;
+    private final EmailSender emailSender;
 
     public PulseResponseServicesImpl(
             PulseResponseRepository pulseResponseRepo,
             MemberProfileServices memberProfileServices,
             MemberProfileRepository memberRepo,
             CurrentUserServices currentUserServices,
-            RolePermissionServices rolePermissionServices
+            RolePermissionServices rolePermissionServices,
+            @Named(MailJetConfig.HTML_FORMAT) EmailSender emailSender
     ) {
         this.pulseResponseRepo = pulseResponseRepo;
         this.memberProfileServices = memberProfileServices;
         this.memberRepo = memberRepo;
         this.currentUserServices = currentUserServices;
         this.rolePermissionServices = rolePermissionServices;
+        this.emailSender = emailSender;
     }
 
     @Override
@@ -55,6 +65,10 @@ public class PulseResponseServicesImpl implements PulseResponseService {
             }
             pulseResponseRet = pulseResponseRepo.save(pulseResponse);
         }
+
+        // Send low pulse survey score if appropriate
+        sendPulseLowScoreEmail(pulseResponseRet);
+
         return pulseResponseRet;
     }
 
@@ -89,6 +103,11 @@ public class PulseResponseServicesImpl implements PulseResponseService {
             }
             pulseResponseRet = pulseResponseRepo.update(pulseResponse);
         }
+
+        System.out.printf("%s%n", pulseResponseRet);
+        // Send low pulse survey score if appropriate
+        sendPulseLowScoreEmail(pulseResponseRet);
+
         return pulseResponseRet;
     }
 
@@ -120,5 +139,60 @@ public class PulseResponseServicesImpl implements PulseResponseService {
     private boolean isSubordinateTo(UUID reportMember, UUID currentUserId) {
         return memberProfileServices.getSubordinatesForId(currentUserId)
                 .stream().anyMatch(member -> member.getId().equals(reportMember));
+    }
+
+    public void sendPulseLowScoreEmail(PulseResponse pulseResponse) {
+        if (pulseResponse == null) return;
+
+        boolean hasLowInternalScore = pulseResponse.getInternalScore() != null && pulseResponse.getInternalScore() <= 2;
+        boolean hasLowExternalScore = pulseResponse.getExternalScore() != null && pulseResponse.getExternalScore() <= 2;
+
+        System.out.printf("%b %b\n%n", hasLowExternalScore, hasLowInternalScore);
+        if (!(hasLowInternalScore || hasLowExternalScore)) return;
+
+        UUID teamMemberId = pulseResponse.getTeamMemberId();
+        MemberProfile profileTakerMemberProfile;
+        if (teamMemberId != null && memberRepo.existsById(teamMemberId)) {
+            profileTakerMemberProfile = memberProfileServices.getById(teamMemberId);
+        } else {
+            log.warn("profileTaker does not exist");
+            return;
+        }
+
+        String firstName = profileTakerMemberProfile.getFirstName();
+        String lastName = profileTakerMemberProfile.getLastName();
+
+        String subject = String.format("%s pulse scores are low for team member %s %s",
+                hasLowInternalScore && hasLowExternalScore ? "Internal and external" :
+                        hasLowInternalScore ? "Internal" : "External", firstName, lastName);
+
+        StringBuilder bodyBuilder = new StringBuilder()
+                .append(String.format("Team member %s %s has left low %s pulse scores. Please consider reaching out to this employee at %s<br>", firstName, lastName,
+                        hasLowInternalScore && hasLowExternalScore ? "internal and external" :
+                                hasLowInternalScore ? "internal" : "external",
+                        profileTakerMemberProfile.getWorkEmail()));
+
+        if (hasLowInternalScore) {
+            bodyBuilder.append(String.format("Internal Feelings: %s<br>", pulseResponse.getInternalFeelings()));
+        }
+        if (hasLowExternalScore) {
+            bodyBuilder.append(String.format("External Feelings: %s<br>", pulseResponse.getExternalFeelings()));
+        }
+
+        List<String> recipients = new ArrayList<>();
+        UUID pdlId = profileTakerMemberProfile.getPdlId();
+        if (pdlId != null && memberRepo.existsById(pdlId)) {
+            recipients.add(memberProfileServices.getById(pdlId).getWorkEmail());
+        }
+
+        UUID supervisorId = profileTakerMemberProfile.getSupervisorid();
+        if (supervisorId != null && memberRepo.existsById(supervisorId)) {
+            recipients.add(memberProfileServices.getById(supervisorId).getWorkEmail());
+        }
+
+        if (!recipients.isEmpty()) {
+            System.out.println(subject);
+            emailSender.sendEmail("hr", "mckiernanc@objectcomputing.com", subject, bodyBuilder.toString(), recipients.toArray(new String[0]));
+        }
     }
 }
