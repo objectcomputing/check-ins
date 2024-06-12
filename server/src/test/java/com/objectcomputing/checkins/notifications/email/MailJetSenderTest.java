41449f844f174c2ddf637e3b505908c6bdd50153
Tim Yates <tim.yates@gmail.com>
2024-06-12 16:00:20 +0100

    Use correct executor and add controller tests for MailJetNotificationController

--- server/src/test/java/com/objectcomputing/checkins/notifications/email/MailJetSenderTest.java
+++ server/src/test/java/com/objectcomputing/checkins/notifications/email/MailJetSenderTest.java
@@ -1,14 +1,50 @@
 package com.objectcomputing.checkins.notifications.email;
 
 import com.objectcomputing.checkins.services.TestContainersSuite;
+import com.objectcomputing.checkins.services.fixture.MemberProfileFixture;
+import io.micronaut.context.annotation.Property;
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MutableHttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.test.annotation.MockBean;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.objectcomputing.checkins.services.role.RoleType.Constants.MEMBER_ROLE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-public class MailJetSenderTest extends TestContainersSuite {
+@Property(name = "micronaut.http.client.read-timeout", value = "5h")
+class MailJetSenderTest extends TestContainersSuite implements MemberProfileFixture {
+
+    private static final String TEST_EMAIL_BODY = """
+            {
+                "subject": "Test Subject",
+                "content": "Test Content",
+                "recipients": ["tim@woo.com", "dave@whee.com"]
+            }
+            """;
+
+    @Inject
+    @Client("/services/email-notifications")
+    HttpClient httpClient;
+
+    @Inject
+    @Named(MailJetConfig.HTML_FORMAT)
+    EmailSender emailSender;
 
     @Test
     void testCreateBatchesForManyRecipients() {
@@ -23,7 +59,7 @@ public class MailJetSenderTest extends TestContainersSuite {
 
         assertEquals(2, batches.size());
 
-        JSONArray firstBatch = batches.get(0);
+        JSONArray firstBatch = batches.getFirst();
         assertEquals(MailJetSender.MAILJET_RECIPIENT_LIMIT, firstBatch.length());
 
         List<String> firstEmailGroup = recipients.subList(0, MailJetSender.MAILJET_RECIPIENT_LIMIT);
@@ -42,5 +78,46 @@ public class MailJetSenderTest extends TestContainersSuite {
             assertInstanceOf(String.class, email);
             assertTrue(secondEmailGroup.contains(email));
         }
+    }
+
+    @Test
+    void successfulEndpointWorksAsExpected() {
+        var tim = memberWithoutBoss("tim");
+
+        when(emailSender.sendEmailReceivesStatus(
+                tim.getFirstName() + " " + tim.getLastName(),
+                tim.getWorkEmail(),
+                "Test Subject",
+                "Test Content",
+                "tim@woo.com",
+                "dave@whee.com"
+        )).thenReturn(true);
+
+        MutableHttpRequest<String> request = HttpRequest.POST("/", TEST_EMAIL_BODY).basicAuth(tim.getWorkEmail(), MEMBER_ROLE);
+        assertEquals(HttpStatus.OK, httpClient.toBlocking().exchange(request).getStatus());
+    }
+
+    @Test
+    void unsuccessfulEndpointWorksAsExpected() {
+        var tim = memberWithoutBoss("tim");
+
+        when(emailSender.sendEmailReceivesStatus(
+                tim.getFirstName() + " " + tim.getLastName(),
+                tim.getWorkEmail(),
+                "Test Subject",
+                "Test Content",
+                "tim@woo.com",
+                "dave@whee.com"
+        )).thenReturn(false);
+
+        MutableHttpRequest<String> request = HttpRequest.POST("/", TEST_EMAIL_BODY).basicAuth(tim.getWorkEmail(), MEMBER_ROLE);
+        var err = assertThrows(HttpClientResponseException.class, () -> httpClient.toBlocking().exchange(request));
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, err.getStatus());
+    }
+
+    @Replaces(MailJetSender.class)
+    @MockBean(value = EmailSender.class, named = MailJetConfig.HTML_FORMAT)
+    MailJetSender emailSender() {
+        return mock(MailJetSender.class);
     }
 }
