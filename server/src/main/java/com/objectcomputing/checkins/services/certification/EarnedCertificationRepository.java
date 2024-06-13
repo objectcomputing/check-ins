d32ec051cecb2e5b243d3b0ba957d1717c2384a4
Tim Yates <tim.yates@gmail.com>
2024-06-13 10:02:44 +0100

    Use nativeQuery for the Certification and Volunteering queries
    Spotted I wasn't using nativeQuery, I'm not 100% sure of the ramifications, but this commit switches to use it as with the other queries in the codebase

--- server/src/main/java/com/objectcomputing/checkins/services/certification/EarnedCertificationRepository.java
+++ server/src/main/java/com/objectcomputing/checkins/services/certification/EarnedCertificationRepository.java
@@ -14,39 +14,39 @@ public interface EarnedCertificationRepository extends CrudRepository<EarnedCert
 
     List<EarnedCertification> findByCertificationId(@NotNull UUID certificationId);
 
-    @Query("""
+    @Query(value = """
             SELECT earned.*
                 FROM earned_certification AS earned
                     LEFT JOIN certification AS cert USING(certification_id)
                 WHERE cert.is_active = TRUE OR :includeDeactivated = TRUE
-                ORDER BY earned.earned_date DESC, earned.description""")
+                ORDER BY earned.earned_date DESC, earned.description""", nativeQuery = true)
     List<EarnedCertification> findAllOrderByEarnedDateDesc(boolean includeDeactivated);
 
-    @Query("""
+    @Query(value = """
             SELECT earned.*
                 FROM earned_certification AS earned
                     LEFT JOIN certification AS cert USING(certification_id)
                 WHERE earned.certification_id = :certificationId
                   AND (cert.is_active = TRUE OR :includeDeactivated = TRUE)
-                ORDER BY earned.earned_date DESC, earned.description""")
+                ORDER BY earned.earned_date DESC, earned.description""", nativeQuery = true)
     List<EarnedCertification> findByCertificationIdOrderByEarnedDateDesc(@NotNull UUID certificationId, boolean includeDeactivated);
 
-    @Query("""
+    @Query(value = """
             SELECT earned.*
                 FROM earned_certification AS earned
                     LEFT JOIN certification AS cert USING(certification_id)
                 WHERE earned.member_id = :memberId
                   AND (cert.is_active = TRUE OR :includeDeactivated = TRUE)
-                ORDER BY earned.earned_date DESC, earned.description""")
+                ORDER BY earned.earned_date DESC, earned.description""", nativeQuery = true)
     List<EarnedCertification> findByMemberIdOrderByEarnedDateDesc(@NotNull UUID memberId, boolean includeDeactivated);
 
-    @Query("""
+    @Query(value = """
             SELECT earned.*
                 FROM earned_certification AS earned
                     LEFT JOIN certification AS cert USING(certification_id)
                 WHERE earned.certification_id = :certificationId
                   AND earned.member_id = :memberId
                   AND (cert.is_active = TRUE OR :includeDeactivated = TRUE)
-                ORDER BY earned.earned_date DESC, earned.description""")
+                ORDER BY earned.earned_date DESC, earned.description""", nativeQuery = true)
     List<EarnedCertification> findByMemberIdAndCertificationIdOrderByEarnedDateDesc(@NotNull UUID memberId, @NotNull UUID certificationId, boolean includeDeactivated);
 }
