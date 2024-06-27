e83d19cfdab8ca9e654d1d3838ce3caf0bb3d9be
Tim Yates <tim.yates@gmail.com>
2024-06-27 14:21:51 +0100

    Also add check-ins.application.name to the config
    And switch to a class so we can validate the nested props

--- server/src/main/java/com/objectcomputing/checkins/configuration/CheckInsConfiguration.java
+++ server/src/main/java/com/objectcomputing/checkins/configuration/CheckInsConfiguration.java
@@ -1,9 +1,28 @@
 package com.objectcomputing.checkins.configuration;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @ConfigurationProperties("check-ins")
-public interface CheckInsConfiguration {
+public class CheckInsConfiguration {
 
-    String getWebAddress();
+    @NotBlank
+    private String webAddress;
+
+    @NotNull
+    private ApplicationConfig application;
+
+    @Getter
+    @Setter
+    @ConfigurationProperties("application")
+    public static class ApplicationConfig {
+
+        @NotBlank
+        private String name;
+    }
 }
--- server/src/test/java/com/objectcomputing/checkins/configuration/CheckInsConfigurationTest.java
+++ server/src/test/java/com/objectcomputing/checkins/configuration/CheckInsConfigurationTest.java
@@ -1,23 +1,61 @@
 package com.objectcomputing.checkins.configuration;
 
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.exceptions.BeanInstantiationException;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.AssertionFailureBuilder.assertionFailure;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CheckInsConfigurationTest {
 
     @Test
-    void getWebAddress() {
-
+    void checkConfigurationGetsParsed() {
         try (var ctx = ApplicationContext.run(Map.of(
                 "datasources.enabled", false,
-                "check-ins.web-address", "http://google.com"
+                "check-ins.web-address", "http://google.com",
+                "check-ins.application.name", "Fancy app"
         ))) {
             var config = ctx.getBean(CheckInsConfiguration.class);
             assertEquals("http://google.com", config.getWebAddress());
+            assertEquals("Fancy app", config.getApplication().getName());
+        }
+    }
+
+    @Test
+    void checkWebAddressGetsValidated() {
+        try (var ctx = ApplicationContext.run(Map.of(
+                "datasources.enabled", false,
+                "check-ins.web-address", "",
+                "check-ins.application.name", "Fancy app"
+        ))) {
+            var beanInstantiationException = assertThrows(BeanInstantiationException.class, () -> ctx.getBean(CheckInsConfiguration.class));
+            assertContains("CheckInsConfiguration.webAddress - must not be blank", beanInstantiationException.getMessage());
+        }
+    }
+
+    @Test
+    void checkApplicationNameGetsValidated() {
+        try (var ctx = ApplicationContext.run(Map.of(
+                "datasources.enabled", false,
+                "check-ins.web-address", "http://google.com",
+                "check-ins.application.name", ""
+        ))) {
+            var beanInstantiationException = assertThrows(BeanInstantiationException.class, () -> ctx.getBean(CheckInsConfiguration.class));
+            assertContains("CheckInsConfiguration$ApplicationConfig.name - must not be blank", beanInstantiationException.getMessage());
+        }
+    }
+
+    private void assertContains(String expected, String actual) {
+        if (!actual.contains(expected)) {
+            assertionFailure()
+                    .message("Expected:\n'%s'\nto contain: '%s'".formatted(actual, expected))
+                    .expected(expected)
+                    .actual(actual)
+                    .buildAndThrow();
         }
     }
 }
