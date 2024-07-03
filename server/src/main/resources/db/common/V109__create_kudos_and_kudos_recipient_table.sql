4368fcc720970de7b70113b24080243662c9f836
Tim Yates <tim.yates@gmail.com>
2024-07-03 10:34:02 +0100

    Put DROP in correct order in migration

--- server/src/main/resources/db/common/V109__create_kudos_and_kudos_recipient_table.sql
+++ server/src/main/resources/db/common/V109__create_kudos_and_kudos_recipient_table.sql
@@ -1,17 +1,20 @@
+DROP TABLE IF EXISTS kudos_recipient;
 DROP TABLE IF EXISTS kudos;
-CREATE TABLE kudos(
-    id varchar PRIMARY KEY,
-    message varchar,
-    senderid varchar REFERENCES member_profile(id),
-    teamid varchar REFERENCES team(id),
-    datecreated date,
-    dateapproved date,
+
+CREATE TABLE kudos
+(
+    id              varchar PRIMARY KEY,
+    message         varchar,
+    senderid        varchar REFERENCES member_profile (id),
+    teamid          varchar REFERENCES team (id),
+    datecreated     date,
+    dateapproved    date,
     publiclyvisible boolean
 );
 
-DROP TABLE IF EXISTS kudos_recipient;
-CREATE TABLE kudos_recipient(
-    id varchar PRIMARY KEY,
-    kudosid varchar REFERENCES kudos(id),
-    memberid varchar REFERENCES member_profile(id)
+CREATE TABLE kudos_recipient
+(
+    id       varchar PRIMARY KEY,
+    kudosid  varchar REFERENCES kudos (id),
+    memberid varchar REFERENCES member_profile (id)
 );
