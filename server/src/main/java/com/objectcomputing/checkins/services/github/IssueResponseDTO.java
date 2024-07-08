48e44a3a60879ee0b82f5aed34790634ec3ee359
Tim Yates <tim.yates@gmail.com>
2024-07-08 14:43:57 +0100

    Add an object to contain GitHub configuration
    We also move the base URL into configuration so that we can test the client against a mock server.
    Added a test which mocks up a GitHub server, and tests agains JSON from the documentation to make sure it's all working.
    
    I'm not sure this is used however, as the GithubClient used to point to `repos/oci-labs/check-ins/issues` and I cannot find a check-ins project under the `oci-labs` owner...🤔

--- server/src/main/java/com/objectcomputing/checkins/services/github/IssueResponseDTO.java
+++ server/src/main/java/com/objectcomputing/checkins/services/github/IssueResponseDTO.java
@@ -2,37 +2,18 @@ package com.objectcomputing.checkins.services.github;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.micronaut.core.annotation.Introspected;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 @Introspected
 public class IssueResponseDTO {
+
     private String url;
 
     @JsonProperty("html_url")
     private String htmlUrl;
-
-    public IssueResponseDTO() {}
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getHtmlUrl() {
-        return htmlUrl;
-    }
-
-    public void setHtmlUrl(String htmlUrl) {
-        this.htmlUrl = htmlUrl;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubResponseDTO{" +
-                "url='" + url + '\'' +
-                ", htmlUrl='" + htmlUrl + '\'' +
-                '}';
-    }
 }
