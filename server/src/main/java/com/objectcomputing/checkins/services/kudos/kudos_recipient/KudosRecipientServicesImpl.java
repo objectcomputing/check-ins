5abb145271eebfc534af20cad7881028b7d6d469
Chad Elliott <elliottc@objectcomputing.com>
2024-07-29 11:09:31 -0500

    Iterate over members to find admin users.  Send email when public kudos is created.

--- server/src/main/java/com/objectcomputing/checkins/services/kudos/kudos_recipient/KudosRecipientServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/kudos/kudos_recipient/KudosRecipientServicesImpl.java
@@ -8,14 +8,20 @@ import com.objectcomputing.checkins.exceptions.NotFoundException;
 import com.objectcomputing.checkins.exceptions.PermissionException;
 import com.objectcomputing.checkins.services.kudos.Kudos;
 import com.objectcomputing.checkins.services.kudos.KudosRepository;
+import com.objectcomputing.checkins.services.role.Role;
+import com.objectcomputing.checkins.services.role.RoleType;
+import com.objectcomputing.checkins.services.role.RoleServices;
 import com.objectcomputing.checkins.services.memberprofile.MemberProfile;
+import com.objectcomputing.checkins.services.memberprofile.MemberProfileServices;
 import com.objectcomputing.checkins.services.memberprofile.MemberProfileRetrievalServices;
 import com.objectcomputing.checkins.services.memberprofile.currentuser.CurrentUserServices;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import static com.objectcomputing.checkins.services.validate.PermissionsValidation.NOT_AUTHORIZED_MSG;
@@ -27,6 +33,8 @@ public class KudosRecipientServicesImpl implements KudosRecipientServices {
     private final CurrentUserServices currentUserServices;
     private final KudosRepository kudosRepository;
     private final MemberProfileRetrievalServices memberProfileRetrievalServices;
+    private final MemberProfileServices memberProfileServices;
+    private final RoleServices roleServices;
     private final EmailSender emailSender;
     private final String webAddress;
 
@@ -34,12 +42,16 @@ public class KudosRecipientServicesImpl implements KudosRecipientServices {
                                       CurrentUserServices currentUserServices,
                                       KudosRepository kudosRepository,
                                       MemberProfileRetrievalServices memberProfileRetrievalServices,
+                                      MemberProfileServices memberProfileServices,
+                                      RoleServices roleServices,
                                       @Named(MailJetFactory.HTML_FORMAT) EmailSender emailSender,
                                       CheckInsConfiguration checkInsConfiguration) {
         this.kudosRecipientRepository = kudosRecipientRepository;
         this.currentUserServices = currentUserServices;
         this.kudosRepository = kudosRepository;
         this.memberProfileRetrievalServices = memberProfileRetrievalServices;
+        this.memberProfileServices = memberProfileServices;
+        this.roleServices = roleServices;
         this.emailSender = emailSender;
         this.webAddress = checkInsConfiguration.getWebAddress();
     }
@@ -82,22 +94,30 @@ public class KudosRecipientServicesImpl implements KudosRecipientServices {
         String fromEmail = currentUser.getWorkEmail();
         String fromName = currentUser.getFirstName() + " " + currentUser.getLastName();
         String subject = "Kudos";
-        String recipient = "";
+        List<String> recipients = new ArrayList<String>();
         String content = "";
         if (kudos.getPubliclyVisible()) {
-            // Need to send email to the admins to notify of new public kudos.
-            System.out.println("DEBUG: Public");
-            // TODO: Use admin email addresses instead of the member email.
-            recipient = member.getWorkEmail();
+            // Build a list of admin email addresses.
+            String adminRole = RoleType.ADMIN.toString();
+            for (MemberProfile profile : memberProfileServices.findAll()) {
+                Set<Role> userRoles = roleServices.findUserRoles(profile.getId());
+                for (Role role : userRoles) {
+                    if (role.getRole().equals(adminRole)) {
+                        recipients.add(profile.getWorkEmail());
+                        break;
+                    }
+                }
+            }
             content = "There are new kudos to review.<br>\nClick " +
                       webAddress + "/admin/manage-kudos to review them.";
         } else {
             // This is a private kudos, so notify the receiver directly.
-            recipient = member.getWorkEmail();
+            recipients.add(member.getWorkEmail());
             content = kudos.getMessage();
         }
-        System.out.println("DEBUG: send email to " + recipient);
-        emailSender.sendEmail(fromName, fromEmail, subject, content, recipient);
-        System.out.println("DEBUG: after send email");
+
+        for (String recipient : recipients) {
+            emailSender.sendEmail(fromName, fromEmail, subject, content, recipient);
+        }
     }
 }
