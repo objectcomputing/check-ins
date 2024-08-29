fb6c4fd730930ec6bf6ed31bf37705a57625ea30
Luciano Belotto <DarthLuch@gmail.com>
2024-08-29 13:59:48 -0400

    2024-08-29 - Added new fields period start/end date to back-end

--- server/src/test/java/com/objectcomputing/checkins/services/fixture/ReviewPeriodFixture.java
+++ server/src/test/java/com/objectcomputing/checkins/services/fixture/ReviewPeriodFixture.java
@@ -11,24 +11,24 @@ public interface ReviewPeriodFixture extends RepositoryFixture {
     default ReviewPeriod createADefaultReviewPeriod() {
         return getReviewPeriodRepository().save(new ReviewPeriod("Period of Time", ReviewStatus.OPEN, null, null,
                 LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(1).truncatedTo(ChronoUnit.MILLIS),
-                LocalDateTime.now().plusDays(2).truncatedTo(ChronoUnit.MILLIS)));
+                LocalDateTime.now().plusDays(2).truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(-30).truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(-1).truncatedTo(ChronoUnit.MILLIS)));
     }
 
     default ReviewPeriod createADefaultReviewPeriod(ReviewStatus reviewStatus) {
         return getReviewPeriodRepository().save(new ReviewPeriod("Period of Time", reviewStatus, null, null,
                 LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(1).truncatedTo(ChronoUnit.MILLIS),
-                LocalDateTime.now().plusDays(2).truncatedTo(ChronoUnit.MILLIS)));
+                LocalDateTime.now().plusDays(2).truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(-30).truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(-1).truncatedTo(ChronoUnit.MILLIS)));
     }
 
     default ReviewPeriod createASecondaryReviewPeriod() {
         return getReviewPeriodRepository().save(new ReviewPeriod("Period of Play", ReviewStatus.OPEN, null, null,
                 LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(1).truncatedTo(ChronoUnit.MILLIS),
-                LocalDateTime.now().plusDays(2).truncatedTo(ChronoUnit.MILLIS)));
+                LocalDateTime.now().plusDays(2).truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(-30).truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(-1).truncatedTo(ChronoUnit.MILLIS)));
     }
 
     default ReviewPeriod createAClosedReviewPeriod() {
         return getReviewPeriodRepository().save(new ReviewPeriod("Period of Closure", ReviewStatus.CLOSED, null, null,
                 LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(1).truncatedTo(ChronoUnit.MILLIS),
-                LocalDateTime.now().plusDays(2).truncatedTo(ChronoUnit.MILLIS)));
+                LocalDateTime.now().plusDays(2).truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(-30).truncatedTo(ChronoUnit.MILLIS), LocalDateTime.now().plusDays(-1).truncatedTo(ChronoUnit.MILLIS)));
     }
 }
