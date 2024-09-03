a82c6534585066972434990c270df9bdd43234b4
Luciano Belotto <DarthLuch@gmail.com>
2024-09-03 14:44:02 -0400

    2024-09-03 - bugfix-2579 - add check on null supervisors

--- server/src/main/java/com/objectcomputing/checkins/services/reviews/ReviewPeriodServicesImpl.java
+++ server/src/main/java/com/objectcomputing/checkins/services/reviews/ReviewPeriodServicesImpl.java
@@ -14,7 +14,6 @@ import jakarta.inject.Singleton;
 import jakarta.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -135,10 +134,11 @@ class ReviewPeriodServicesImpl implements ReviewPeriodServices {
     }
 
     private void notifyRevieweeSupervisorsByReviewPeriod(UUID reviewPeriodId, String reviewPeriodName) {
-        if (environment.getActiveNames().contains(Environments.LOCAL)) return;
+        // if (environment.getActiveNames().contains(Environments.LOCAL)) return;
         Set<ReviewAssignment> reviewAssignments = reviewAssignmentRepository.findByReviewPeriodId(reviewPeriodId);
         Set<UUID> revieweeIds = reviewAssignments.stream().map(ReviewAssignment::getRevieweeId).collect(Collectors.toSet());
         Set<UUID> supervisorIds = new HashSet<>(memberProfileRepository.findSupervisoridByIdIn(revieweeIds));
+        supervisorIds.removeIf(Objects::isNull);
         if (supervisorIds.isEmpty()) {
             LOG.info(String.format("Supervisors not found for Reviewees %s", revieweeIds));
             return;
