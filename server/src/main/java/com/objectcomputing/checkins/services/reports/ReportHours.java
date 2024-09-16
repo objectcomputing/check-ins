c00f63c1fb287bdac12e84a1eb8b4f8a36ad6597
Chad Elliott <elliottc@objectcomputing.com>
2024-09-16 09:25:42 -0500

    Converted classes to records.

--- server/src/main/java/com/objectcomputing/checkins/services/reports/ReportHours.java
+++ server/src/main/java/com/objectcomputing/checkins/services/reports/ReportHours.java
@@ -1,14 +1,9 @@
 package com.objectcomputing.checkins.services.reports;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-class ReportHours {
-  private final float contributionHours;
-  private final float ptoHours;
-  private final float overtimeHours;
-  private final float billableUtilization;
+record ReportHours(
+  float contributionHours,
+  float ptoHours,
+  float overtimeHours,
+  float billableUtilization
+) {
 }
-
--- server/src/main/java/com/objectcomputing/checkins/services/reports/ReportKudos.java
+++ server/src/main/java/com/objectcomputing/checkins/services/reports/ReportKudos.java
@@ -1,15 +1,10 @@
 package com.objectcomputing.checkins.services.reports;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.time.LocalDate;
 
-@AllArgsConstructor
-@Getter
-class ReportKudos {
-  private final LocalDate dateCreated;
-  private final String message;
-  private final String sender;
+record ReportKudos(
+  LocalDate dateCreated,
+  String message,
+  String sender
+) {
 }
-
