2b72a1a519b2701676b7a46effbbe780bf6c98d5
Tim Yates <tim.yates@gmail.com>
2024-09-19 14:03:14 +0100

    Mark serialized classes as Introspected for Graal

--- server/src/main/java/com/objectcomputing/checkins/services/reports/ReportKudos.java
+++ server/src/main/java/com/objectcomputing/checkins/services/reports/ReportKudos.java
@@ -1,7 +1,10 @@
 package com.objectcomputing.checkins.services.reports;
 
+import io.micronaut.core.annotation.Introspected;
+
 import java.time.LocalDate;
 
+@Introspected
 record ReportKudos(
   LocalDate dateCreated,
   String message,
