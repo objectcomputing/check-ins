d661f2fb2b33a8d198686b76d141e9be99039301
Chad Elliott <elliottc@objectcomputing.com>
2024-10-07 12:38:37 -0500

    Updated the tests to check the rendering for both with and without permission.

--- web-ui/src/pages/BirthdayReportPage.test.jsx
+++ web-ui/src/pages/BirthdayReportPage.test.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 import BirthdayReportPage from './BirthdayReportPage';
 import { AppContextProvider } from '../context/AppContext';
 
+const userStateWithPermission = {
+  state: {
+    userProfile: {
+      name: 'john watson',
+      role: ['MEMBER'],
+      permissions: [{ permission: 'CAN_VIEW_BIRTHDAY_REPORT' }],
+    }
+  }
+};
+
 it('renders correctly', () => {
+  const mockDate = new Date(2022, 1, 1);
+  const spy = vi.spyOn(global, 'Date').mockImplementation(() => mockDate);
+
+  snapshot(
+    <AppContextProvider value={userStateWithPermission}>
+      <BirthdayReportPage />
+    </AppContextProvider>
+  );
+
+  spy.mockRestore();
+});
+
+it('renders an error if user does not have appropriate permission', () => {
   const mockDate = new Date(2022, 1, 1);
   const spy = vi.spyOn(global, 'Date').mockImplementation(() => mockDate);
 
