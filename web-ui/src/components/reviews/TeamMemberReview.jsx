68838e3a09c1719155696e6ec7412c1a23339777
Chad Elliott <elliottc@objectcomputing.com>
2024-10-29 09:48:46 -0500

    Removed the Cancel and Reassign buttons from the review tab.

--- web-ui/src/components/reviews/TeamMemberReview.jsx
+++ web-ui/src/components/reviews/TeamMemberReview.jsx
@@ -98,25 +98,6 @@ const TeamMemberReview = ({
   const currentUser = selectCurrentUser(state);
   const theme = useTheme();
   const [value, setValue] = useState(1);
-  const [reassignOpen, setReassignOpen] = useState(false);
-  const [cancelOpen, setCancelOpen] = useState(false);
-
-  const handleOpenReassign = useCallback(
-    () => setReassignOpen(true),
-    [setReassignOpen]
-  );
-  const handleCloseReassign = useCallback(
-    () => setReassignOpen(false),
-    [setReassignOpen]
-  );
-  const handleOpenCancel = useCallback(
-    () => setCancelOpen(true),
-    [setCancelOpen]
-  );
-  const handleCloseCancel = useCallback(
-    () => setCancelOpen(false),
-    [setCancelOpen]
-  );
 
   const review = reviews && reviews[value - 1];
   const recipient = selectProfile(state, review?.recipientId);
@@ -128,84 +109,6 @@ const TeamMemberReview = ({
   const handleChangeIndex = index => {
     setValue(index);
   };
-
-  const handleCancelClick = useCallback(() => {
-    const cancelRequest = async () => {
-      const res = await cancelFeedbackRequest(review, csrf);
-      const cancellationResponse =
-        res && res.payload && res.payload.status === 200 && !res.error
-          ? res.payload.data
-          : null;
-      if (!cancellationResponse) {
-        window.snackDispatch({
-          type: UPDATE_TOAST,
-          payload: {
-            severity: 'error',
-            toast:
-              'There was an error cancelling the review. Please contact your administrator.'
-          }
-        });
-      }
-      return cancellationResponse;
-    };
-
-    handleCloseCancel();
-    if (csrf) {
-      cancelRequest().then(res => {
-        if (res) {
-          reloadReviews();
-          window.snackDispatch({
-            type: UPDATE_TOAST,
-            payload: {
-              severity: 'success',
-              toast: 'Review canceled'
-            }
-          });
-        }
-      });
-    }
-  }, [csrf, handleCloseCancel, review, reloadReviews]);
-
-  const handleReassign = useCallback(
-    assignee => {
-      const reassignRequest = async () => {
-        review.recipientId = assignee.id;
-        const res = await updateFeedbackRequest(review, csrf);
-        const updateResponse =
-          res && res.payload && res.payload.status === 200 && !res.error
-            ? res.payload.data
-            : null;
-        if (!updateResponse) {
-          window.snackDispatch({
-            type: UPDATE_TOAST,
-            payload: {
-              severity: 'error',
-              toast:
-                'There was an error reassigning the review. Please contact your administrator.'
-            }
-          });
-        }
-        return updateResponse;
-      };
-
-      handleCloseReassign();
-      if (csrf) {
-        reassignRequest().then(res => {
-          if (res) {
-            reloadReviews();
-            window.snackDispatch({
-              type: UPDATE_TOAST,
-              payload: {
-                severity: 'success',
-                toast: 'Review reassigned'
-              }
-            });
-          }
-        });
-      }
-    },
-    [csrf, handleCloseReassign, review, reloadReviews]
-  );
 
   let selfReviewIcon = <HourglassEmptyIcon />;
   if (selfReview && selfReview.status?.toUpperCase() === 'SUBMITTED') {
@@ -280,26 +183,6 @@ const TeamMemberReview = ({
 
               return (
                 <TabPanel value={value} index={index + 1} dir={theme.direction}>
-                  {review.status?.toUpperCase() !== 'SUBMITTED' && (
-                    <div className={classes.buttonRow}>
-                      <Button
-                        onClick={handleOpenCancel}
-                        className={classes.actionButtons}
-                        variant="outlined"
-                        color="secondary"
-                      >
-                        Cancel
-                      </Button>
-                      <Button
-                        onClick={handleOpenReassign}
-                        className={classes.actionButtons}
-                        variant="outlined"
-                        color="primary"
-                      >
-                        Reassign
-                      </Button>
-                    </div>
-                  )}
                   <FeedbackSubmitForm
                     requesteeName={requesteeName}
                     requestId={review.id}
@@ -310,39 +193,6 @@ const TeamMemberReview = ({
               );
             })}
         </SwipeableViews>
-        <SelectUserModal
-          userLabel="Reviewer"
-          open={reassignOpen}
-          onSelect={handleReassign}
-          onClose={handleCloseReassign}
-        />
-        <Modal open={cancelOpen} onClose={handleCloseCancel}>
-          <Card className="cancel-feedback-request-modal">
-            <CardHeader
-              title={
-                <Typography variant="h5" fontWeight="bold">
-                  Cancel Review
-                </Typography>
-              }
-            />
-            <CardContent>
-              <Typography variant="body1">
-                Are you sure you want to cancel the review sent to{' '}
-                <b>{recipient?.name}</b> on <b>{review?.sendDate}</b>? The
-                recipient will not be able to respond to this request once it is
-                canceled.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button color="secondary" onClick={handleCloseCancel}>
-                No, Keep Feedback Request
-              </Button>
-              <Button color="primary" onClick={handleCancelClick}>
-                Yes, Cancel Feedback Request
-              </Button>
-            </CardActions>
-          </Card>
-        </Modal>
       </Box>
     </Root>
   );
