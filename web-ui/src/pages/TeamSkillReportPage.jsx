752e4adcbe7cfce1b57ddce874428883ee5a3a52
Chad Elliott <elliottc@objectcomputing.com>
2024-11-01 12:31:59 -0500

    Filter member skills based on non-terminated members.

--- web-ui/src/pages/TeamSkillReportPage.jsx
+++ web-ui/src/pages/TeamSkillReportPage.jsx
@@ -82,6 +82,10 @@ const TeamSkillReportPage = () => {
           : undefined;
     }
     if (memberSkillsFound && memberProfiles) {
+      memberSkillsFound = memberSkillsFound.filter(
+        mSkill => memberProfiles.find(member => member.id == mSkill.id)
+      );
+
       setAllSearchResults(memberSkillsFound);
       let membersSelected = memberSkillsFound.filter(mSkill =>
         selectedMembers.some(member => member.id === mSkill.id)
