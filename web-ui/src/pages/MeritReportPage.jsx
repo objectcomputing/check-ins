c55e32de7a294912414097ecb7bc5840db37e290
Chad Elliott <elliottc@objectcomputing.com>
2024-11-06 12:02:08 -0600

    Added a totalComp column and allow either amount or totalComp to be empty.  The merit report includes total comp for the year along with the compensation history.

--- web-ui/src/pages/MeritReportPage.jsx
+++ web-ui/src/pages/MeritReportPage.jsx
@@ -432,9 +432,8 @@ const MeritReportPage = () => {
     return text;
   };
 
-  const markdownCompensationHistory = (data) => {
-    // Sort them latest to oldest and truncate to the first 3.
-    const compHistory = data.compensationHistory.sort((a, b) => {
+  const prepareCompensationHistory = (data, fn) => {
+    return data.compensationHistory.filter(fn).sort((a, b) => {
       for(let i = 0; i < a.length; i++) {
         if (a.startDate[i] != b.startDate[i]) {
           return b.startDate[i] - a.startDate[i];
@@ -443,10 +442,20 @@ const MeritReportPage = () => {
       return 0;
     }).slice(0, 3);
 
+  };
+
+  const markdownCompensationHistory = (data) => {
+    // Sort them latest to oldest and truncate to the first 3.
+    const compBase = prepareCompensationHistory(data, (comp) => !!comp.amount);
+    const compTotal = prepareCompensationHistory(data, (comp) => !!comp.totalComp);
+
     let text = markdown.headers.h2("Compensation History");
-    text += markdown.lists.ul(compHistory,
+    text += markdown.lists.ul(compBase,
                 (comp) => formatDate(dateFromArray(comp.startDate)) + " - " +
-                "$" + comp.amount.toFixed(2) + " (base)");
+                "$" + parseFloat(comp.amount).toFixed(2) + " (base)");
+    text += markdown.lists.ul(compTotal,
+                (comp) => dateFromArray(comp.startDate).getFullYear() + " - " +
+                comp.totalComp);
     return text;
   };
 
