7d15622ed47a3f8f195cceef7c3bc1d8a6d0ef16
thelenw <thelenw@objectcomputing.com>
2024-11-12 15:12:59 -0600

    Attempt to resolve build test failure

--- web-ui/src/components/view_feedback_responses/feedback_response_card/FeedbackResponseCard.jsx
+++ web-ui/src/components/view_feedback_responses/feedback_response_card/FeedbackResponseCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -10,13 +10,6 @@ import { selectProfile } from '../../../context/selectors';
 import Avatar from '@mui/material/Avatar';
 import { getAvatarURL } from '../../../api/api.js';
 import FeedbackAnswerInput from '../../feedback_answer_input/FeedbackAnswerInput';
-
-const propTypes = {
-  responderId: PropTypes.string.isRequired,
-  answer: PropTypes.string, // Allow answer to be null or undefined
-  inputType: PropTypes.string.isRequired,
-  sentiment: PropTypes.number
-};
 
 const FeedbackResponseCard = props => {
   const { state } = useContext(AppContext);
@@ -59,10 +52,10 @@ const FeedbackResponseCard = props => {
 
 FeedbackResponseCard.propTypes = {
   responderId: PropTypes.string.isRequired,
-  answer: PropTypes.string.isRequired,
+  answer: PropTypes.string,
   inputType: PropTypes.string.isRequired,
   sentiment: PropTypes.number,
-  handleDenyClick: PropTypes.func.isRequired
+  handleDenyClick: PropTypes.func
 };
 
 export default FeedbackResponseCard;
