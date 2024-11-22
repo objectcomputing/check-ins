5c5a07655638d067b551899f315e24e0c150b935
Luciano Belotto <DarthLuch@gmail.com>
2024-11-22 14:59:04 -0500

    2024-11-22 - allow edit of ext-recip

--- web-ui/src/components/feedback_external_recipient_card/EditRecipientModal.jsx
+++ web-ui/src/components/feedback_external_recipient_card/EditRecipientModal.jsx
@@ -30,68 +30,73 @@ const EditRecipientModal = ({ open, onClose, profile, onChange, onSubmit }) => {
     };
 
     return (
-        <Modal open={open} onClose={onClose}>
-            <Box sx={{ ...modalStyle, width: 400 }}>
-                <Typography variant="h6" component="h2">
-                    Edit Recipient Profile
-                </Typography>
-                <TextField
-                    label="First Name"
-                    name="firstName"
-                    value={profile.firstName}
-                    onChange={onChange}
-                    fullWidth
-                    margin="normal"
-                    error={!!error && !profile.firstName}
-                    helperText={!!error && !profile.firstName ? 'First Name is required' : ''}
-                />
-                <TextField
-                    label="Last Name"
-                    name="lastName"
-                    value={profile.lastName}
-                    onChange={onChange}
-                    fullWidth
-                    margin="normal"
-                    error={!!error && !profile.lastName}
-                    helperText={!!error && !profile.lastName ? 'Last Name is required' : ''}
-                />
-                <TextField
-                    label="Email"
-                    name="email"
-                    value={profile.email}
-                    onChange={onChange}
-                    fullWidth
-                    margin="normal"
-                    error={!!error && (!profile.email || error === 'Invalid email address')}
-                    helperText={!!error && (!profile.email ? 'Email is required' : error === 'Invalid email address' ? 'Invalid email address' : '')}
-                />
-                <TextField
-                    label="Company Name"
-                    name="companyName"
-                    value={profile.companyName}
-                    onChange={onChange}
-                    fullWidth
-                    margin="normal"
-                    error={!!error && !profile.companyName}
-                    helperText={!!error && !profile.companyName ? 'Company Name is required' : ''}
-                />
-                <Select
-                    label="Status"
-                    name="inactive"
-                    value={profile.inactive ? 'Inactive' : 'Active'}
-                    onChange={(e) => onChange({ target: { name: 'inactive', value: e.target.value === 'Inactive' } })}
-                    fullWidth
-                    margin="normal"
-                >
-                    <MenuItem value="Active">Active</MenuItem>
-                    <MenuItem value="Inactive">Inactive</MenuItem>
-                </Select>
-                <Box mt={2} />
-                <Button onClick={handleSubmit} variant="contained" color="primary">
-                    Save
-                </Button>
-            </Box>
-        </Modal>
+        <>
+        { profile ? (
+                <Modal open={open} onClose={onClose}>
+                    <Box sx={{ ...modalStyle, width: 400 }}>
+                        <Typography variant="h6" component="h2">
+                            Edit Recipient Profile
+                        </Typography>
+                        <TextField
+                            label="First Name"
+                            name="firstName"
+                            value={profile.firstName}
+                            onChange={onChange}
+                            fullWidth
+                            margin="normal"
+                            error={!!error && !profile.firstName}
+                            helperText={!!error && !profile.firstName ? 'First Name is required' : ''}
+                        />
+                        <TextField
+                            label="Last Name"
+                            name="lastName"
+                            value={profile.lastName}
+                            onChange={onChange}
+                            fullWidth
+                            margin="normal"
+                            error={!!error && !profile.lastName}
+                            helperText={!!error && !profile.lastName ? 'Last Name is required' : ''}
+                        />
+                        <TextField
+                            label="Email"
+                            name="email"
+                            value={profile.email}
+                            onChange={onChange}
+                            fullWidth
+                            margin="normal"
+                            error={!!error && (!profile.email || error === 'Invalid email address')}
+                            helperText={!!error && (!profile.email ? 'Email is required' : error === 'Invalid email address' ? 'Invalid email address' : '')}
+                        />
+                        <TextField
+                            label="Company Name"
+                            name="companyName"
+                            value={profile.companyName}
+                            onChange={onChange}
+                            fullWidth
+                            margin="normal"
+                            error={!!error && !profile.companyName}
+                            helperText={!!error && !profile.companyName ? 'Company Name is required' : ''}
+                        />
+                        <Select
+                            label="Status"
+                            name="inactive"
+                            value={profile.inactive ? 'Inactive' : 'Active'}
+                            onChange={(e) => onChange({ target: { name: 'inactive', value: e.target.value === 'Inactive' } })}
+                            fullWidth
+                            margin="normal"
+                        >
+                            <MenuItem value="Active">Active</MenuItem>
+                            <MenuItem value="Inactive">Inactive</MenuItem>
+                        </Select>
+                        <Box mt={2} />
+                        <Button onClick={handleSubmit} variant="contained" color="primary">
+                            Save
+                        </Button>
+                    </Box>
+                </Modal>
+            ) : null
+        }
+        </>
     );
 };
 
