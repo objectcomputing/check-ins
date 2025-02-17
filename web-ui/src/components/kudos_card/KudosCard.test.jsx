3b73a48f28ee43346157de9494b8ae5fb62ec0ba
Chad Elliott <elliottc@objectcomputing.com>
2025-02-17 10:24:13 -0600

    Added a test for KudosCard.

---
+++ web-ui/src/components/kudos_card/KudosCard.test.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import KudosCard from './KudosCard';
+import { AppContextProvider } from '../../context/AppContext';
+
+const initialState = {
+  state: {
+    csrf: 'O_3eLX2-e05qpS_yOeg1ZVAs9nDhspEi',
+    teams: [],
+    userProfile: {
+      id: "1",
+      firstName: 'Jimmy',
+      lastName: 'Johnson',
+      role: ['MEMBER'],
+    },
+    terminatedMembers: [
+      {
+        id: "5",
+        firstName: 'Jerry',
+        lastName: 'Garcia',
+        name: 'Jerry Garcia',
+        role: ['MEMBER'],
+      },
+    ],
+    memberProfiles: [
+      {
+        id: "1",
+        firstName: 'Jimmy',
+        lastName: 'Johnson',
+        name: 'Jimmy Johnson',
+        role: ['MEMBER'],
+      },
+      {
+        id: "2",
+        firstName: 'Jimmy',
+        lastName: 'Olsen',
+        name: 'Jimmy Olsen',
+        role: ['MEMBER'],
+      },
+      {
+        id: "3",
+        firstName: 'Clark',
+        lastName: 'Kent',
+        name: 'Clark Kent',
+        role: ['MEMBER'],
+      },
+      {
+        id: "4",
+        firstName: 'Kent',
+        lastName: 'Brockman',
+        name: 'Kent Brockman',
+        role: ['MEMBER'],
+      },
+      {
+        id: "6",
+        firstName: 'Brock',
+        lastName: 'Smith',
+        name: 'Brock Smith',
+        role: ['MEMBER'],
+      },
+      {
+        id: "7",
+        firstName: 'Jimmy',
+        middleName: 'T.',
+        lastName: 'Olsen',
+        name: 'Jimmy T. Olsen',
+        role: ['MEMBER'],
+      },
+    ],
+  }
+};
+
+const kudos = {
+  id: 'test-kudos',
+  message: "Brock and Brockman did a great job helping Clark, Jimmy Olsen, Jimmy T. Olsen, and Johnson",
+  senderId: "5",
+  dateCreated: [ 2025, 2, 14 ],
+  recipientMembers: [
+      {
+        id: "1",
+        firstName: 'Jimmy',
+        lastName: 'Johnson',
+        role: ['MEMBER'],
+      },
+      {
+        id: "2",
+        firstName: 'Jimmy',
+        lastName: 'Olsen',
+        role: ['MEMBER'],
+      },
+      {
+        id: "3",
+        firstName: 'Clark',
+        lastName: 'Kent',
+        role: ['MEMBER'],
+      },
+      {
+        id: "6",
+        firstName: 'Brock',
+        lastName: 'Smith',
+        role: ['MEMBER'],
+      },
+      {
+        id: "4",
+        firstName: 'Kent',
+        lastName: 'Brockman',
+        role: ['MEMBER'],
+      },
+      {
+        id: "7",
+        firstName: 'Jimmy',
+        middleName: 'T.',
+        lastName: 'Olsen',
+        role: ['MEMBER'],
+      },
+  ],
+};
+
+it('renders correctly', () => {
+  snapshot(
+    <AppContextProvider value={initialState}>
+      <KudosCard
+        kudos={kudos}
+        includeActions
+        includeEdit
+        onKudosAction={() =>{}}
+      />
+    </AppContextProvider>
+  );
+});
