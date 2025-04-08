cc8bb32ff0562627e4c198ab6d55eae7afa9554b
Michael Kimberlin <mkimberlin@gmail.com>
2025-04-08 10:00:09 -0500

    Added caching to emoji results

--- server/src/main/java/com/objectcomputing/checkins/services/slack/SlackController.java
+++ server/src/main/java/com/objectcomputing/checkins/services/slack/SlackController.java
@@ -1,5 +1,6 @@
 package com.objectcomputing.checkins.services.slack;
 
+import io.micronaut.cache.CacheConfiguration;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -8,10 +9,14 @@ import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.inject.Named;
 
 import java.net.URI;
+import java.time.Duration;
 import java.util.Map;
 import java.util.UUID;
+
+import static io.micronaut.http.HttpHeaders.CACHE_CONTROL;
 
 @Controller("/services/slack")
 @ExecuteOn(TaskExecutors.BLOCKING)
@@ -20,8 +25,13 @@ import java.util.UUID;
 public class SlackController {
 
     private final SlackSearch slackSearch;
+    private final long expiry;
 
-    public SlackController(SlackSearch slackSearch) {
+    public SlackController(@Named("slack-cache") CacheConfiguration cacheConfiguration, SlackSearch slackSearch) {
+        // If un-configured, default to 1 hour
+        this.expiry = cacheConfiguration.getExpireAfterWrite()
+                .map(Duration::toSeconds)
+                .orElseGet(() -> Duration.ofHours(1).toSeconds());
         this.slackSearch = slackSearch;
     }
 
@@ -30,7 +40,7 @@ public class SlackController {
         Map<String, String> customEmoji = slackSearch.getCustomEmoji();
 
         return HttpResponse
-                .ok()
+                .ok().header(CACHE_CONTROL, "public, max-age=%d".formatted(expiry))
                 .headers(headers -> headers.location(URI.create("/services/slack/emoji")))
                 .body(customEmoji);
     }
--- server/src/main/java/com/objectcomputing/checkins/services/slack/SlackSearch.java
+++ server/src/main/java/com/objectcomputing/checkins/services/slack/SlackSearch.java
@@ -17,6 +17,8 @@ import com.slack.api.methods.response.users.UsersLookupByEmailResponse;
 import com.slack.api.methods.request.users.UsersInfoRequest;
 import com.slack.api.methods.response.users.UsersInfoResponse;
 
+import io.micronaut.cache.annotation.CacheConfig;
+import io.micronaut.cache.annotation.Cacheable;
 import jakarta.inject.Singleton;
 import jakarta.inject.Inject;
 
@@ -28,6 +30,7 @@ import jnr.ffi.annotations.In;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@CacheConfig("slack-cache")
 @Singleton
 public class SlackSearch {
     private static final Logger LOG = LoggerFactory.getLogger(SlackSearch.class);
@@ -127,7 +130,7 @@ public class SlackSearch {
     }
 
 
-
+    @Cacheable
     public Map<String, String> getCustomEmoji() {
         String token = configuration.getApplication().getSlack().getBotToken();
         if (token != null) {
