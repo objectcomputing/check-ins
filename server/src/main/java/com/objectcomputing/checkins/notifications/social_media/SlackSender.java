22939047e663d7f1fc5ccd62a4b0432f5e2094bd
Michael Kimberlin <mkimberlin@gmail.com>
2025-04-10 15:20:59 -0500

    Log the slack blocks

--- server/src/main/java/com/objectcomputing/checkins/notifications/social_media/SlackSender.java
+++ server/src/main/java/com/objectcomputing/checkins/notifications/social_media/SlackSender.java
@@ -69,6 +69,7 @@ public class SlackSender {
                     .blocksAsString(slackBlocks)
                     .build();
 
+                LOG.info("Slack Blocks: {}", slackBlocks);
                 LOG.info("Chat Post Request: {}", request.toString());
 
                 // Send it to Slack
